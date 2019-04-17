3a3eb1f9060a13f53862ea5518b4c5cc89295890
Andreas Gruenbacher <agruenba@redhat.com>
2019-04-17 12:04:27 -0600

    gfs2: new slab for transactions
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1925121
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=38277141
    Tested: xfstests, QE tests
    Upstream Status: upstream
    
    commit b839dadae8721eaf7245fcef3d67d82b95d00c77
    Author: Bob Peterson <rpeterso@redhat.com>
    Date:   Wed Apr 17 12:04:27 2019 -0600
    
        gfs2: new slab for transactions
    
        This patch adds a new slab for gfs2 transactions. That allows us to
        reduce kernel memory fragmentation, have better organization of data
        for analysis of vmcore dumps. A new centralized function is added to
        free the slab objects, and it exposes use-after-free by giving
        warnings if a transaction is freed while it still has bd elements
        attached to its buffers or ail lists. We make sure to initialize
        those transaction ail lists so we can check their integrity when freeing.
    
        At a later time, we should add a slab initialization function to
        make it more efficient, but for this initial patch I wanted to
        minimize the impact.
    
        Signed-off-by: Bob Peterson <rpeterso@redhat.com>
        Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    
    Signed-off-by: Bob Peterson <rpeterso@redhat.com>
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- fs/gfs2/log.c
+++ fs/gfs2/log.c
@@ -33,6 +33,7 @@
 #include "util.h"
 #include "dir.h"
 #include "trace_gfs2.h"
+#include "trans.h"
 
 static void gfs2_log_shutdown(struct gfs2_sbd *sdp);
 
@@ -353,7 +354,7 @@ static void ail2_empty(struct gfs2_sbd *sdp, unsigned int new_tail)
 		list_del(&tr->tr_list);
 		gfs2_assert_warn(sdp, list_empty(&tr->tr_ail1_list));
 		gfs2_assert_warn(sdp, list_empty(&tr->tr_ail2_list));
-		kfree(tr);
+		gfs2_trans_free(sdp, tr);
 	}
 
 	spin_unlock(&sdp->sd_ail_lock);
@@ -833,14 +834,14 @@ static void ail_drain(struct gfs2_sbd *sdp)
 		gfs2_ail_empty_tr(sdp, tr, &tr->tr_ail1_list);
 		gfs2_ail_empty_tr(sdp, tr, &tr->tr_ail2_list);
 		list_del(&tr->tr_list);
-		kfree(tr);
+		gfs2_trans_free(sdp, tr);
 	}
 	while (!list_empty(&sdp->sd_ail2_list)) {
 		tr = list_first_entry(&sdp->sd_ail2_list, struct gfs2_trans,
 				      tr_list);
 		gfs2_ail_empty_tr(sdp, tr, &tr->tr_ail2_list);
 		list_del(&tr->tr_list);
-		kfree(tr);
+		gfs2_trans_free(sdp, tr);
 	}
 	spin_unlock(&sdp->sd_ail_lock);
 }
@@ -983,7 +984,7 @@ out_end:
 	trace_gfs2_log_flush(sdp, 0, flags);
 out:
 	up_write(&sdp->sd_log_flush_lock);
-	kfree(tr);
+	gfs2_trans_free(sdp, tr);
 	if (gfs2_withdrawing(sdp))
 		gfs2_withdraw(sdp);
 	return;
