641bed34d0f92e9cdac9f9d17786d447d9fe3bad
Chris von Recklinghausen <crecklin@redhat.com>
2020-01-01 20:05:03 +0100

    Revert "fs: remove ksys_dup()"
    Conflicts: init/main.c - We already have
    	dfd402a4c4ba ("kcsan: Add Kernel Concurrency Sanitizer infrastructure")
    	so keep #include of linux/kcsan.h
    
    Bugzilla: http://bugzilla.redhat.com/1985062
    
    This reverts commit 8243186f0cc7 ("fs: remove ksys_dup()") and the
    subsequent fix for it in commit 2d3145f8d280 ("early init: fix error
    handling when opening /dev/console").
    
    Trying to use filp_open() and f_dupfd() instead of pseudo-syscalls
    caused more trouble than what is worth it: it requires accessing vfs
    internals and it turns out there were other bugs in it too.
    
    In particular, the file reference counting was wrong - because unlike
    the original "open+2*dup" sequence it used "filp_open+3*f_dupfd" and
    thus had an extra leaked file reference.
    
    That in turn then caused odd problems with Androidx86 long after boot
    becaue of how the extra reference to the console kept the session active
    even after all file descriptors had been closed.
    
    Reported-by: youling 257 <youling257@gmail.com>
    Cc: Arvind Sankar <nivedita@alum.mit.edu>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 74f1a299107b9e1a563831a4ba85f769ab577164)
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -1150,6 +1150,7 @@ asmlinkage long sys_ni_syscall(void);
 int ksys_mount(const char __user *dev_name, const char __user *dir_name,
 	       const char __user *type, unsigned long flags, void __user *data);
 int ksys_umount(char __user *name, int flags);
+int ksys_dup(unsigned int fildes);
 int ksys_chroot(const char __user *filename);
 ssize_t ksys_write(unsigned int fd, const char __user *buf, size_t count);
 int ksys_chdir(const char __user *filename);
