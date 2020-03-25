c6b8431ee199ea410bd2296afe6120fc28e41e88
Chris von Recklinghausen <crecklin@redhat.com>
2020-03-25 10:00:21 -0500

    exec: Factor unshare_sighand out of de_thread and call it separately
    Conflicts: fs/exec.c - The RHEL-only patch
    	3e303ef430a3 ("tasklist_lock, ppc64: Convert tasklist_lock to use qrwlock for ppc64")
    	changed read_{un}lock to qread_{un}lock, causing a difference in
    	deleted code. Also call qread_{un}lock instead of read_{un}lock in
    	added hunk.
    
    Bugzilla: http://bugzilla.redhat.com/1993665
    
    This makes the code clearer and makes it easier to implement a mutex
    that is not taken over any locations that may block indefinitely waiting
    for userspace.
    
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Reviewed-by: Bernd Edlinger <bernd.edlinger@hotmail.de>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Acked-by: Christian Brauner <christian.brauner@ubuntu.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Bernd Edlinger <bernd.edlinger@hotmail.de>
    Signed-off-by: Eric W. Biederman <ebiederm@xmission.com>
    (cherry picked from commit 021691559245498dfa15454c9fc4351f367d0b7f)
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1200,6 +1200,23 @@ no_thread_group:
 	flush_itimer_signals();
 #endif
 
+	BUG_ON(!thread_group_leader(tsk));
+	return 0;
+
+killed:
+	/* protects against exit_notify() and __exit_signal() */
+	qread_lock(&tasklist_lock);
+	sig->group_exit_task = NULL;
+	sig->notify_count = 0;
+	qread_unlock(&tasklist_lock);
+	return -EAGAIN;
+}
+
+
+static int unshare_sighand(struct task_struct *me)
+{
+	struct sighand_struct *oldsighand = me->sighand;
+
 	if (refcount_read(&oldsighand->count) != 1) {
 		struct sighand_struct *newsighand;
 		/*
@@ -1216,23 +1233,13 @@ no_thread_group:
 
 		qwrite_lock_irq(&tasklist_lock);
 		spin_lock(&oldsighand->siglock);
-		rcu_assign_pointer(tsk->sighand, newsighand);
+		rcu_assign_pointer(me->sighand, newsighand);
 		spin_unlock(&oldsighand->siglock);
 		qwrite_unlock_irq(&tasklist_lock);
 
 		__cleanup_sighand(oldsighand);
 	}
-
-	BUG_ON(!thread_group_leader(tsk));
 	return 0;
-
-killed:
-	/* protects against exit_notify() and __exit_signal() */
-	qread_lock(&tasklist_lock);
-	sig->group_exit_task = NULL;
-	sig->notify_count = 0;
-	qread_unlock(&tasklist_lock);
-	return -EAGAIN;
 }
 
 char *__get_task_comm(char *buf, size_t buf_size, struct task_struct *tsk)
@@ -1270,10 +1277,16 @@ int flush_old_exec(struct linux_binprm * bprm)
 	int retval;
 
 	/*
-	 * Make sure we have a private signal table and that
-	 * we are unassociated from the previous thread group.
+	 * Make this the only thread in the thread group.
 	 */
 	retval = de_thread(me);
+	if (retval)
+		goto out;
+
+	/*
+	 * Make the signal table private.
+	 */
+	retval = unshare_sighand(me);
 	if (retval)
 		goto out;
 
