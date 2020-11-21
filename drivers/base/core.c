9902d7652702e8c96699ed27a93d2210fd92f3c3
Mark Langsdorf <mlangsdo@redhat.com>
2020-11-20 18:02:28 -0800

    driver core: Use device's fwnode to check if it is waiting for suppliers
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1982374
    Upstream Status: Linux v5.11
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=41307652
    Test Info: https://beaker.engineering.redhat.com/jobs/?jobsearch-0.table=Whiteboard&jobsearch-0.operation=contains&jobsearch-0.value=cki%40gitlab%3A409314108
    Conflicts:
           drivers/base/core.c - some fixup errors because RHEL8 uses
    dev->links_needs_suppliers instead of dev->links.needs_suppliers but
    the final result is obvious
    
    commit 25ac86c6dbe62fba9b97e997fa648cdbe2d40173
    Author: Saravana Kannan <saravanak@google.com>
    Date: Fri, 20 Nov 2020 18:02:28 -0800
    
    To check if a device is still waiting for its supplier devices to be
    added, we used to check if the devices is in a global
    waiting_for_suppliers list. Since the global list will be deleted in
    subsequent patches, this patch stops using this check.
    
    Instead, this patch uses a more device specific check. It checks if the
    device's fwnode has any fwnode links that haven't been converted to
    device links yet.
    
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Link: https://lore.kernel.org/r/20201121020232.908850-14-saravanak@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 25ac86c6dbe62fba9b97e997fa648cdbe2d40173)
    
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -50,6 +50,7 @@ static DEFINE_MUTEX(wfs_lock);
 static LIST_HEAD(deferred_sync);
 static unsigned int defer_sync_state_count = 1;
 static DEFINE_MUTEX(fwnode_link_lock);
+static bool fw_devlink_is_permissive(void);
 
 /**
  * fwnode_link_add - Create a link between two fwnode_handles.
@@ -984,13 +985,13 @@ int device_links_check_suppliers(struct device *dev)
 	 * Device waiting for supplier to become available is not allowed to
 	 * probe.
 	 */
-	mutex_lock(&wfs_lock);
-	if (!list_empty(&dev->links_needs_suppliers) &&
-	    dev->links_need_for_probe) {
-		mutex_unlock(&wfs_lock);
+        mutex_lock(&fwnode_link_lock);
+	if (dev->fwnode && !list_empty(&dev->fwnode->suppliers) &&
+	    !fw_devlink_is_permissive()) {
+		mutex_unlock(&fwnode_link_lock);
 		return -EPROBE_DEFER;
 	}
-	mutex_unlock(&wfs_lock);
+	mutex_unlock(&fwnode_link_lock);
 
 	device_links_write_lock();
 
@@ -1156,8 +1157,7 @@ static ssize_t waiting_for_supplier_show(struct device *dev,
 	bool val;
 
 	device_lock(dev);
-	val = !list_empty(&dev->links_needs_suppliers)
-	      && dev->links_need_for_probe;
+	val = !list_empty(&dev->fwnode->suppliers);
 	device_unlock(dev);
 	return sysfs_emit(buf, "%u\n", val);
 }
@@ -1550,8 +1550,8 @@ static void fw_devlink_parse_fwtree(struct fwnode_handle *fwnode)
 	while ((child = fwnode_get_next_available_child_node(fwnode, child)))
 		fw_devlink_parse_fwtree(child);
 }
-
 #endif
+
 static void fw_devlink_link_device(struct device *dev)
 {
 	int fw_ret;
@@ -2174,7 +2174,7 @@ static int device_add_attrs(struct device *dev)
 			goto err_remove_dev_groups;
 	}
 
-	if (fw_devlink_flags && !fw_devlink_is_permissive()) {
+	if (fw_devlink_flags && !fw_devlink_is_permissive() && dev->fwnode) {
 		error = device_create_file(dev, &dev_attr_waiting_for_supplier);
 		if (error)
 			goto err_remove_dev_online;
