095c272f55e7a7ec660c5b99897f4391abdf562b
Alexander Aring <aahringo@redhat.com>
2021-01-13 09:25:18 -0500

    net: sock: add sock_set_mark
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1509204
    Upstream Status: linux.git
    Build info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=37457786
    Tested: set skb mark and add a tc classifier to match on it
    Conflicts: At upstream exists more in-kernel socketoptions helper which was migrated from kernel_setsockopt(). This patch is changed in the way that only mark socket option helpers is introduced without introducing all others. DLM still use some kernel_setsockopt() functionality however this will may migrated to in-kernel socket option helpers in due courses.
    
    This patch adds a new socket helper function to set the mark value for a
    kernel socket.
    
    Signed-off-by: Alexander Aring <aahringo@redhat.com>
    Signed-off-by: David Teigland <teigland@redhat.com>
    (cherry picked from commit 84d1c617402e7e67fc95ab2384da8dae7d1b0efe)

--- include/net/sock.h
+++ include/net/sock.h
@@ -2653,6 +2653,7 @@ int sock_bindtoindex(struct sock *sk, int ifindex, bool lock_sk);
 void sock_no_linger(struct sock *sk);
 void sock_set_keepalive(struct sock *sk);
 void sock_set_priority(struct sock *sk, u32 priority);
+void sock_set_mark(struct sock *sk, u32 val);
 void sock_set_reuseaddr(struct sock *sk);
 
 #endif	/* _SOCK_H */
--- net/core/sock.c
+++ net/core/sock.c
@@ -784,6 +784,14 @@ void sock_set_keepalive(struct sock *sk)
 }
 EXPORT_SYMBOL(sock_set_keepalive);
 
+void sock_set_mark(struct sock *sk, u32 val)
+{
+	lock_sock(sk);
+	sk->sk_mark = val;
+	release_sock(sk);
+}
+EXPORT_SYMBOL(sock_set_mark);
+
 /*
  *	This is meant for all protocols to use and covers goings on
  *	at the socket level. Everything here is generic.
