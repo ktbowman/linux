430e41b8d099d667625a7f509c74b2d63d34945b
Mark Langsdorf <mlangsdo@redhat.com>
2021-01-17 22:28:47 +0100

    perf: Constify static struct attribute_group
    Upstream Status: Linus v5.14
    Testing Status: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=40498166
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1997732
    
    commit f0c140481d1b807217cacdcf11d24cfa407a7a53
    Author: Rikard Falkeborn <rikard.falkeborn@gmail.com>
    Date: Sun, 17 Jan 2021 22:28:47 +0100
    
    The only usage is to put their addresses in an array of pointers to
    const struct attribute group. Make them const to allow the compiler
    to put them in read-only memory.
    
    Signed-off-by: Rikard Falkeborn <rikard.falkeborn@gmail.com>
    Link: https://lore.kernel.org/r/20210117212847.21319-5-rikard.falkeborn@gmail.com
    Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit f0c140481d1b807217cacdcf11d24cfa407a7a53)
    
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/perf/arm_spe_pmu.c
+++ drivers/perf/arm_spe_pmu.c
@@ -156,7 +156,7 @@ static struct attribute *arm_spe_pmu_cap_attr[] = {
 	NULL,
 };
 
-static struct attribute_group arm_spe_pmu_cap_group = {
+static const struct attribute_group arm_spe_pmu_cap_group = {
 	.name	= "caps",
 	.attrs	= arm_spe_pmu_cap_attr,
 };
@@ -237,7 +237,7 @@ static struct attribute *arm_spe_pmu_formats_attr[] = {
 	NULL,
 };
 
-static struct attribute_group arm_spe_pmu_format_group = {
+static const struct attribute_group arm_spe_pmu_format_group = {
 	.name	= "format",
 	.attrs	= arm_spe_pmu_formats_attr,
 };
@@ -257,7 +257,7 @@ static struct attribute *arm_spe_pmu_attrs[] = {
 	NULL,
 };
 
-static struct attribute_group arm_spe_pmu_group = {
+static const struct attribute_group arm_spe_pmu_group = {
 	.attrs	= arm_spe_pmu_attrs,
 };
 
