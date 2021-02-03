2c0990342ce6bcf52badf94cb178a72df790f17a
Andreas Gruenbacher <agruenba@redhat.com>
2021-02-03 16:15:27 +0100

    gfs2: Use sb_start_intwrite in gfs2_ail_empty_gl
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1442151
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=38277141
    Tested: xfstests, QE tests
    Upstream Status: upstream
    
    commit 15e20a301ab06575482c7ab3b442a6830cec928e
    Author: Andreas Gruenbacher <agruenba@redhat.com>
    Date:   Wed Feb 3 16:15:27 2021 +0100
    
        gfs2: Use sb_start_intwrite in gfs2_ail_empty_gl
    
        Commit 2e60d7683c8d ("GFS2: update freeze code to use freeze/thaw_super
        on all nodes") optimized away the sb_start_intwrite ... sb_end_intwrite
        protection for the on-stack transactions in gfs2_ail_empty_gl with no
        explanation.  I can't think of a valid reason for doing that, so revert
        that change.  This simplifies the next commit.
    
        Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>
    
    Signed-off-by: Andreas Gruenbacher <agruenba@redhat.com>

--- fs/gfs2/trans.c
+++ fs/gfs2/trans.c
@@ -101,10 +101,9 @@ void gfs2_trans_end(struct gfs2_sbd *sdp)
 
 	if (!test_bit(TR_TOUCHED, &tr->tr_flags)) {
 		gfs2_log_release(sdp, tr->tr_reserved);
-		if (alloced) {
+		if (alloced)
 			gfs2_trans_free(sdp, tr);
-			sb_end_intwrite(sdp->sd_vfs);
-		}
+		sb_end_intwrite(sdp->sd_vfs);
 		return;
 	}
 
@@ -124,8 +123,7 @@ void gfs2_trans_end(struct gfs2_sbd *sdp)
 	if (sdp->sd_vfs->s_flags & SB_SYNCHRONOUS)
 		gfs2_log_flush(sdp, NULL, GFS2_LOG_HEAD_FLUSH_NORMAL |
 			       GFS2_LFC_TRANS_END);
-	if (alloced)
-		sb_end_intwrite(sdp->sd_vfs);
+	sb_end_intwrite(sdp->sd_vfs);
 }
 
 static struct gfs2_bufdata *gfs2_alloc_bufdata(struct gfs2_glock *gl,
