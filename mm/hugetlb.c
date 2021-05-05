d5073703182df0964f11ac3b21593f5545f4df2b
Chris von Recklinghausen <crecklin@redhat.com>
2021-05-04 18:35:20 -0700

    mm,hugetlb: drop clearing of flag from prep_new_huge_page
    Conflicts: mm/hugetlb.c -
    	We don't have db71ef79b59b ("hugetlb: make free_huge_page irq safe")
    	so just delete the call to ClearHPageFreed
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1981661
    
    Pages allocated via the page allocator or CMA get its private field
    cleared by means of post_alloc_hook().
    
    Pages allocated during boot, that is directly from the memblock
    allocator, get cleared by paging_init()-> ..  ->memmap_init_zone-> ..
    ->__init_single_page() before any memblock allocation.
    
    Based on this ground, let us remove the clearing of the flag from
    prep_new_huge_page() as it is not needed.  This was a leftover from
    commit 6c0371490140 ("hugetlb: convert PageHugeFreed to HPageFreed
    flag").
    
    Previously the explicit clearing was necessary because compound
    allocations do not get this initialization (see prep_compound_page).
    
    Link: https://lkml.kernel.org/r/20210419075413.1064-4-osalvador@suse.de
    Signed-off-by: Oscar Salvador <osalvador@suse.de>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Muchun Song <songmuchun@bytedance.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 9f27b34f234da7a185b4f1a2aa2cea2c47c458bf)
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -1313,7 +1313,6 @@ static void prep_new_huge_page(struct hstate *h, struct page *page, int nid)
 	set_hugetlb_cgroup(page, NULL);
 	h->nr_huge_pages++;
 	h->nr_huge_pages_node[nid]++;
-	ClearHPageFreed(page);
 	spin_unlock(&hugetlb_lock);
 }
 
