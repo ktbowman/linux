ea744f6f4bc036b2d6528c8a395f5ac040c38d3a
Mark Langsdorf <mlangsdo@redhat.com>
2021-05-11 20:27:32 +0800

    drivers/perf: arm_pmu: Fix some coding style issues
    Upstream Status: Linus v5.14
    Testing Status: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=40498166
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1997732
    
    Conflicts:
           drivers/perf/arm_pmu.c - first stanza depends on some NMI code not
    yet present in RHEL8, so I skipped it entirely
    
    commit f265fd166bce9837ce1ae6c2a4b56f8bd18d1fe4
    Author: Junhao He <hejunhao2@hisilicon.com>
    Date: Tue, 11 May 2021 20:27:32 +0800
    
    Fix some coding style issues reported by checkpatch.pl, including
    following types:
    
    ERROR: spaces required around that '=' (ctx:VxW)
    WARNING: Possible unnecessary 'out of memory' message
    
    Signed-off-by: Junhao He <hejunhao2@hisilicon.com>
    Signed-off-by: Jay Fang <f.fangjian@huawei.com>
    Link: https://lore.kernel.org/r/1620736054-58412-3-git-send-email-f.fangjian@huawei.com
    Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit f265fd166bce9837ce1ae6c2a4b56f8bd18d1fe4)
    
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/perf/arm_pmu.c
+++ drivers/perf/arm_pmu.c
@@ -783,10 +783,8 @@ static struct arm_pmu *__armpmu_alloc(gfp_t flags)
 	int cpu;
 
 	pmu = kzalloc(sizeof(*pmu), flags);
-	if (!pmu) {
-		pr_info("failed to allocate PMU device!\n");
+	if (!pmu)
 		goto out;
-	}
 
 	pmu->hw_events = alloc_percpu_gfp(struct pmu_hw_events, flags);
 	if (!pmu->hw_events) {
