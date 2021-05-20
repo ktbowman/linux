27cacd6524438b8d6fc81f7b035313d872263478
Ronnie Sahlberg <lsahlber@redhat.com>
2021-05-21 09:14:41 +1000

    cifs: Add missing sentinel to smb3_fs_parameters
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1812436
    commit 427c4f004e173732ab0eb248e6ec89ef79524cd5
    
    Add missing sentinel to smb3_fs_parameters. This fixes ARM32 kernel
    crashing once CIFS is registered.
    
     Unable to handle kernel paging request at virtual address 33626d73
    ...
     (strcmp) from (fs_validate_description)
     (fs_validate_description) from (register_filesystem)
     (register_filesystem) from (init_cifs [cifs])
     (init_cifs [cifs]) from (do_one_initcall)
     (do_one_initcall) from (do_init_module)
     (do_init_module) from (load_module)
     (load_module) from (sys_finit_module)
     (sys_finit_module) from (ret_fast_syscal)
    
        Fixes: e07724d1cf38 ("cifs: switch to new mount api")
        Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
        Reviewed-by: Aurelien Aptel <aaptel@suse.com>
        Signed-off-by: Steve French <stfrench@microsoft.com>
        (cherry picked from commit 427c4f004e173732ab0eb248e6ec89ef79524cd5)
    Signed-off-by: Ronnie Sahlberg <lsahlber@redhat.com>

--- fs/cifs/fs_context.c
+++ fs/cifs/fs_context.c
@@ -172,6 +172,7 @@ const struct fs_parameter_spec smb3_fs_parameters[] = {
 	fsparam_flag_no("mand", Opt_ignore),
 	fsparam_string("cred", Opt_ignore),
 	fsparam_string("credentials", Opt_ignore),
+	{}
 };
 
 int
