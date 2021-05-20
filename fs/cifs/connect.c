6ebc0699c231cc0403fec704a552c244e796795e
Ronnie Sahlberg <lsahlber@redhat.com>
2021-05-21 09:39:31 +1000

    cifs: Set CIFS_MOUNT_USE_PREFIX_PATH flag on setting cifs_sb->prepath.
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1812436
    commit a738c93fb1c17e386a09304b517b1c6b2a6a5a8b
    
    While debugging another issue today, Steve and I noticed that if a
    subdir for a file share is already mounted on the client, any new
    mount of any other subdir (or the file share root) of the same share
    results in sharing the cifs superblock, which e.g. can result in
    incorrect device name.
    
    While setting prefix path for the root of a cifs_sb,
    CIFS_MOUNT_USE_PREFIX_PATH flag should also be set.
    Without it, prepath is not even considered in some places,
    and output of "mount" and various /proc/<>/*mount* related
    options can be missing part of the device name.
    
        Signed-off-by: Shyam Prasad N <sprasad@microsoft.com>
        Reviewed-by: Aurelien Aptel <aaptel@suse.com>
        Signed-off-by: Steve French <stfrench@microsoft.com>
        (cherry picked from commit a738c93fb1c17e386a09304b517b1c6b2a6a5a8b)
    Signed-off-by: Ronnie Sahlberg <lsahlber@redhat.com>

--- fs/cifs/connect.c
+++ fs/cifs/connect.c
@@ -2688,6 +2688,7 @@ int cifs_setup_cifs_sb(struct cifs_sb_info *cifs_sb)
 		cifs_sb->prepath = kstrdup(ctx->prepath, GFP_KERNEL);
 		if (cifs_sb->prepath == NULL)
 			return -ENOMEM;
+		cifs_sb->mnt_cifs_flags |= CIFS_MOUNT_USE_PREFIX_PATH;
 	}
 
 	return 0;
