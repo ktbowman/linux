e76861aa4281e12f8cb081a06e6c760b1ced1d9d
Raju Rangoju <rrangoju@redhat.com>
2021-05-25 04:42:10 -0400

    [netdrv] flow_offload: check for basic action hw stats type
    Bugzilla: http://bugzilla.redhat.com/1955208
    
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=36971749
    
    Tested: Successfully tested by Chelsio with a brewbuild provided
    
    Conflicts:
     - This commit was previously backported. Taking just the cxgb4 hunks.
    
    commit 319a1d19471ec49b8a91a7f6a3fe2c4535e5c279
    Author: Jiri Pirko <jiri@mellanox.com>
    Date:   Sat Mar 7 12:40:13 2020 +0100
    
        flow_offload: check for basic action hw stats type
    
        Introduce flow_action_basic_hw_stats_types_check() helper and use it
        in drivers. That sanitizes the drivers which do not have support
        for action HW stats types.
    
        Signed-off-by: Jiri Pirko <jiri@mellanox.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Raju Rangoju <rrangoju@redhat.com>

--- drivers/net/ethernet/chelsio/cxgb4/cxgb4_tc_flower.c
+++ drivers/net/ethernet/chelsio/cxgb4/cxgb4_tc_flower.c
@@ -544,13 +544,17 @@ static bool valid_pedit_action(struct net_device *dev,
 }
 
 int cxgb4_validate_flow_actions(struct net_device *dev,
-				struct flow_action *actions)
+				struct flow_action *actions,
+				struct netlink_ext_ack *extack)
 {
 	struct flow_action_entry *act;
 	bool act_redir = false;
 	bool act_pedit = false;
 	bool act_vlan = false;
 	int i;
+
+	if (!flow_action_basic_hw_stats_types_check(actions, extack))
+		return -EOPNOTSUPP;
 
 	flow_action_for_each(i, act, actions) {
 		switch (act->id) {
@@ -701,7 +705,7 @@ int cxgb4_tc_flower_replace(struct net_device *dev,
 	u8 inet_family;
 	int fidx, ret;
 
-	if (cxgb4_validate_flow_actions(dev, &rule->action))
+	if (cxgb4_validate_flow_actions(dev, &rule->action, extack))
 		return -EOPNOTSUPP;
 
 	if (cxgb4_validate_flow_match(dev, cls))
