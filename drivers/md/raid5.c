33790ab870215ed541cea6a6a92f0191634107d3
Nigel Croxon <ncroxon@redhat.com>
2021-05-25 17:46:17 +0800

    md: add io accounting for raid0 and raid5
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1985919
    Upstream Status: kernel.org Linus tree
    Tested: Problem has been fixed, iostat now reports good data.
    
    Merge conflict
    Block folks did not pick up the following for RHEL8
    commit 21cf866145047f8bfecb38ec8d2fed64464c074f
    writeback: remove bdi->congested_fn
    
    md: add io accounting for raid0 and raid5
    
    We introduce a new bioset (io_acct_set) for raid0 and raid5 since they
    don't own clone infrastructure to accounting io. And the bioset is added
    to mddev instead of to raid0 and raid5 layer, because with this way, we
    can put common functions to md.h and reuse them in raid0 and raid5.
    
    Also struct md_io_acct is added accordingly which includes io start_time,
    the origin bio and cloned bio. Then we can call bio_{start,end}_io_acct
    to get related io status.
    
    Signed-off-by: Guoqing Jiang <jiangguoqing@kylinos.cn>
    Signed-off-by: Song Liu <song@kernel.org>
    (cherry picked from commit 10764815ff4728d2c57da677cd5d3dd6f446cf5f)
    Signed-off-by: Nigel Croxon <ncroxon@redhat.com>

--- drivers/md/raid5.c
+++ drivers/md/raid5.c
@@ -5495,6 +5495,7 @@ static struct bio *chunk_aligned_read(struct mddev *mddev, struct bio *raid_bio)
 	sector_t sector = raid_bio->bi_iter.bi_sector;
 	unsigned chunk_sects = mddev->chunk_sectors;
 	unsigned sectors = chunk_sects - (sector & (chunk_sects-1));
+	struct r5conf *conf = mddev->private;
 
 	if (sectors < bio_sectors(raid_bio)) {
 		struct r5conf *conf = mddev->private;
@@ -5503,6 +5504,9 @@ static struct bio *chunk_aligned_read(struct mddev *mddev, struct bio *raid_bio)
 		generic_make_request(raid_bio);
 		raid_bio = split;
 	}
+
+	if (raid_bio->bi_pool != &conf->bio_split)
+		md_account_bio(mddev, &raid_bio);
 
 	if (!raid5_read_one_chunk(mddev, raid_bio))
 		return raid_bio;
@@ -5783,6 +5787,7 @@ static bool raid5_make_request(struct mddev *mddev, struct bio * bi)
 	DEFINE_WAIT(w);
 	bool do_prepare;
 	bool do_flush = false;
+	bool do_clone = false;
 
 	if (unlikely(bi->bi_opf & REQ_PREFLUSH)) {
 		int ret = log_handle_flush_request(conf, bi);
@@ -5811,6 +5816,7 @@ static bool raid5_make_request(struct mddev *mddev, struct bio * bi)
 	if (rw == READ && mddev->degraded == 0 &&
 	    mddev->reshape_position == MaxSector) {
 		bi = chunk_aligned_read(mddev, bi);
+		do_clone = true;
 		if (!bi)
 			return true;
 	}
@@ -5824,6 +5830,9 @@ static bool raid5_make_request(struct mddev *mddev, struct bio * bi)
 	logical_sector = bi->bi_iter.bi_sector & ~((sector_t)RAID5_STRIPE_SECTORS(conf)-1);
 	last_sector = bio_end_sector(bi);
 	bi->bi_next = NULL;
+
+	if (!do_clone)
+		md_account_bio(mddev, &bi);
 
 	prepare_to_wait(&conf->wait_for_overlap, &w, TASK_UNINTERRUPTIBLE);
 	for (; logical_sector < last_sector; logical_sector += RAID5_STRIPE_SECTORS(conf)) {
