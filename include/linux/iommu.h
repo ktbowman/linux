df8e7a6e51df3da60ce4534b9a56fbb4d2be0786
Jerry Snitselaar <jsnitsel@redhat.com>
2021-05-25 23:34:15 -0700

    iommu: Add iova and size as parameters in iotlb_sync_map
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1955767
    Upstream Status: kernel/git/torvalds/linux.git
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=37184688
    Conflicts: RH_KABI_REPLACE was used to add iotlb_sync_map to iommu_ops
               in a previous backport, so there will be a conflict in iommu.h.
               tegra-gart not built in rhel8.
    
    commit 2ebbd25873cef06f739489fd8ff9f707a3dfa2fa
    Author: Yong Wu <yong.wu@mediatek.com>
    Date:   Thu Jan 7 20:29:04 2021 +0800
    
        iommu: Add iova and size as parameters in iotlb_sync_map
    
        iotlb_sync_map allow IOMMU drivers tlb sync after completing the whole
        mapping. This patch adds iova and size as the parameters in it. then the
        IOMMU driver could flush tlb with the whole range once after iova mapping
        to improve performance.
    
        Signed-off-by: Yong Wu <yong.wu@mediatek.com>
        Reviewed-by: Robin Murphy <robin.murphy@arm.com>
        Acked-by: Will Deacon <will@kernel.org>
        Link: https://lore.kernel.org/r/20210107122909.16317-3-yong.wu@mediatek.com
        Signed-off-by: Will Deacon <will@kernel.org>
    
    (cherry picked from commit 2ebbd25873cef06f739489fd8ff9f707a3dfa2fa)
    Cc: Myron Stowe <myron.stowe@redhat.com>
    Cc: Alex Williamson <alex.williamson@redhat.com>
    Cc: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -270,7 +270,8 @@ struct iommu_ops {
 	void (*flush_iotlb_all)(struct iommu_domain *domain);
 	RH_KABI_BROKEN_REPLACE(void (*iotlb_range_add)(struct iommu_domain *domain,\
 						       unsigned long iova, size_t size), \
-			       void (*iotlb_sync_map)(struct iommu_domain *domain))
+			       void (*iotlb_sync_map)(struct iommu_domain *domain, unsigned long iova,\
+				       size_t size))
 	RH_KABI_BROKEN_REPLACE(void (*iotlb_sync)(struct iommu_domain *domain),\
 			       void (*iotlb_sync)(struct iommu_domain *domain, \
 						  struct iommu_iotlb_gather *iotlb_gather))
