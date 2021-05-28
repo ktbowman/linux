8cd90f4566b2f357995eaf299c6bd35484d90508
Raju Rangoju <rrangoju@redhat.com>
2021-05-28 11:30:48 -0400

    crypto: chelsio - Fix some pr_xxx messages
    Bugzilla: http://bugzilla.redhat.com/1961368
    
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=37650924
    
    Tested: Sanity tests ran over x86_64 systems by Chelsio with a brewbuild provided
    
    commit 1b3eeb8761f053422a4b3bcb024fc3bf41e12304
    Author: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Date:   Sun Jul 12 23:14:24 2020 +0200
    
        crypto: chelsio - Fix some pr_xxx messages
    
        At the top this file, we have:
           #define pr_fmt(fmt) "chcr:" fmt
    
        So there is no need to repeat "chcr : " in some error message when the
        pr_xxx macro is used.
        This would lead to log "chcr:chcr : blabla"
    
        Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
        Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    
    Signed-off-by: Raju Rangoju <rrangoju@redhat.com>

--- drivers/crypto/chelsio/chcr_algo.c
+++ drivers/crypto/chelsio/chcr_algo.c
@@ -1223,7 +1223,7 @@ static int chcr_handle_cipher_resp(struct skcipher_request *req,
 	wrparam.bytes = bytes;
 	skb = create_cipher_wr(&wrparam);
 	if (IS_ERR(skb)) {
-		pr_err("chcr : %s : Failed to form WR. No memory\n", __func__);
+		pr_err("%s : Failed to form WR. No memory\n", __func__);
 		err = PTR_ERR(skb);
 		goto unmap;
 	}
@@ -1569,7 +1569,7 @@ static int get_alg_config(struct algo_param *params,
 		params->result_size = SHA512_DIGEST_SIZE;
 		break;
 	default:
-		pr_err("chcr : ERROR, unsupported digest size\n");
+		pr_err("ERROR, unsupported digest size\n");
 		return -EINVAL;
 	}
 	return 0;
@@ -3590,7 +3590,7 @@ static int chcr_authenc_setkey(struct crypto_aead *authenc, const u8 *key,
 		goto out;
 
 	if (get_alg_config(&param, max_authsize)) {
-		pr_err("chcr : Unsupported digest size\n");
+		pr_err("Unsupported digest size\n");
 		goto out;
 	}
 	subtype = get_aead_subtype(authenc);
@@ -3609,7 +3609,7 @@ static int chcr_authenc_setkey(struct crypto_aead *authenc, const u8 *key,
 	} else if (keys.enckeylen == AES_KEYSIZE_256) {
 		ck_size = CHCR_KEYCTX_CIPHER_KEY_SIZE_256;
 	} else {
-		pr_err("chcr : Unsupported cipher key\n");
+		pr_err("Unsupported cipher key\n");
 		goto out;
 	}
 
@@ -3627,7 +3627,7 @@ static int chcr_authenc_setkey(struct crypto_aead *authenc, const u8 *key,
 	}
 	base_hash  = chcr_alloc_shash(max_authsize);
 	if (IS_ERR(base_hash)) {
-		pr_err("chcr : Base driver cannot be loaded\n");
+		pr_err("Base driver cannot be loaded\n");
 		goto out;
 	}
 	{
@@ -3644,7 +3644,7 @@ static int chcr_authenc_setkey(struct crypto_aead *authenc, const u8 *key,
 						  keys.authkeylen,
 						  o_ptr);
 			if (err) {
-				pr_err("chcr : Base driver cannot be loaded\n");
+				pr_err("Base driver cannot be loaded\n");
 				goto out;
 			}
 			keys.authkeylen = max_authsize;
@@ -3732,7 +3732,7 @@ static int chcr_aead_digest_null_setkey(struct crypto_aead *authenc,
 	} else if (keys.enckeylen == AES_KEYSIZE_256) {
 		ck_size = CHCR_KEYCTX_CIPHER_KEY_SIZE_256;
 	} else {
-		pr_err("chcr : Unsupported cipher key %d\n", keys.enckeylen);
+		pr_err("Unsupported cipher key %d\n", keys.enckeylen);
 		goto out;
 	}
 	memcpy(aeadctx->key, keys.enckey, keys.enckeylen);
@@ -3768,7 +3768,7 @@ static int chcr_aead_op(struct aead_request *req,
 
 	cdev = a_ctx(tfm)->dev;
 	if (!cdev) {
-		pr_err("chcr : %s : No crypto device.\n", __func__);
+		pr_err("%s : No crypto device.\n", __func__);
 		return -ENXIO;
 	}
 
@@ -4516,8 +4516,7 @@ static int chcr_register_alg(void)
 			break;
 		}
 		if (err) {
-			pr_err("chcr : %s : Algorithm registration failed\n",
-			       name);
+			pr_err("%s : Algorithm registration failed\n", name);
 			goto register_err;
 		} else {
 			driver_algs[i].is_registered = 1;
