cddcd3e09a925340f7bcc22f6d3154fda8c34499
Lyude Paul <lyude@redhat.com>
2021-06-01 19:04:17 -0400

    drm/i915: Sprinkle a few missing locks around shared DDI clock registers
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1944404
    Upstream Status: v5.13-rc1
    
    commit be317ca0a338ab3976d7c74975458638d0cedf90
    Author:     Ville Syrjälä <ville.syrjala@linux.intel.com>
    AuthorDate: Fri Feb  5 23:46:28 2021 +0200
    Commit:     Ville Syrjälä <ville.syrjala@linux.intel.com>
    CommitDate: Tue Feb 16 14:35:33 2021 +0200
    
        The current code attempts to protect the RMWs into global
        clock routing registers with a mutex, but forgets to do so
        in a few places. Let's remedy that.
    
        Note that at the moment we serialize all modesets onto single
        wq, so this shouldn't actually matter. But maybe one day we
        wish to attempt parallel modesets again...
    
        Reviewed-by: Lucas De Marchi <lucas.demarchi@intel.com>
        Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
        Link: https://patchwork.freedesktop.org/patch/msgid/20210205214634.19341-10-ville.syrjala@linux.intel.com
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>

--- drivers/gpu/drm/i915/display/intel_ddi.c
+++ drivers/gpu/drm/i915/display/intel_ddi.c
@@ -3456,8 +3456,12 @@ static void cnl_ddi_disable_clock(struct intel_encoder *encoder)
 	struct drm_i915_private *i915 = to_i915(encoder->base.dev);
 	enum port port = encoder->port;
 
+	mutex_lock(&i915->dpll.lock);
+
 	intel_de_rmw(i915, DPCLKA_CFGCR0,
 		     0, DPCLKA_CFGCR0_DDI_CLK_OFF(port));
+
+	mutex_unlock(&i915->dpll.lock);
 }
 
 static void skl_ddi_enable_clock(struct intel_encoder *encoder,
@@ -3486,8 +3490,12 @@ static void skl_ddi_disable_clock(struct intel_encoder *encoder)
 	struct drm_i915_private *i915 = to_i915(encoder->base.dev);
 	enum port port = encoder->port;
 
+	mutex_lock(&i915->dpll.lock);
+
 	intel_de_rmw(i915, DPLL_CTRL2,
 		     0, DPLL_CTRL2_DDI_CLK_OFF(port));
+
+	mutex_unlock(&i915->dpll.lock);
 }
 
 void hsw_ddi_enable_clock(struct intel_encoder *encoder,
