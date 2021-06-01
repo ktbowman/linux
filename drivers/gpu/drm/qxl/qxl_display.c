bee4d90afaa51718c5412b3f064db18f5cbb8e12
Lyude Paul <lyude@redhat.com>
2021-06-01 19:04:23 -0400

    drm/qxl: use drmm_mode_config_init
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1944404
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1907341
    Upstream Status: v5.13-rc1
    
    commit f8752f14c964e9b7e160f491e4b0a0377914e18f
    Author:     Gerd Hoffmann <kraxel@redhat.com>
    AuthorDate: Thu Feb  4 15:57:04 2021 +0100
    Commit:     Gerd Hoffmann <kraxel@redhat.com>
    CommitDate: Fri Feb  5 08:52:21 2021 +0100
    
        Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
        Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
        Acked-by: Thomas Zimmermann <tzimmermann@suse.de>
        Link: http://patchwork.freedesktop.org/patch/msgid/20210204145712.1531203-4-kraxel@redhat.com
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>

--- drivers/gpu/drm/qxl/qxl_display.c
+++ drivers/gpu/drm/qxl/qxl_display.c
@@ -1196,7 +1196,9 @@ int qxl_modeset_init(struct qxl_device *qdev)
 	int i;
 	int ret;
 
-	drm_mode_config_init(&qdev->ddev);
+	ret = drmm_mode_config_init(&qdev->ddev);
+	if (ret)
+		return ret;
 
 	ret = qxl_create_monitors_object(qdev);
 	if (ret)
@@ -1233,5 +1235,4 @@ void qxl_modeset_fini(struct qxl_device *qdev)
 		qdev->dumb_shadow_bo = NULL;
 	}
 	qxl_destroy_monitors_object(qdev);
-	drm_mode_config_cleanup(&qdev->ddev);
 }
