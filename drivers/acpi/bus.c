56f7265820436c1a1222dbec8434e158ac447a7f
Mark Langsdorf <mlangsdo@redhat.com>
2021-06-02 17:36:49 +0800

    ACPI: bus: Remove unneeded assignment
    Upstream Status: Linus v5.14
    Testing Status: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=39199451
        s390x storage blktests failed, but that failed on a default build
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1997696
    Conflicts:
    	drivers/acpi/bus.c - fixup error because RHEL8 doesn't always
    use pr_debug but the result was obvious
    
    commit 9b64560134a0032d2de6bb565a76418ad90386fe
    Author: Hanjun Guo <guohanjun@huawei.com>
    Date: Wed, 2 Jun 2021 17:36:49 +0800
    
    When acpi_kobj is NULL already, assigning NULL to it is redundant,
    so don't do that.
    
    Signed-off-by: Hanjun Guo <guohanjun@huawei.com>
    [ rjw: Subject and changelog edits ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 9b64560134a0032d2de6bb565a76418ad90386fe)
    
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- drivers/acpi/bus.c
+++ drivers/acpi/bus.c
@@ -1335,10 +1335,8 @@ static int __init acpi_init(void)
 	}
 
 	acpi_kobj = kobject_create_and_add("acpi", firmware_kobj);
-	if (!acpi_kobj) {
+	if (!acpi_kobj)
 		printk(KERN_WARNING "%s: kset create error\n", __func__);
-		acpi_kobj = NULL;
-	}
 
 	result = acpi_bus_init();
 	if (result) {
