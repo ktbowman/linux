07c89fab41af6c0d22189b42130f052670f0a7b8
Jeremy Linton <jlinton@redhat.com>
2021-06-03 14:11:33 -0500

    arm64: tlbi: Set MAX_TLBI_OPS to PTRS_PER_PTE
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1861872
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=37008894
    Upstream: mainline
    
    commit 3d65b6bbc01ecece8142e62a8a5f1d48ba41a240
    Author: Will Deacon <will@kernel.org>
    Date:   Mon Nov 19 18:08:49 2018 +0000
    
    In order to reduce the possibility of soft lock-ups, we bound the
    maximum number of TLBI operations performed by a single call to
    flush_tlb_range() to an arbitrary constant of 1024.
    
    Whilst this does the job of avoiding lock-ups, we can actually be a bit
    smarter by defining this as PTRS_PER_PTE. Due to the structure of our
    page tables, using PTRS_PER_PTE means that an outer loop calling
    flush_tlb_range() for entire table entries will end up performing just a
    single TLBI operation for each entry. As an example, mremap()ing a 1GB
    range mapped using 4k pages now requires only 512 TLBI operations when
    moving the page tables as opposed to 262144 operations (512*512) when
    using the current threshold of 1024.
    
    Cc: Joel Fernandes <joel@joelfernandes.org>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Jeremy Linton <jlinton@redhat.com>

--- arch/arm64/include/asm/tlbflush.h
+++ arch/arm64/include/asm/tlbflush.h
@@ -250,7 +250,7 @@ static inline void flush_tlb_page(struct vm_area_struct *vma,
  * This is meant to avoid soft lock-ups on large TLB flushing ranges and not
  * necessarily a performance improvement.
  */
-#define MAX_TLBI_OPS	1024UL
+#define MAX_TLBI_OPS	PTRS_PER_PTE
 
 static inline void __flush_tlb_range(struct vm_area_struct *vma,
 				     unsigned long start, unsigned long end,
@@ -261,7 +261,7 @@ static inline void __flush_tlb_range(struct vm_area_struct *vma,
 	unsigned long addr;
 	enum tlb_flush_types flush;
 
-	if ((end - start) > (MAX_TLBI_OPS * stride)) {
+	if ((end - start) >= (MAX_TLBI_OPS * stride)) {
 		flush_tlb_mm(mm);
 		return;
 	}
