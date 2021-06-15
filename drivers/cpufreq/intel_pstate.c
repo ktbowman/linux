da2c0d5a90af11e0e200b7dabbbaba8b4a85d06c
David Arcari <darcari@redhat.com>
2021-06-15 10:48:59 -0400

    cpufreq: intel_pstate: Fix active mode setting from command line
    Bugzilla: http://bugzilla.redhat.com/1973225
    Conflicts: some differences due to out of order commits. RHEL and
    	   upstream compare favorably after the commit is applied.
    
    commit 39a188b88332545073b8e07633f5e3298e066b61
    Author: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Date:   Mon Jul 13 15:58:38 2020 +0200
    
        cpufreq: intel_pstate: Fix active mode setting from command line
    
        If intel_pstate starts in the passive mode by default (that happens
        when the processor in the system doesn't support HWP), passing
        intel_pstate=active in the kernel command line doesn't work, so
        fix that.
    
        Fixes: 33aa46f252c7 ("cpufreq: intel_pstate: Use passive mode by default without HWP")
        Reported-by: Doug Smythies <dsmythies@telus.net>
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
        Acked-by: Doug Smythies <dsmythies@telus.net>
    
    Signed-off-by: David Arcari <darcari@redhat.com>

--- drivers/cpufreq/intel_pstate.c
+++ drivers/cpufreq/intel_pstate.c
@@ -2761,7 +2761,7 @@ static struct cpufreq_driver intel_cpufreq = {
 	.name		= "intel_cpufreq",
 };
 
-static struct cpufreq_driver *default_driver = &intel_pstate;
+static struct cpufreq_driver *default_driver;
 
 static void intel_pstate_driver_cleanup(void)
 {
@@ -3091,7 +3091,8 @@ static int __init intel_pstate_init(void)
 		return -ENODEV;
 	}
 	/* Without HWP start in the passive mode. */
-	default_driver = &intel_cpufreq;
+	if (!default_driver)
+		default_driver = &intel_cpufreq;
 
 hwp_cpu_matched:
 	/*
@@ -3147,6 +3148,8 @@ static int __init intel_pstate_setup(char *str)
 
 	if (!strcmp(str, "disable"))
 		no_load = 1;
+	else if (!strcmp(str, "active"))
+		default_driver = &intel_pstate;
 	else if (!strcmp(str, "passive"))
 		default_driver = &intel_cpufreq;
 
