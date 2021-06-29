3a82ef79df6032f06db08844f3e1683c940d80dc
Alaa Hleihel <ahleihel@redhat.com>
2021-06-29 03:59:25 -0400

    net/mlx5e: Enable napi in channel's activation stage
    Bugzilla: http://bugzilla.redhat.com/1915308
    Upstream: v5.12-rc1
    
    commit 7637e499e219ae89e5c42d947b96603ef3ab5a44
    Author: Tariq Toukan <tariqt@mellanox.com>
    Date:   Mon Dec 30 14:41:53 2019 +0200
    
        net/mlx5e: Enable napi in channel's activation stage
    
        The channel's napi is first needed upon activation, not creation.
        Minimize its enabled scope by moving it from the channel's open/close
        stage into the activate/deactivate stage.
    
        Signed-off-by: Tariq Toukan <tariqt@mellanox.com>
        Reviewed-by: Maxim Mikityanskiy <maximmi@mellanox.com>
        Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    
    Signed-off-by: Alaa Hleihel <ahleihel@redhat.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -1893,13 +1893,11 @@ static int mlx5e_open_queues(struct mlx5e_channel *c,
 	if (err)
 		goto err_close_rx_cq;
 
-	napi_enable(&c->napi);
-
 	spin_lock_init(&c->async_icosq_lock);
 
 	err = mlx5e_open_icosq(c, params, &cparam->async_icosq, &c->async_icosq);
 	if (err)
-		goto err_disable_napi;
+		goto err_close_xdpsq_cq;
 
 	err = mlx5e_open_icosq(c, params, &cparam->icosq, &c->icosq);
 	if (err)
@@ -1942,9 +1940,7 @@ err_close_icosq:
 err_close_async_icosq:
 	mlx5e_close_icosq(&c->async_icosq);
 
-err_disable_napi:
-	napi_disable(&c->napi);
-
+err_close_xdpsq_cq:
 	if (c->xdp)
 		mlx5e_close_cq(&c->rq_xdpsq.cq);
 
@@ -1975,7 +1971,6 @@ static void mlx5e_close_queues(struct mlx5e_channel *c)
 	mlx5e_close_sqs(c);
 	mlx5e_close_icosq(&c->icosq);
 	mlx5e_close_icosq(&c->async_icosq);
-	napi_disable(&c->napi);
 	if (c->xdp)
 		mlx5e_close_cq(&c->rq_xdpsq.cq);
 	mlx5e_close_cq(&c->rq.cq);
@@ -2060,6 +2055,8 @@ static void mlx5e_activate_channel(struct mlx5e_channel *c)
 {
 	int tc;
 
+	napi_enable(&c->napi);
+
 	for (tc = 0; tc < c->num_tc; tc++)
 		mlx5e_activate_txqsq(&c->sq[tc]);
 	mlx5e_activate_icosq(&c->icosq);
@@ -2082,8 +2079,9 @@ static void mlx5e_deactivate_channel(struct mlx5e_channel *c)
 	mlx5e_deactivate_icosq(&c->icosq);
 	for (tc = 0; tc < c->num_tc; tc++)
 		mlx5e_deactivate_txqsq(&c->sq[tc]);
-
 	mlx5e_qos_deactivate_queues(c);
+
+	napi_disable(&c->napi);
 }
 
 static void mlx5e_close_channel(struct mlx5e_channel *c)
