44dbb8e03b65f7c1df1342c828c0fb09b6609965
Waiman Long <longman@redhat.com>
2021-06-29 21:51:47 -0400

    workqueue: more destroy_workqueue() fixes
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1977537
    Conflicts: There is a fuzz due to the presence of the obsolete
    	   __alloc_workqueue_key() function which was not removed in RHEL
    	   commit 359276a06fdf ("[kernel] workqueue: Use dynamic lockdep
    	   keys for workqueue") for maintaining kABI. An additional hunk
    	   from upstream commit e66b39af00f4 ("workqueue: Fix pwq ref
    	   leak in rescuer_thread()") is also included as that hunk was
    	   left out in RHEL commit aea9af7b4ae0 ("[kernel] workqueue: Fix
    	   pwq ref leak in rescuer_thread()") due to missing this patch.
    
    commit c29eb85386880750130a01aabf288408a6614d65
    Author: Tejun Heo <tj@kernel.org>
    Date:   Mon, 23 Sep 2019 11:08:58 -0700
    
        workqueue: more destroy_workqueue() fixes
    
        destroy_workqueue() warnings still, at a lower frequency, trigger
        spuriously.  The problem seems to be in-flight operations which
        haven't reached put_pwq() yet.
    
        * Make sanity check grab all the related locks so that it's
          synchronized against operations which puts pwq at the end.
    
        * Always print out the offending pwq.
    
        Signed-off-by: Tejun Heo <tj@kernel.org>
        Cc: "Williams, Gerald S" <gerald.s.williams@intel.com>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -354,6 +354,7 @@ EXPORT_SYMBOL_GPL(system_freezable_power_efficient_wq);
 
 static int worker_thread(void *__worker);
 static void workqueue_sysfs_unregister(struct workqueue_struct *wq);
+static void show_pwq(struct pool_workqueue *pwq);
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/workqueue.h>
@@ -4322,6 +4323,22 @@ struct workqueue_struct *__alloc_workqueue_key(const char *fmt,
 }
 EXPORT_SYMBOL_GPL(__alloc_workqueue_key);
 
+static bool pwq_busy(struct pool_workqueue *pwq)
+{
+	int i;
+
+	for (i = 0; i < WORK_NR_COLORS; i++)
+		if (pwq->nr_in_flight[i])
+			return true;
+
+	if ((pwq != pwq->wq->dfl_pwq) && (pwq->refcnt > 1))
+		return true;
+	if (pwq->nr_active || !list_empty(&pwq->delayed_works))
+		return true;
+
+	return false;
+}
+
 /**
  * destroy_workqueue - safely terminate a workqueue
  * @wq: target workqueue
@@ -4356,28 +4373,28 @@ void destroy_workqueue(struct workqueue_struct *wq)
 		kfree(rescuer);
 	}
 
-	/* sanity checks */
+	/*
+	 * Sanity checks - grab all the locks so that we wait for all
+	 * in-flight operations which may do put_pwq().
+	 */
+	mutex_lock(&wq_pool_mutex);
 	mutex_lock(&wq->mutex);
 	for_each_pwq(pwq, wq) {
-		int i;
-
-		for (i = 0; i < WORK_NR_COLORS; i++) {
-			if (WARN_ON(pwq->nr_in_flight[i])) {
-				mutex_unlock(&wq->mutex);
-				show_workqueue_state();
-				return;
-			}
-		}
-
-		if (WARN_ON((pwq != wq->dfl_pwq) && (pwq->refcnt > 1)) ||
-		    WARN_ON(pwq->nr_active) ||
-		    WARN_ON(!list_empty(&pwq->delayed_works))) {
+		spin_lock_irq(&pwq->pool->lock);
+		if (WARN_ON(pwq_busy(pwq))) {
+			pr_warning("%s: %s has the following busy pwq\n",
+				   __func__, wq->name);
+			show_pwq(pwq);
+			spin_unlock_irq(&pwq->pool->lock);
 			mutex_unlock(&wq->mutex);
+			mutex_unlock(&wq_pool_mutex);
 			show_workqueue_state();
 			return;
 		}
+		spin_unlock_irq(&pwq->pool->lock);
 	}
 	mutex_unlock(&wq->mutex);
+	mutex_unlock(&wq_pool_mutex);
 
 	/*
 	 * wq list is used to freeze wq, remove from list after
