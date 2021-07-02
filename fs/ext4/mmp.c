38e76af28c4e5c6201fb85e9430658ff304f08d1
Lukas Czerner <lczerner@redhat.com>
2021-07-02 12:45:02 -0400

    ext4: fix possible UAF when remounting r/o a mmp-protected file system
    Bugzilla: https://bugzilla.redhat.com/2046355
    Tested: xfstests
    Upstream Status: upstream
    
    commit 61bb4a1c417e5b95d9edb4f887f131de32e419cb
    Author: Theodore Ts'o <tytso@mit.edu>
        
        After commit 618f003199c6 ("ext4: fix memory leak in
        ext4_fill_super"), after the file system is remounted read-only, there
        is a race where the kmmpd thread can exit, causing sbi->s_mmp_tsk to
        point at freed memory, which the call to ext4_stop_mmpd() can trip
        over.
        
        Fix this by only allowing kmmpd() to exit when it is stopped via
        ext4_stop_mmpd().
        
        Link: https://lore.kernel.org/r/20210707002433.3719773-1-tytso@mit.edu
        Reported-by: Ye Bin <yebin10@huawei.com>
        Bug-Report-Link: <20210629143603.2166962-1-yebin10@huawei.com>
        Signed-off-by: Theodore Ts'o <tytso@mit.edu>
        Reviewed-by: Jan Kara <jack@suse.cz>
    
    Signed-off-by: Lukas Czerner <lczerner@redhat.com>

--- fs/ext4/mmp.c
+++ fs/ext4/mmp.c
@@ -160,7 +160,12 @@ static int kmmpd(void *data)
 	memcpy(mmp->mmp_nodename, init_utsname()->nodename,
 	       sizeof(mmp->mmp_nodename));
 
-	while (!kthread_should_stop()) {
+	while (!kthread_should_stop() && !sb_rdonly(sb)) {
+		if (!ext4_has_feature_mmp(sb)) {
+			ext4_warning(sb, "kmmpd being stopped since MMP feature"
+				     " has been disabled.");
+			goto wait_to_exit;
+		}
 		if (++seq > EXT4_MMP_SEQ_MAX)
 			seq = 1;
 
@@ -180,16 +185,6 @@ static int kmmpd(void *data)
 			}
 			failed_writes++;
 		}
-
-		if (!(le32_to_cpu(es->s_feature_incompat) &
-		    EXT4_FEATURE_INCOMPAT_MMP)) {
-			ext4_warning(sb, "kmmpd being stopped since MMP feature"
-				     " has been disabled.");
-			goto exit_thread;
-		}
-
-		if (sb_rdonly(sb))
-			break;
 
 		diff = jiffies - last_update_time;
 		if (diff < mmp_update_interval * HZ)
@@ -211,7 +206,7 @@ static int kmmpd(void *data)
 				ext4_error_err(sb, -retval,
 					       "error reading MMP data: %d",
 					       retval);
-				goto exit_thread;
+				goto wait_to_exit;
 			}
 
 			mmp_check = (struct mmp_struct *)(bh_check->b_data);
@@ -225,7 +220,7 @@ static int kmmpd(void *data)
 				ext4_error_err(sb, EBUSY, "abort");
 				put_bh(bh_check);
 				retval = -EBUSY;
-				goto exit_thread;
+				goto wait_to_exit;
 			}
 			put_bh(bh_check);
 		}
@@ -248,7 +243,13 @@ static int kmmpd(void *data)
 
 	retval = write_mmp_block(sb, bh);
 
-exit_thread:
+wait_to_exit:
+	while (!kthread_should_stop()) {
+		set_current_state(TASK_INTERRUPTIBLE);
+		if (!kthread_should_stop())
+			schedule();
+	}
+	set_current_state(TASK_RUNNING);
 	return retval;
 }
 
@@ -395,5 +396,3 @@ failed:
 	brelse(bh);
 	return 1;
 }
-
-
