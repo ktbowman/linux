80ae34a01321629cc418bdd7a9b9081a09d9ef8e
Jiri Benc <jbenc@redhat.com>
2021-07-02 12:31:07 +0200

    bpf: Adds support for setting window clamp
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1882711
    
    Conflicts:
    - [minor] tcp_set_window_clamp: context difference due to missing
      480aeb9639d6 ("tcp: add tcp_sock_set_keepcnt")
    - [minor] tcp.h: context difference due to out of order backport of
      892bfd3ded0e ("tcp: export timestamp helpers for mptcp")
    
    commit cb81110997d1f5097f29dd8e49d32a1fc55cbf86
    Author: Prankur gupta <prankgup@fb.com>
    Date:   Wed Dec 2 13:31:51 2020 -0800
    
        bpf: Adds support for setting window clamp
    
        Adds a new bpf_setsockopt for TCP sockets, TCP_BPF_WINDOW_CLAMP,
        which sets the maximum receiver window size. It will be useful for
        limiting receiver window based on RTT.
    
        Signed-off-by: Prankur gupta <prankgup@fb.com>
        Signed-off-by: Alexei Starovoitov <ast@kernel.org>
        Link: https://lore.kernel.org/bpf/20201202213152.435886-2-prankgup@fb.com
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -418,6 +418,7 @@ int tcp_recvmsg(struct sock *sk, struct msghdr *msg, size_t len, int nonblock,
 int tcp_set_rcvlowat(struct sock *sk, int val);
 
 struct scm_timestamping;
+int tcp_set_window_clamp(struct sock *sk, int val);
 void tcp_update_recv_tstamps(struct sk_buff *skb,
 			     struct scm_timestamping *tss);
 void tcp_recv_timestamp(struct msghdr *msg, const struct sock *sk,
