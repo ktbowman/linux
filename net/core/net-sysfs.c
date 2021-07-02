3b36ee476a3ddb3a203c3278bcb8eac872fea2b7
Jiri Benc <jbenc@redhat.com>
2021-07-02 12:31:04 +0200

    net: napi: use READ_ONCE()/WRITE_ONCE()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1882711
    
    Conflicts:
    - napi_complete_done: list_empty instead of gro_bitmask due to missing
      07d78363dcff ("net: Convert NAPI gro list into a small hash table.") and
      d9f37d01e294 ("net: convert gro_count to bitmask")
    
    commit 7e417a66b86c110f4b282945dac82e21e0b08328
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Wed Apr 22 09:13:28 2020 -0700
    
        net: napi: use READ_ONCE()/WRITE_ONCE()
    
        gro_flush_timeout and napi_defer_hard_irqs can be read
        from napi_complete_done() while other cpus write the value,
        whithout explicit synchronization.
    
        Use READ_ONCE()/WRITE_ONCE() to annotate the races.
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>

--- net/core/net-sysfs.c
+++ net/core/net-sysfs.c
@@ -372,7 +372,7 @@ NETDEVICE_SHOW_RW(tx_queue_len, fmt_dec);
 
 static int change_gro_flush_timeout(struct net_device *dev, unsigned long val)
 {
-	dev->gro_flush_timeout = val;
+	WRITE_ONCE(dev->gro_flush_timeout, val);
 	return 0;
 }
 
@@ -389,7 +389,7 @@ NETDEVICE_SHOW_RW(gro_flush_timeout, fmt_ulong);
 
 static int change_napi_defer_hard_irqs(struct net_device *dev, unsigned long val)
 {
-	dev->napi_defer_hard_irqs = val;
+	WRITE_ONCE(dev->napi_defer_hard_irqs, val);
 	return 0;
 }
 
