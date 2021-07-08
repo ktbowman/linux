46f43ea08cb5abc24eed09784cb206c4ecf142c4
Petr Oros <poros@redhat.com>
2021-07-08 07:13:25 +0200

    net: phy: improve phy_driver callback handle_interrupt
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1955165
    
    Conflicts:
    - added missing chunk for mscc
    
    Upstream commit(s):
    commit 9010f9deb000edce823cb79345f137742ccffa19
    Author: Heiner Kallweit <hkallweit1@gmail.com>
    Date:   Mon Mar 16 22:32:33 2020 +0100
    
        net: phy: improve phy_driver callback handle_interrupt
    
        did_interrupt() clears the interrupt, therefore handle_interrupt() can
        not check which event triggered the interrupt. To overcome this
        constraint and allow more flexibility for customer interrupt handlers,
        let's decouple handle_interrupt() from parts of the phylib interrupt
        handling. Custom interrupt handlers now have to implement the
        did_interrupt() functionality in handle_interrupt() if needed.
    
        Fortunately we have just one custom interrupt handler so far (in the
        mscc PHY driver), convert it to the changed API.
    
        Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Petr Oros <poros@redhat.com>

--- drivers/net/phy/mscc.c
+++ drivers/net/phy/mscc.c
@@ -1764,6 +1764,19 @@ err:
 	return ret;
 }
 
+static irqreturn_t vsc8584_handle_interrupt(struct phy_device *phydev)
+{
+	int irq_status;
+
+	irq_status = phy_read(phydev, MII_VSC85XX_INT_STATUS);
+	if (irq_status < 0 || !(irq_status & MII_VSC85XX_INT_MASK_MASK))
+		return IRQ_NONE;
+
+	phy_mac_interrupt(phydev);
+
+	return IRQ_HANDLED;
+}
+
 static int vsc85xx_config_init(struct phy_device *phydev)
 {
 	int rc, i, phy_id;
