da87c51ecf9fb85b4b20a1a9f7454429d9284059
Paolo Abeni <pabeni@redhat.com>
2021-07-07 21:10:51 -0700

    skbuff: Fix build with SKB extensions disabled
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1987391
    Tested: GRO fwd self-tests
    
    Upstream commit:
    commit 9615fe36b31d926f1c5107013b772dc226a6a7ca
    Author: Florian Fainelli <f.fainelli@gmail.com>
    Date:   Wed Jul 7 21:10:51 2021 -0700
    
        skbuff: Fix build with SKB extensions disabled
    
        We will fail to build with CONFIG_SKB_EXTENSIONS disabled after
        8550ff8d8c75 ("skbuff: Release nfct refcount on napi stolen or re-used
        skbs") since there is an unconditionally use of skb_ext_find() without
        an appropriate stub. Simply build the code conditionally and properly
        guard against both COFNIG_SKB_EXTENSIONS as well as
        CONFIG_NET_TC_SKB_EXT being disabled.
    
        Fixes: Fixes: 8550ff8d8c75 ("skbuff: Release nfct refcount on napi stolen or re-used skbs")
        Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
        Reviewed-by: Roi Dayan <roid@nvidia.com>
        Reviewed-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -5510,7 +5510,7 @@ static void gro_list_prepare(struct napi_struct *napi, struct sk_buff *skb)
 				       maclen);
 
 		diffs |= skb_get_nfct(p) ^ skb_get_nfct(skb);
-
+#if IS_ENABLED(CONFIG_SKB_EXTENSIONS) && IS_ENABLED(CONFIG_NET_TC_SKB_EXT)
 		if (!diffs) {
 			struct tc_skb_ext *skb_ext = skb_ext_find(skb, TC_SKB_EXT);
 			struct tc_skb_ext *p_ext = skb_ext_find(p, TC_SKB_EXT);
@@ -5519,6 +5519,7 @@ static void gro_list_prepare(struct napi_struct *napi, struct sk_buff *skb)
 			if (!diffs && unlikely(skb_ext))
 				diffs |= p_ext->chain ^ skb_ext->chain;
 		}
+#endif
 
 		NAPI_GRO_CB(p)->same_flow = !diffs;
 	}
