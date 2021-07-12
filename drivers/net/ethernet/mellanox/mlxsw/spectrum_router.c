0ce22629d6041a9f12a6f1e6723bf61e6835e565
Ivan Vecera <ivecera@redhat.com>
2021-07-12 18:10:38 +0200

    mlxsw: spectrum_router: Pass nexthop netdev to mlxsw_sp_nexthop4_type_init()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1941938
    
    Conflicts:
    - context due to missing b75ed8b1aa9c3a ("ipv4: Rename fib_nh entries")
    
    commit c3bde5a914bdf7e0db32b98ab02125af736a6b6c
    Author: Ido Schimmel <idosch@nvidia.com>
    Date:   Fri Nov 13 18:05:55 2020 +0200
    
        mlxsw: spectrum_router: Pass nexthop netdev to mlxsw_sp_nexthop4_type_init()
    
        Instead of passing the nexthop and resolving the nexthop netdev from it,
        pass the nexthop netdev directly.
    
        This will later allow us to consolidate code paths between IPv4 and IPv6
        code.
    
        Signed-off-by: Ido Schimmel <idosch@nvidia.com>
        Reviewed-by: Jiri Pirko <jiri@nvidia.com>
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum_router.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum_router.c
@@ -3908,10 +3908,9 @@ static void mlxsw_sp_nexthop_type_fini(struct mlxsw_sp *mlxsw_sp,
 
 static int mlxsw_sp_nexthop4_type_init(struct mlxsw_sp *mlxsw_sp,
 				       struct mlxsw_sp_nexthop *nh,
-				       struct fib_nh *fib_nh)
+				       const struct net_device *dev)
 {
 	const struct mlxsw_sp_ipip_ops *ipip_ops;
-	struct net_device *dev = fib_nh->nh_dev;
 	struct mlxsw_sp_ipip_entry *ipip_entry;
 	struct mlxsw_sp_rif *rif;
 	int err;
@@ -3986,7 +3985,7 @@ static int mlxsw_sp_nexthop4_init(struct mlxsw_sp *mlxsw_sp,
 	}
 	rcu_read_unlock();
 
-	err = mlxsw_sp_nexthop4_type_init(mlxsw_sp, nh, fib_nh);
+	err = mlxsw_sp_nexthop4_type_init(mlxsw_sp, nh, dev);
 	if (err)
 		goto err_nexthop_neigh_init;
 
@@ -4022,7 +4021,7 @@ static void mlxsw_sp_nexthop4_event(struct mlxsw_sp *mlxsw_sp,
 
 	switch (event) {
 	case FIB_EVENT_NH_ADD:
-		mlxsw_sp_nexthop4_type_init(mlxsw_sp, nh, fib_nh);
+		mlxsw_sp_nexthop4_type_init(mlxsw_sp, nh, fib_nh->nh_dev);
 		break;
 	case FIB_EVENT_NH_DEL:
 		mlxsw_sp_nexthop4_type_fini(mlxsw_sp, nh);
