fbf94c1e12ff0a63159dab955a4d40245305b5d9
Ivan Vecera <ivecera@redhat.com>
2021-07-12 18:11:34 +0200

    mlxsw: spectrum_router: Add an indication if a nexthop group can be destroyed
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1941938
    
    commit 2efca2bfba995e67a5372288a4d745fcefc6e8ce
    Author: Ido Schimmel <idosch@nvidia.com>
    Date:   Tue Nov 17 19:47:03 2020 +0200
    
        mlxsw: spectrum_router: Add an indication if a nexthop group can be destroyed
    
        Currently, a nexthop group is destroyed when the last FIB entry is
        detached from it.
    
        When nexthop objects are supported, this can no longer be the case, as
        the group is a separate object whose lifetime is managed by user space.
    
        Add an indication if a nexthop group can be destroyed and always set it
        to true for the existing IPv4 and IPv6 nexthop groups.
    
        Signed-off-by: Ido Schimmel <idosch@nvidia.com>
        Reviewed-by: Jiri Pirko <jiri@nvidia.com>
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- drivers/net/ethernet/mellanox/mlxsw/spectrum_router.c
+++ drivers/net/ethernet/mellanox/mlxsw/spectrum_router.c
@@ -2896,6 +2896,7 @@ struct mlxsw_sp_nexthop_group {
 	};
 	struct mlxsw_sp_nexthop_group_info *nhgi;
 	enum mlxsw_sp_nexthop_group_type type;
+	bool can_destroy;
 };
 
 void mlxsw_sp_nexthop_counter_alloc(struct mlxsw_sp *mlxsw_sp,
@@ -4169,6 +4170,8 @@ mlxsw_sp_nexthop4_group_create(struct mlxsw_sp *mlxsw_sp, struct fib_info *fi)
 	if (err)
 		goto err_nexthop_group_insert;
 
+	nh_grp->can_destroy = true;
+
 	return nh_grp;
 
 err_nexthop_group_insert:
@@ -4183,6 +4186,8 @@ static void
 mlxsw_sp_nexthop4_group_destroy(struct mlxsw_sp *mlxsw_sp,
 				struct mlxsw_sp_nexthop_group *nh_grp)
 {
+	if (!nh_grp->can_destroy)
+		return;
 	mlxsw_sp_nexthop_group_remove(mlxsw_sp, nh_grp);
 	mlxsw_sp_nexthop4_group_info_fini(mlxsw_sp, nh_grp);
 	fib_info_put(nh_grp->ipv4.fi);
@@ -5474,6 +5479,8 @@ mlxsw_sp_nexthop6_group_create(struct mlxsw_sp *mlxsw_sp,
 	if (err)
 		goto err_nexthop_group_insert;
 
+	nh_grp->can_destroy = true;
+
 	return nh_grp;
 
 err_nexthop_group_insert:
@@ -5487,6 +5494,8 @@ static void
 mlxsw_sp_nexthop6_group_destroy(struct mlxsw_sp *mlxsw_sp,
 				struct mlxsw_sp_nexthop_group *nh_grp)
 {
+	if (!nh_grp->can_destroy)
+		return;
 	mlxsw_sp_nexthop_group_remove(mlxsw_sp, nh_grp);
 	mlxsw_sp_nexthop6_group_info_fini(mlxsw_sp, nh_grp);
 	kfree(nh_grp);
