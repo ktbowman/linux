dfb714963a7b4762aa1c716eb94e84a0f49f37f3
Jonathan Toppins <jtoppins@redhat.com>
2021-07-13 12:50:05 -0400

    bnxt_en: Add a new BNXT_STATE_NAPI_DISABLED flag to keep track of NAPI state.
    Up until now, we don't need to keep track of this state because NAPI
    is always enabled once and disabled once during bring up and shutdown.
    For better error recovery in subsequent patches, we want to quiesce
    the device earlier during fatal error conditions.  The normal shutdown
    sequence will disable NAPI again and the flag will prevent disabling
    NAPI twice.
    
    Reviewed-by: Pavan Chebbi <pavan.chebbi@broadcom.com>
    Reviewed-by: Andy Gospodarek <gospo@broadcom.com>
    Reviewed-by: Edwin Peer <edwin.peer@broadcom.com>
    Reviewed-by: Vasundhara Volam <vasundhara-v.volam@broadcom.com>
    Signed-off-by: Michael Chan <michael.chan@broadcom.com>
    Acked-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit e340a5c4fbdde20fec8c16b83bce386aaad6b6eb)
    Signed-off-by: Jonathan Toppins <jtoppins@redhat.com>
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1921449
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=38028096
    Tested: compile tested
    Signed-off-by: Jonathan Toppins <jtoppins@redhat.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.c
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.c
@@ -8840,7 +8840,8 @@ static void bnxt_disable_napi(struct bnxt *bp)
 {
 	int i;
 
-	if (!bp->bnapi)
+	if (!bp->bnapi ||
+	    test_and_set_bit(BNXT_STATE_NAPI_DISABLED, &bp->state))
 		return;
 
 	for (i = 0; i < bp->cp_nr_rings; i++) {
@@ -8857,6 +8858,7 @@ static void bnxt_enable_napi(struct bnxt *bp)
 {
 	int i;
 
+	clear_bit(BNXT_STATE_NAPI_DISABLED, &bp->state);
 	for (i = 0; i < bp->cp_nr_rings; i++) {
 		struct bnxt_napi *bnapi = bp->bnapi[i];
 		struct bnxt_cp_ring_info *cpr;
