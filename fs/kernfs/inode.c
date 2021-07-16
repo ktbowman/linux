2aa0dad66de817f68b5367cba3e0988097f3a51d
Ian Kent <ikent@redhat.com>
2021-07-16 12:22:42 +0800

    kernfs: switch kernfs to use an rwsem
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1939133
    Testing: The change has been tested in the reported environment
    Upstream status: https://git.kernel.org/pub/scm/linux/kernel/git/gregkh/driver-core.git
    
    Conflicts: The setattr_prepare(), generic_fillattr() and
     generic_permission() functions that appear in the hunk contexts of
     this patch gained a user namespace argument. But I only saw one actual
     reject when appling the patch, the remaining hunks that contained these
     function calls in their context applied with fuzz.
    
    commit 7ba0273b2f34a55efe967d3c7381fb1da2ca195f driver-core-next
    Author: Ian Kent <raven@themaw.net>
    Date:   Mon Jun 7 16:29:59 2021 +0800
    
        kernfs: switch kernfs to use an rwsem
    
        The kernfs global lock restricts the ability to perform kernfs node
        lookup operations in parallel during path walks.
    
        Change the kernfs mutex to an rwsem so that, when opportunity arises,
        node searches can be done in parallel with path walk lookups.
    
        Reviewed-by: Miklos Szeredi <mszeredi@redhat.com>
        Signed-off-by: Ian Kent <raven@themaw.net>
        Link: https://lore.kernel.org/r/162642770946.63632.2218304587223241374.stgit@web.messagingengine.com
        Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Ian Kent <ikent@redhat.com>

--- fs/kernfs/inode.c
+++ fs/kernfs/inode.c
@@ -105,9 +105,9 @@ int kernfs_setattr(struct kernfs_node *kn, const struct iattr *iattr)
 {
 	int ret;
 
-	mutex_lock(&kernfs_mutex);
+	down_write(&kernfs_rwsem);
 	ret = __kernfs_setattr(kn, iattr);
-	mutex_unlock(&kernfs_mutex);
+	up_write(&kernfs_rwsem);
 	return ret;
 }
 
@@ -120,7 +120,7 @@ int kernfs_iop_setattr(struct dentry *dentry, struct iattr *iattr)
 	if (!kn)
 		return -EINVAL;
 
-	mutex_lock(&kernfs_mutex);
+	down_write(&kernfs_rwsem);
 	error = setattr_prepare(dentry, iattr);
 	if (error)
 		goto out;
@@ -133,7 +133,7 @@ int kernfs_iop_setattr(struct dentry *dentry, struct iattr *iattr)
 	setattr_copy(inode, iattr);
 
 out:
-	mutex_unlock(&kernfs_mutex);
+	up_write(&kernfs_rwsem);
 	return error;
 }
 
@@ -188,9 +188,9 @@ int kernfs_iop_getattr(const struct path *path, struct kstat *stat,
 	struct inode *inode = d_inode(path->dentry);
 	struct kernfs_node *kn = inode->i_private;
 
-	mutex_lock(&kernfs_mutex);
+	down_write(&kernfs_rwsem);
 	kernfs_refresh_inode(kn, inode);
-	mutex_unlock(&kernfs_mutex);
+	up_write(&kernfs_rwsem);
 
 	generic_fillattr(inode, stat);
 	return 0;
@@ -280,9 +280,9 @@ int kernfs_iop_permission(struct inode *inode, int mask)
 
 	kn = inode->i_private;
 
-	mutex_lock(&kernfs_mutex);
+	down_write(&kernfs_rwsem);
 	kernfs_refresh_inode(kn, inode);
-	mutex_unlock(&kernfs_mutex);
+	up_write(&kernfs_rwsem);
 
 	return generic_permission(inode, mask);
 }
