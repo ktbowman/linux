a974be00f61dd686720360a75f097bebb7431110
Ken Cox <jkc@redhat.com>
2021-07-30 07:22:45 -0500

    ixgbevf: Fix fall-through warnings for Clang
    Bugzilla: http://bugzilla.redhat.com/1920268
    
    commit d8f0c306985eef4893106e24b9a68dbe8561e3b7
    Author: Gustavo A. R. Silva <gustavoars@kernel.org>
    Date:   Fri Mar 5 03:46:42 2021 -0600
    
        ixgbevf: Fix fall-through warnings for Clang
    
        In preparation to enable -Wimplicit-fallthrough for Clang, fix a warning
        by explicitly adding a break statement instead of just letting the code
        fall through to the next case.
    
        Link: https://github.com/KSPP/linux/issues/115
        Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
        Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    
    Signed-off-by: Ken Cox <jkc@redhat.com>

--- drivers/net/ethernet/intel/ixgbevf/ixgbevf_main.c
+++ drivers/net/ethernet/intel/ixgbevf/ixgbevf_main.c
@@ -2635,6 +2635,7 @@ static void ixgbevf_set_num_queues(struct ixgbevf_adapter *adapter)
 			adapter->num_rx_queues = rss;
 			adapter->num_tx_queues = rss;
 			adapter->num_xdp_queues = adapter->xdp_prog ? rss : 0;
+			break;
 		default:
 			break;
 		}
