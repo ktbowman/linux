afd7c7181b000c9a1e0a74f78983f062df0265dc
Paolo Abeni <pabeni@redhat.com>
2021-08-04 18:23:23 +0200

    sk_buff: avoid potentially clearing 'slow_gro' field
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1987391
    Tested: GRO fwd self-tests
    
    Upstream commit:
    commit a432934a30679c0e3c47b87f13e4901bc1a3fc03
    Author: Paolo Abeni <pabeni@redhat.com>
    Date:   Fri Jul 30 18:30:53 2021 +0200
    
        sk_buff: avoid potentially clearing 'slow_gro' field
    
        If skb_dst_set_noref() is invoked with a NULL dst, the 'slow_gro'
        field is cleared, too. That could lead to wrong behavior if
        the skb later enters the GRO stage.
    
        Fix the potential issue replacing preserving a non-zero value of
        the 'slow_gro' field.
    
        Additionally, fix a comment typo.
    
        Reported-by: Sabrina Dubroca <sd@queasysnail.net>
        Reported-by: Jakub Kicinski <kuba@kernel.org>
        Fixes: 8a886b142bd0 ("sk_buff: track dst status in slow_gro")
        Signed-off-by: Paolo Abeni <pabeni@redhat.com>
        Link: https://lore.kernel.org/r/aa42529252dc8bb02bd42e8629427040d1058537.1627662501.git.pabeni@redhat.com
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -5508,7 +5508,7 @@ static void gro_list_prepare(struct napi_struct *napi, struct sk_buff *skb)
 				       skb_mac_header(skb),
 				       maclen);
 
-		/* in most common scenarions _state is 0
+		/* in most common scenarions 'slow_gro' is 0
 		 * otherwise we are already on some slower paths
 		 * either skip all the infrequent tests altogether or
 		 * avoid trying too hard to skip each of them individually
