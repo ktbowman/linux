72b9e0bf793a133de9ea2ff1191b62dbf1953409
Michael Petlan <mpetlan@redhat.com>
2021-09-02 23:36:16 +0200

    perf/x86/intel: Hybrid PMU support for perf capabilities
    Bugzilla: https://bugzilla.redhat.com/1838573
    
    upstream
    ========
    commit d0946a882e6220229a29f9031641e54379be5a1e
    Author: Kan Liang <kan.liang@linux.intel.com>
    Date: Mon Apr 12 07:30:44 2021 -0700
    
    description
    ===========
    Some platforms, e.g. Alder Lake, have hybrid architecture. Although most
    PMU capabilities are the same, there are still some unique PMU
    capabilities for different hybrid PMUs. Perf should register a dedicated
    pmu for each hybrid PMU.
    
    Add a new struct x86_hybrid_pmu, which saves the dedicated pmu and
    capabilities for each hybrid PMU.
    
    The architecture MSR, MSR_IA32_PERF_CAPABILITIES, only indicates the
    architecture features which are available on all hybrid PMUs. The
    architecture features are stored in the global x86_pmu.intel_cap.
    
    For Alder Lake, the model-specific features are perf metrics and
    PEBS-via-PT. The corresponding bits of the global x86_pmu.intel_cap
    should be 0 for these two features. Perf should not use the global
    intel_cap to check the features on a hybrid system.
    Add a dedicated intel_cap in the x86_hybrid_pmu to store the
    model-specific capabilities. Use the dedicated intel_cap to replace
    the global intel_cap for thse two features. The dedicated intel_cap
    will be set in the following "Add Alder Lake Hybrid support" patch.
    
    Add is_hybrid() to distinguish a hybrid system. ADL may have an
    alternative configuration. With that configuration, the
    X86_FEATURE_HYBRID_CPU is not set. Perf cannot rely on the feature bit.
    Add a new static_key_false, perf_is_hybrid, to indicate a hybrid system.
    It will be assigned in the following "Add Alder Lake Hybrid support"
    patch as well.
    
    Signed-off-by: Michael Petlan <mpetlan@redhat.com>

--- arch/x86/events/core.c
+++ arch/x86/events/core.c
@@ -53,6 +53,7 @@ DEFINE_PER_CPU(struct cpu_hw_events, cpu_hw_events) = {
 
 DEFINE_STATIC_KEY_FALSE(rdpmc_never_available_key);
 DEFINE_STATIC_KEY_FALSE(rdpmc_always_available_key);
+DEFINE_STATIC_KEY_FALSE(perf_is_hybrid);
 
 u64 __read_mostly hw_cache_event_ids
 				[PERF_COUNT_HW_CACHE_MAX]
@@ -1068,8 +1069,9 @@ static void del_nr_metric_event(struct cpu_hw_events *cpuc,
 static int collect_event(struct cpu_hw_events *cpuc, struct perf_event *event,
 			 int max_count, int n)
 {
+	union perf_capabilities intel_cap = hybrid(cpuc->pmu, intel_cap);
 
-	if (x86_pmu.intel_cap.perf_metrics && add_nr_metric_event(cpuc, event))
+	if (intel_cap.perf_metrics && add_nr_metric_event(cpuc, event))
 		return -EINVAL;
 
 	if (n >= max_count + cpuc->n_metric)
@@ -1547,6 +1549,7 @@ void x86_pmu_stop(struct perf_event *event, int flags)
 static void x86_pmu_del(struct perf_event *event, int flags)
 {
 	struct cpu_hw_events *cpuc = this_cpu_ptr(&cpu_hw_events);
+	union perf_capabilities intel_cap = hybrid(cpuc->pmu, intel_cap);
 	int i;
 
 	/*
@@ -1587,7 +1590,7 @@ static void x86_pmu_del(struct perf_event *event, int flags)
 	}
 	cpuc->event_constraint[i-1] = NULL;
 	--cpuc->n_events;
-	if (x86_pmu.intel_cap.perf_metrics)
+	if (intel_cap.perf_metrics)
 		del_nr_metric_event(cpuc, event);
 
 	perf_event_update_userpage(event);
