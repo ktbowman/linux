a18c96b309f95e856f15d3765c8f4da5f88877a7
Michael Petlan <mpetlan@redhat.com>
2021-09-08 11:38:45 +0200

    perf evsel: Add missing cloning of evsel->use_config_name
    Bugzilla: https://bugzilla.redhat.com/1838635
    
    upstream
    ========
    commit 2dc065eae56df804e4da5f8a9e4139033f7ea605
    Author: Namhyung Kim <namhyung@kernel.org>
    Date:   Wed Jun 2 14:22:40 2021 -0700
    
    description
    ===========
    The evsel__clone() should copy all fields in the evsel which are set
    during the event parsing.  But it missed the use_config_name field.
    
    Fixes: 12279429d862 ("perf stat: Uniquify hybrid event name")
    Link: http://lore.kernel.org/lkml/20210602212241.2175005-2-namhyung@kernel.org
    
    Conflicts:
    ==========
    Different context due to not-yet-backported 01bd8efcec44 ("perf stat:
    Introduce ':b' modifier")
    
    Signed-off-by: Michael Petlan <mpetlan@redhat.com>

--- tools/perf/util/evsel.h
+++ tools/perf/util/evsel.h
@@ -80,8 +80,10 @@ struct evsel {
 		bool			auto_merge_stats;
 		bool			collect_stat;
 		bool			weak_group;
+		bool			use_config_name;
 		int			bpf_fd;
 		struct bpf_object	*bpf_obj;
+		struct list_head	config_terms;
 	};
 
 	/*
@@ -113,10 +115,8 @@ struct evsel {
 	bool			merged_stat;
 	bool			reset_group;
 	bool			errored;
-	bool			use_config_name;
 	struct hashmap		*per_pkg_mask;
 	struct evsel		*leader;
-	struct list_head	config_terms;
 	int			err;
 	int			cpu_iter;
 	struct {
