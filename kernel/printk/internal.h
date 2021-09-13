2e0ca24640bfb948bc97045471e311e36438098f
Chris von Recklinghausen <crecklin@redhat.com>
2021-09-13 08:52:13 -0400

    printk: remove logbuf_lock
    Bugzilla: http://bugzilla.redhat.com/1985062
    
    commit 996e966640ddea7b535cbe7b749e87a3b24f37e8
    Author: John Ogness <john.ogness@linutronix.de>
    Date:   Wed Mar 3 11:15:26 2021 +0100
    
        printk: remove logbuf_lock
    
        Since the ringbuffer is lockless, there is no need for it to be
        protected by @logbuf_lock. Remove @logbuf_lock.
    
        @console_seq, @exclusive_console_stop_seq, @console_dropped are
        protected by @console_lock.
    
        Signed-off-by: John Ogness <john.ogness@linutronix.de>
        Reviewed-by: Petr Mladek <pmladek@suse.com>
        Signed-off-by: Petr Mladek <pmladek@suse.com>
        Link: https://lore.kernel.org/r/20210303101528.29901-14-john.ogness@linutronix.de
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- kernel/printk/internal.h
+++ kernel/printk/internal.h
@@ -22,8 +22,6 @@
 #define PRINTK_NMI_DIRECT_CONTEXT_MASK	 0x40000000
 #define PRINTK_NMI_CONTEXT_MASK		 0x80000000
 
-extern raw_spinlock_t logbuf_lock;
-
 __printf(4, 0)
 int vprintk_store(int facility, int level,
 		  const struct dev_printk_info *dev_info,
@@ -69,7 +67,7 @@ void defer_console_output(void);
 __printf(1, 0) int vprintk_func(const char *fmt, va_list args) { return 0; }
 
 /*
- * In !PRINTK builds we still export logbuf_lock spin_lock, console_sem
+ * In !PRINTK builds we still export console_sem
  * semaphore and some of console functions (console_unlock()/etc.), so
  * printk-safe must preserve the existing local IRQ guarantees.
  */
