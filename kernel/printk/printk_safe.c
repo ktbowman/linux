ab6b1caff863faecee8fb0d0a5bc9da498591ffa
Chris von Recklinghausen <crecklin@redhat.com>
2021-09-13 08:52:13 -0400

    printk: Move EXPORT_SYMBOL() closer to vprintk definition
    Bugzilla: http://bugzilla.redhat.com/1985062
    
    commit 6262e1b906a1ba12688ea6039453b4a088dbaf44
    Author: Punit Agrawal <punitagrawal@gmail.com>
    Date:   Tue Jun 15 08:56:35 2021 +0900
    
        printk: Move EXPORT_SYMBOL() closer to vprintk definition
    
        Commit 28e1745b9fa2 ("printk: rename vprintk_func to vprintk") while
        improving readability by removing vprintk indirection, inadvertently
        placed the EXPORT_SYMBOL() for the newly renamed function at the end
        of the file.
    
        For reader sanity, and as is convention move the EXPORT_SYMBOL()
        declaration just after the end of the function.
    
        Fixes: 28e1745b9fa2 ("printk: rename vprintk_func to vprintk")
        Signed-off-by: Punit Agrawal <punitagrawal@gmail.com>
        Acked-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
        Acked-by: Sergey Senozhatsky <senozhatsky@chromium.org>
        Signed-off-by: Petr Mladek <pmladek@suse.com>
        Link: https://lore.kernel.org/r/20210614235635.887365-1-punitagrawal@gmail.com
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- kernel/printk/printk_safe.c
+++ kernel/printk/printk_safe.c
@@ -387,6 +387,7 @@ asmlinkage int vprintk(const char *fmt, va_list args)
 	/* No obstacles. */
 	return vprintk_default(fmt, args);
 }
+EXPORT_SYMBOL(vprintk);
 
 void __init printk_safe_init(void)
 {
@@ -407,4 +408,3 @@ void __init printk_safe_init(void)
 	/* Flush pending messages that did not have scheduled IRQ works. */
 	printk_safe_flush();
 }
-EXPORT_SYMBOL(vprintk);
