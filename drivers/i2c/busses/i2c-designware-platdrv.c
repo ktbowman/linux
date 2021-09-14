c244fca99b13e0f1407ae70dcd8e675a9043d0de
David Arcari <darcari@redhat.com>
2021-09-14 11:48:40 -0400

    i2c: designware: Add ACPI HID for Hisilicon Hip08-Lite I2C controller
    Bugzilla: http://bugzilla.redhat.com/2004468
    
    commit dec0a81a7814c8a876e663e0627468e6b1a7d2fb
    Author: Hanjun Guo <guohanjun@huawei.com>
    Date:   Mon Feb 3 09:36:07 2020 +0800
    
        i2c: designware: Add ACPI HID for Hisilicon Hip08-Lite I2C controller
    
        Add ACPI HID HISI02A3 for Hisilicon Hip08 Lite, which has different
        clock frequency from Hip08 for I2C controller.
    
        Tested-by: Sheng Feng <fengsheng5@huawei.com>
        Signed-off-by: Hanjun Guo <guohanjun@huawei.com>
        Acked-by: Nikula <jarkko.nikula@linux.intel.com>
        Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
        Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    Signed-off-by: David Arcari <darcari@redhat.com>

--- drivers/i2c/busses/i2c-designware-platdrv.c
+++ drivers/i2c/busses/i2c-designware-platdrv.c
@@ -130,6 +130,7 @@ static const struct acpi_device_id dw_i2c_acpi_match[] = {
 	{ "APMC0D0F", 0 },
 	{ "HISI02A1", 0 },
 	{ "HISI02A2", 0 },
+	{ "HISI02A3", 0 },
 	{ "HYGO0010", ACCESS_INTR_MASK },
 	{ }
 };
