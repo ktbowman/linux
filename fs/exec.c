cb4137944beab60b9ab75af295bed8b4bdb05b57
Chris von Recklinghausen <crecklin@redhat.com>
2021-09-14 12:46:00 -0400

    exec: Move would_dump into flush_old_exec
    Conflicts: fs/exec.c - The upstream commit applies this hunk to
            __do_execve_file() which has been renamed to do_execveat_common()
    	in RHEL8 by RHEL commit
    	2e95e46882c2 ("[fs] exec: Remove do_execve_file").
    
    Bugzilla: http://bugzilla.redhat.com/1993665
    
    commit f87d1c9559164294040e58f5e3b74a162bf7c6e8
    Author: Eric W. Biederman <ebiederm@xmission.com>
    Date:   Sat May 16 16:29:20 2020 -0500
    
        exec: Move would_dump into flush_old_exec
    
        I goofed when I added mm->user_ns support to would_dump.  I missed the
        fact that in the case of binfmt_loader, binfmt_em86, binfmt_misc, and
        binfmt_script bprm->file is reassigned.  Which made the move of
        would_dump from setup_new_exec to __do_execve_file before exec_binprm
        incorrect as it can result in would_dump running on the script instead
        of the interpreter of the script.
    
        The net result is that the code stopped making unreadable interpreters
        undumpable.  Which allows them to be ptraced and written to disk
        without special permissions.  Oops.
    
        The move was necessary because the call in set_new_exec was after
        bprm->mm was no longer valid.
    
        To correct this mistake move the misplaced would_dump from
        __do_execve_file into flos_old_exec, before exec_mmap is called.
    
        I tested and confirmed that without this fix I can attach with gdb to
        a script with an unreadable interpreter, and with this fix I can not.
    
        Cc: stable@vger.kernel.org
        Fixes: f84df2a6f268 ("exec: Ensure mm->user_ns contains the execed files")
        Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1297,6 +1297,8 @@ int begin_new_exec(struct linux_binprm * bprm)
 	 */
 	set_mm_exe_file(bprm->mm, bprm->file);
 
+	would_dump(bprm, bprm->file);
+
 	/*
 	 * Release all of the old mmap stuff
 	 */
@@ -1858,8 +1860,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 	retval = copy_strings(bprm->argc, argv, bprm);
 	if (retval < 0)
 		goto out;
-
-	would_dump(bprm, bprm->file);
 
 	retval = exec_binprm(bprm);
 	if (retval < 0)
