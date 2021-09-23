7459ad718b9ac3a36483a3bc3b3c5757f67856d2
Kamal Heib <kheib@redhat.com>
2021-09-23 10:25:29 +0300

    RDMA: Fix kernel-doc compilation warnings
    Bugzilla: http://bugzilla.redhat.com/2006259
    Conflicts:
    Drop the QIB hunk(s) as this driver is not supported in RHEL-8.
    
    commit ae360f41b1263887ada64d9a708e12c1f52f91b9
    Author: Leon Romanovsky <leonro@nvidia.com>
    Date:   Sun Mar 14 15:39:07 2021 +0200
    
        RDMA: Fix kernel-doc compilation warnings
    
        This patch fixes bunch of kernel-doc compilation warnings like below:
    
         drivers/infiniband/hw/i40iw/i40iw_cm.c:4372: warning: expecting prototype for i40iw_ifdown_notify(). Prototype was for i40iw_if_notify() instead
    
        Link: https://lore.kernel.org/r/20210314133908.291945-2-leon@kernel.org
        Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
        Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    
    Signed-off-by: Kamal Heib <kheib@redhat.com>

--- drivers/infiniband/hw/i40iw/i40iw_main.c
+++ drivers/infiniband/hw/i40iw/i40iw_main.c
@@ -255,7 +255,7 @@ static void i40iw_destroy_cqp(struct i40iw_device *iwdev, bool free_hwcqp)
 }
 
 /**
- * i40iw_disable_irqs - disable device interrupts
+ * i40iw_disable_irq - disable device interrupts
  * @dev: hardware control device structure
  * @msic_vec: msix vector to disable irq
  * @dev_id: parameter to pass to free_irq (used during irq setup)
