d436cacde16aab38d40085b3521eaf40f62319ef
Nico Pache <npache@redhat.com>
2021-09-24 19:10:33 -0400

    sched: highmem: Store local kmaps in task struct
    commit 5fbda3ecd14a5343644979c98d6eb65b7e7de9d8
    Author: Thomas Gleixner <tglx@linutronix.de>
    Date:   Wed Nov 18 20:48:43 2020 +0100
    
        sched: highmem: Store local kmaps in task struct
    
        Instead of storing the map per CPU provide and use per task storage. That
        prepares for local kmaps which are preemptible.
    
        The context switch code is preparatory and not yet in use because
        kmap_atomic() runs with preemption disabled. Will be made usable in the
        next step.
    
        The context switch logic is safe even when an interrupt happens after
        clearing or before restoring the kmaps. The kmap index in task struct is
        not modified so any nesting kmap in an interrupt will use unused indices
        and on return the counter is the same as before.
    
        Also add an assert into the return to user space code. Going back to user
        space with an active kmap local is a nono.
    
        Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
        Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
        Link: https://lore.kernel.org/r/20201118204007.372935758@linutronix.de
    
    Conflicts:
           include/linux/sched.h: use RH_KABI_FILL_HOLE for kmap_ctrl. The
    	struct size will be 0 so no need to utilize task_struct_rh.
           kernel/entry/common.c: missing generic syscall entry functions
            introduced in commit 142781e108b1 ("entry: Provide generic syscall
            entry functionality")
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2001854
    Signed-off-by: Nico Pache <npache@redhat.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -35,6 +35,7 @@
 #include <linux/seqlock.h>
 #include <linux/kcsan.h>
 #include <linux/rh_kabi.h>
+#include <asm/kmap_size.h>
 
 /* task_struct member predeclarations (sorted alphabetically): */
 struct audit_context;
@@ -580,6 +581,13 @@ enum perf_event_task_context {
 
 struct wake_q_node {
 	struct wake_q_node *next;
+};
+
+struct kmap_ctrl {
+#ifdef CONFIG_KMAP_LOCAL
+	int				idx;
+	pte_t				pteval[KM_MAX_IDX];
+#endif
 };
 
 struct task_struct;
@@ -1279,6 +1287,7 @@ struct task_struct {
 	unsigned int			sequential_io;
 	unsigned int			sequential_io_avg;
 #endif
+	RH_KABI_FILL_HOLE(struct kmap_ctrl kmap_ctrl)
 #ifdef CONFIG_DEBUG_ATOMIC_SLEEP
 	unsigned long			task_state_change;
 #endif
