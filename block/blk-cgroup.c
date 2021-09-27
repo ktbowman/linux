5449cea72628a3eeacccd0194a2dba8a3a02db72
Ming Lei <ming.lei@redhat.com>
2021-09-27 22:17:57 +0800

    block: Introduce the ioprio rq-qos policy
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1997338
    
    commit 556910e39249d55e23deaec479f49e7d85bc0d24
    Author: Bart Van Assche <bvanassche@acm.org>
    Date:   Thu Jun 17 17:44:44 2021 -0700
    
        block: Introduce the ioprio rq-qos policy
    
        Introduce an rq-qos policy that assigns an I/O priority to requests based
        on blk-cgroup configuration settings. This policy has the following
        advantages over the ioprio_set() system call:
        - This policy is cgroup based so it has all the advantages of cgroups.
        - While ioprio_set() does not affect page cache writeback I/O, this rq-qos
          controller affects page cache writeback I/O for filesystems that support
          assiociating a cgroup with writeback I/O. See also
          Documentation/admin-guide/cgroup-v2.rst.
    
        Cc: Damien Le Moal <damien.lemoal@wdc.com>
        Cc: Hannes Reinecke <hare@suse.de>
        Cc: Christoph Hellwig <hch@lst.de>
        Cc: Ming Lei <ming.lei@redhat.com>
        Cc: Johannes Thumshirn <johannes.thumshirn@wdc.com>
        Cc: Himanshu Madhani <himanshu.madhani@oracle.com>
        Signed-off-by: Bart Van Assche <bvanassche@acm.org>
        Link: https://lore.kernel.org/r/20210618004456.7280-5-bvanassche@acm.org
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- block/blk-cgroup.c
+++ block/blk-cgroup.c
@@ -30,6 +30,7 @@
 #include <linux/tracehook.h>
 #include <linux/psi.h>
 #include "blk.h"
+#include "blk-ioprio.h"
 
 /*
  * blkcg_pol_mutex protects blkcg_policy[] and policy [de]activation.
@@ -1219,6 +1220,10 @@ int blkcg_init_queue(struct request_queue *q)
 		radix_tree_preload_end();
 
 	ret = blk_iolatency_init(q);
+	if (ret)
+		goto err_destroy_all;
+
+	ret = blk_ioprio_init(q);
 	if (ret)
 		goto err_destroy_all;
 
