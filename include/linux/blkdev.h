d82770138151808a34c483932ab6a1e7c067e7a9
Ming Lei <ming.lei@redhat.com>
2021-09-27 22:18:02 +0800

    block: return errors from blk_execute_rq()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1997338
    Conflicts: blk_execute_rq() is in whitelist, and we can't change
    its type, so add blk_execute_rq_rh() instead.
    
    commit fb9b16e15cd70e21d8af7f03d700deb9509c2ce8
    Author: Keith Busch <kbusch@kernel.org>
    Date:   Thu Jun 10 14:44:36 2021 -0700
    
        block: return errors from blk_execute_rq()
    
        The synchronous blk_execute_rq() had not provided a way for its callers
        to know if its request was successful or not. Return the blk_status_t
        result of the request.
    
        Reviewed-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Ming Lei <ming.lei@redhat.com>
        Signed-off-by: Keith Busch <kbusch@kernel.org>
        Reviewed-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
        Link: https://lore.kernel.org/r/20210610214437.641245-4-kbusch@kernel.org
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -947,6 +947,8 @@ extern void blk_execute_rq(struct request_queue *, struct gendisk *,
 			  struct request *, int);
 extern void blk_execute_rq_nowait(struct request_queue *, struct gendisk *,
 				  struct request *, int, rq_end_io_fn *);
+blk_status_t blk_execute_rq_rh(struct request_queue *, struct gendisk *,
+			  struct request *, int);
 
 /* Helper to convert REQ_OP_XXX to its string format XXX */
 extern const char *blk_op_str(unsigned int op);
