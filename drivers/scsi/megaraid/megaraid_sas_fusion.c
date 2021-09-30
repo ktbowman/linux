6763aef9c41d4018975c529246edb74bbcae2a3c
Tomas Henzl <thenzl@redhat.com>
2021-09-30 15:51:23 +0200

    scsi: megaraid_sas: mq_poll support
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2009022
    (cherry picked from commit 9e4bec5b2a230066a0dc9f79f24b4c1bcb668c5a)
    Upstream Status: RHEL only +
    (This patch is part of the above upstream patch we have taken only the
    part which fixes an irq/poll synv issue.)
    
    Implement mq_poll interface support in megaraid_sas. This feature
    requires shared host tag support in kernel and driver.
    
    The driver can work in non-IRQ mode which means there will not be any MSI-x
    vector associated for poll_queues. The MegaRAID hardware has a single
    submission queue and multiple reply queues. However, using the shared host
    tagset support will enable the driver to simulate multiple hardware queues.
    
    Change driver to allocate some extra reply queues which will be marked as
    poll_queues. These poll_queues will not have associated MSI-x vectors. All
    I/O completions on these queues will be done through the IOPOLL interface.
    
    megaraid_sas with 8 poll_queues and using the io_uring hiprio=1 setting can
    reach 3.2M IOPS with zero interrupts generated by the hardware.
    
    The IOPOLL feature can be enabled using module parameter poll_queues.
    
    Link: https://lore.kernel.org/r/20210215074048.19424-3-kashyap.desai@broadcom.com
    Cc: sumit.saxena@broadcom.com
    Cc: chandrakanth.patil@broadcom.com
    Cc: linux-block@vger.kernel.org
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Kashyap Desai <kashyap.desai@broadcom.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Tomas Henzl <thenzl@redhat.com>

--- drivers/scsi/megaraid/megaraid_sas_fusion.c
+++ drivers/scsi/megaraid/megaraid_sas_fusion.c
@@ -3536,6 +3536,9 @@ complete_cmd_fusion(struct megasas_instance *instance, u32 MSIxIndex,
 	if (reply_descript_type == MPI2_RPY_DESCRIPT_FLAGS_UNUSED)
 		return IRQ_NONE;
 
+	if (irq_context && !atomic_add_unless(&irq_context->in_used, 1, 1))
+		return 0;
+
 	num_completed = 0;
 
 	while (d_val.u.low != cpu_to_le32(UINT_MAX) &&
@@ -3650,6 +3653,7 @@ complete_cmd_fusion(struct megasas_instance *instance, u32 MSIxIndex,
 					irq_context->irq_line_enable = true;
 					irq_poll_sched(&irq_context->irqpoll);
 				}
+				atomic_dec(&irq_context->in_used);
 				return num_completed;
 			}
 		}
@@ -3667,6 +3671,10 @@ complete_cmd_fusion(struct megasas_instance *instance, u32 MSIxIndex,
 				instance->reply_post_host_index_addr[0]);
 		megasas_check_and_restore_queue_depth(instance);
 	}
+
+	if (irq_context)
+		atomic_dec(&irq_context->in_used);
+
 	return num_completed;
 }
 
