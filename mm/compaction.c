754fc11ca3dd9eedb322928eb39290f29d1e6dcf
Waiman Long <longman@redhat.com>
2021-10-06 20:22:35 -0400

    mm/swap.c: don't pass "enum lru_list" to del_page_from_lru_list()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2008341
    Conflicts:
     1) Fuzz in include/linux/mm_inline.h due to missing upstream commit
        f86196ea8737 ("fs: don't open code lru_to_page()").
     2) Fuzz in mm/compaction.c due to RHEL commit 2584c095cbb3 ("[mm] mm:
        replace hpage_nr_pages with thp_nr_pages") did not replace
        all instances of hpage_nr_pages() to thp_nr_pages(). Change
        hpage_nr_pages() in that hunk to thp_nr_pages() too.
     3)
    
    commit 46ae6b2cc2a47904a368d238425531ea91f3a2a5
    Author: Yu Zhao <yuzhao@google.com>
    Date:   Wed, 24 Feb 2021 12:08:25 -0800
    
        mm/swap.c: don't pass "enum lru_list" to del_page_from_lru_list()
    
        The parameter is redundant in the sense that it can be potentially
        extracted from the "struct page" parameter by page_lru(). We need to
        make sure that existing PageActive() or PageUnevictable() remains
        until the function returns. A few places don't conform, and simple
        reordering fixes them.
    
        This patch may have left page_off_lru() seemingly odd, and we'll take
        care of it in the next patch.
    
        Link: https://lore.kernel.org/linux-mm/20201207220949.830352-6-yuzhao@google.com/
        Link: https://lkml.kernel.org/r/20210122220600.906146-6-yuzhao@google.com
        Signed-off-by: Yu Zhao <yuzhao@google.com>
        Cc: Alex Shi <alex.shi@linux.alibaba.com>
        Cc: Hugh Dickins <hughd@google.com>
        Cc: Johannes Weiner <hannes@cmpxchg.org>
        Cc: Matthew Wilcox <willy@infradead.org>
        Cc: Michal Hocko <mhocko@kernel.org>
        Cc: Roman Gushchin <guro@fb.com>
        Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
        Cc: Vlastimil Babka <vbabka@suse.cz>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -1035,10 +1035,10 @@ isolate_migratepages_block(struct compact_control *cc, unsigned long low_pfn,
 			low_pfn += compound_nr(page) - 1;
 
 		/* Successfully isolated */
-		del_page_from_lru_list(page, lruvec, page_lru(page));
+		del_page_from_lru_list(page, lruvec);
 		mod_node_page_state(page_pgdat(page),
 				NR_ISOLATED_ANON + page_is_file_lru(page),
-				hpage_nr_pages(page));
+				thp_nr_pages(page));
 
 isolate_success:
 		list_add(&page->lru, &cc->migratepages);
