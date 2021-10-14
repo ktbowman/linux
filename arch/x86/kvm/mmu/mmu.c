33ea4d16a6d9dea639733abb3225c7f31ce827fe
Vitaly Kuznetsov <vkuznets@redhat.com>
2021-10-14 11:43:34 +0200

    KVM: x86/mmu: Use MMU's role to get CR4.PSE for computing rsvd bits
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2009340
    
    commit 4e9c0d80dbbd2dd411d726ed10eccaaba6d63a08
    Author: Sean Christopherson <seanjc@google.com>
    Date:   Tue Jun 22 10:57:15 2021 -0700
    
        KVM: x86/mmu: Use MMU's role to get CR4.PSE for computing rsvd bits
    
        Use the MMU's role to get CR4.PSE when calculating reserved bits for the
        guest's PTEs.  Practically speaking, this is a glorified nop as the role
        always come from vCPU state for the relevant flows, but converting to
        the roles will provide consistency once everything else is converted, and
        will Just Work if the "always comes from vCPU" behavior were ever to
        change (unlikely).
    
        Signed-off-by: Sean Christopherson <seanjc@google.com>
        Message-Id: <20210622175739.3610207-31-seanjc@google.com>
        Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    Conflicts:
    	arch/x86/kvm/mmu/mmu.c (context, no guest_cpuid_is_amd_or_hygon() in RHEL8)
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -4233,7 +4233,8 @@ static void reset_rsvds_bits_mask(struct kvm_vcpu *vcpu,
 				vcpu->arch.reserved_gpa_bits,
 				context->root_level, context->nx,
 				guest_cpuid_has(vcpu, X86_FEATURE_GBPAGES),
-				is_pse(vcpu), guest_cpuid_is_amd(vcpu));
+				is_cr4_pse(context),
+				guest_cpuid_is_amd(vcpu));
 }
 
 static void
