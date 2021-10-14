3008d4874daa0aea166f83008571af8f99c66f65
Vitaly Kuznetsov <vkuznets@redhat.com>
2021-10-14 11:26:02 +0200

    KVM: x86: Introduce KVM_GET_SREGS2 / KVM_SET_SREGS2
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2009340
    
    commit 6dba940352038b56db9b591b172fb2ec76a5fd5e
    Author: Maxim Levitsky <mlevitsk@redhat.com>
    Date:   Mon Jun 7 12:02:02 2021 +0300
    
        KVM: x86: Introduce KVM_GET_SREGS2 / KVM_SET_SREGS2
    
        This is a new version of KVM_GET_SREGS / KVM_SET_SREGS.
    
        It has the following changes:
           * Has flags for future extensions
           * Has vcpu's PDPTRs, allowing to save/restore them on migration.
           * Lacks obsolete interrupt bitmap (done now via KVM_SET_VCPU_EVENTS)
    
        New capability, KVM_CAP_SREGS2 is added to signal
        the userspace of this ioctl.
    
        Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>
        Message-Id: <20210607090203.133058-8-mlevitsk@redhat.com>
        Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -1081,6 +1081,7 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_SGX_ATTRIBUTE 196
 #define KVM_CAP_VM_COPY_ENC_CONTEXT_FROM 197
 #define KVM_CAP_HYPERV_ENFORCE_CPUID 199
+#define KVM_CAP_SREGS2 200
 
 #ifdef KVM_CAP_IRQ_ROUTING
 
@@ -1617,6 +1618,9 @@ struct kvm_xen_hvm_attr {
 /* Per-vCPU Xen attributes */
 #define KVM_XEN_VCPU_GET_ATTR	_IOWR(KVMIO, 0xca, struct kvm_xen_vcpu_attr)
 #define KVM_XEN_VCPU_SET_ATTR	_IOW(KVMIO,  0xcb, struct kvm_xen_vcpu_attr)
+
+#define KVM_GET_SREGS2             _IOR(KVMIO,  0xcc, struct kvm_sregs2)
+#define KVM_SET_SREGS2             _IOW(KVMIO,  0xcd, struct kvm_sregs2)
 
 struct kvm_xen_vcpu_attr {
 	__u16 type;
