f873ee038ca9e4b0d15c38c7cf9890fdc35114ae
Nico Pache <npache@redhat.com>
2021-10-15 11:12:48 -0400

    bdi: replace BDI_CAP_STABLE_WRITES with a queue and a sb flag
    commit 1cb039f3dc1619eb795c54aad0a98fdb379b4237
    Author: Christoph Hellwig <hch@lst.de>
    Date:   Thu Sep 24 08:51:38 2020 +0200
    
        bdi: replace BDI_CAP_STABLE_WRITES with a queue and a sb flag
    
        The BDI_CAP_STABLE_WRITES is one of the few bits of information in the
        backing_dev_info shared between the block drivers and the writeback code.
        To help untangling the dependency replace it with a queue flag and a
        superblock flag derived from it.  This also helps with the case of e.g.
        a file system requiring stable writes due to its own checksumming, but
        not forcing it on other users of the block device like the swap code.
    
        One downside is that we an't support the stable_pages_required bdi
        attribute in sysfs anymore.  It is replaced with a queue attribute which
        also is writable for easier testing.
    
        Signed-off-by: Christoph Hellwig <hch@lst.de>
        Reviewed-by: Jan Kara <jack@suse.cz>
        Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
        Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Conflicts:
           drivers/md/dm-table.c: only preform the changes in the commit
           drivers/nvme/host/multipath.c: was not expecting commit 73a1a2298f3e
            ("nvme-multipath: set nr_zones for zoned namespaces")
           include/linux/blkdev.h: add QUEUE_FLAG_STABLE_WRITES as #32 and
            update block/blk-mq-debugfs.c accordingly
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1990580
    Signed-off-by: Nico Pache <npache@redhat.com>

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -322,6 +322,7 @@ queue_##name##_store(struct request_queue *q, const char *page, size_t count) \
 QUEUE_SYSFS_BIT_FNS(nonrot, NONROT, 1);
 QUEUE_SYSFS_BIT_FNS(random, ADD_RANDOM, 0);
 QUEUE_SYSFS_BIT_FNS(iostats, IO_STAT, 0);
+QUEUE_SYSFS_BIT_FNS(stable_writes, STABLE_WRITES, 0);
 #undef QUEUE_SYSFS_BIT_FNS
 
 static ssize_t queue_zoned_show(struct request_queue *q, char *page)
@@ -646,6 +647,7 @@ static struct queue_sysfs_entry queue_unpriv_sgio_entry = {
 QUEUE_RW_ENTRY(queue_nonrot, "rotational");
 QUEUE_RW_ENTRY(queue_iostats, "iostats");
 QUEUE_RW_ENTRY(queue_random, "add_random");
+QUEUE_RW_ENTRY(queue_stable_writes, "stable_writes");
 
 static struct attribute *default_attrs[] = {
 	&queue_requests_entry.attr,
@@ -677,6 +679,7 @@ static struct attribute *default_attrs[] = {
 	&queue_nomerges_entry.attr,
 	&queue_rq_affinity_entry.attr,
 	&queue_iostats_entry.attr,
+	&queue_stable_writes_entry.attr,
 	&queue_random_entry.attr,
 	&queue_poll_entry.attr,
 	&queue_wc_entry.attr,
