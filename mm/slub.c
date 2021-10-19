71e1a0dd68e073e36cddc682ed20129014598de3
Scott Wood <swood@redhat.com>
2021-10-19 02:02:05 -0400

    mm, slub: don't call flush_all() from slab_debug_trace_open()
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2011667
    
    commit eafb1d64030abf5f885026c2074d120c13e0ca9d
    Author: Vlastimil Babka <vbabka@suse.cz>
    Date:   Fri May 28 14:32:10 2021 +0200
    
        mm, slub: don't call flush_all() from slab_debug_trace_open()
    
        slab_debug_trace_open() can only be called on caches with SLAB_STORE_USER flag
        and as with all slub debugging flags, such caches avoid cpu or percpu partial
        slabs altogether, so there's nothing to flush.
    
        Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
        Acked-by: Christoph Lameter <cl@linux.com>
    
    Signed-off-by: Scott Wood <swood@redhat.com>
    
    Conflicts:
    	mm/slub.c - change applied to list_locations instead of
    	  slab_debug_trace_open(); the SLAB_STORE_USER-only condition
    	  still holds.  This conflict is due to missing commit
    	  64dd68497be7 ("mm: slub: move sysfs slab alloc/free interfaces
    	  to debugfs"), which I didn't include because it changes
    	  a usersyace interface.

--- mm/slub.c
+++ mm/slub.c
@@ -4803,8 +4803,6 @@ static int list_locations(struct kmem_cache *s, char *buf,
 			     GFP_KERNEL)) {
 		return sysfs_emit(buf, "Out of memory\n");
 	}
-	/* Push back cpu slabs */
-	flush_all(s);
 
 	for_each_kmem_cache_node(s, node, n) {
 		unsigned long flags;
