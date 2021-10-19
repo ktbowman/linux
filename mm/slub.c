785bc8a9d3c783011b2e5f34ac12c001a3e80563
Scott Wood <swood@redhat.com>
2021-10-19 05:17:04 -0400

    mm, slub: check new pages with restored irqs
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2011667
    
    commit 9f101ee89465e0b2c11b477f5b55e03039b2c308
    Author: Vlastimil Babka <vbabka@suse.cz>
    Date:   Tue May 11 16:56:09 2021 +0200
    
        mm, slub: check new pages with restored irqs
    
        Building on top of the previous patch, re-enable irqs before checking new
        pages. alloc_debug_processing() is now called with enabled irqs so we need to
        remove VM_BUG_ON(!irqs_disabled()); in check_slab() - there doesn't seem to be
        a need for it anyway.
    
        Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    
    Signed-off-by: Scott Wood <swood@redhat.com>

--- mm/slub.c
+++ mm/slub.c
@@ -963,8 +963,6 @@ static int check_slab(struct kmem_cache *s, struct page *page)
 {
 	int maxobj;
 
-	VM_BUG_ON(!irqs_disabled());
-
 	if (!PageSlab(page)) {
 		slab_err(s, page, "Not a valid slab page");
 		return 0;
@@ -2736,10 +2734,10 @@ new_objects:
 	lockdep_assert_irqs_disabled();
 
 	freelist = get_partial(s, gfpflags, node, &page);
+	local_irq_restore(flags);
 	if (freelist)
 		goto check_new_page;
 
-	local_irq_restore(flags);
 	put_cpu_ptr(s->cpu_slab);
 	page = new_slab(s, gfpflags, node);
 	c = get_cpu_ptr(s->cpu_slab);
@@ -2749,7 +2747,6 @@ new_objects:
 		return NULL;
 	}
 
-	local_irq_save(flags);
 	/*
 	 * No other reference to the page yet so we can
 	 * muck around with it freely without cmpxchg
@@ -2764,7 +2761,6 @@ check_new_page:
 	if (kmem_cache_debug(s)) {
 		if (!alloc_debug_processing(s, page, freelist, addr)) {
 			/* Slab failed checks. Next slab needed */
-			local_irq_restore(flags);
 			goto new_slab;
 		} else {
 			/*
@@ -2782,6 +2778,7 @@ check_new_page:
 		 */
 		goto return_single;
 
+	local_irq_save(flags);
 	if (unlikely(c->page))
 		flush_slab(s, c);
 	c->page = page;
@@ -2790,6 +2787,7 @@ check_new_page:
 
 return_single:
 
+	local_irq_save(flags);
 	if (unlikely(c->page))
 		flush_slab(s, c);
 	c->page = page;
