a35612a908130cb0c487477f64cb53401ad2a4d1
Michel Dänzer <mdaenzer@redhat.com>
2021-10-20 12:44:14 +0200

    drm/i915/display/adlp: Add new PSR2 workarounds
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1988103
    Upstream Status: https://anongit.freedesktop.org/git/drm-intel.git
    
    Conflicts:
            drivers/gpu/drm/i915/display/intel_psr.c
    
    commit 641dd82ffa9daad38f630d9f4a36e4875fd4e0c6
    Author:     José Roberto de Souza <jose.souza@intel.com>
    AuthorDate: Tue Sep 14 14:25:07 2021 -0700
    Commit:     José Roberto de Souza <jose.souza@intel.com>
    CommitDate: Fri Sep 17 10:58:49 2021 -0700
    
        Wa_16014451276 fixes the starting coordinate for PSR2 selective
        updates. CHICKEN_TRANS definition of the workaround bit has a wrong
        name based on workaround definition and HSD.
    
        Wa_14014971508 allows the screen to continue to be updated when
        coming back from DC5/DC6 and SF_SINGLE_FULL_FRAME bit is not kept
        set in PSR2_MAN_TRK_CTL.
    
        Wa_16012604467 fixes underruns when exiting PSR2 when it is in one
        of its internal states.
    
        Wa_14014971508 is still in pending status in BSpec but by
        the time this is reviewed and ready to be merged it will be finalized.
    
        v2:
        - renamed register to ADLP_1_BASED_X_GRANULARITY
        - added comment about all ADL-P supported panels being 1 based X
        granularity
    
        BSpec: 54369
        BSpec: 50054
        Cc: Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>
        Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
        Reviewed-by: Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>
        Link: https://patchwork.freedesktop.org/patch/msgid/20210914212507.177511-5-jose.souza@intel.com
    
    Signed-off-by: Michel Dänzer <mdaenzer@redhat.com>

--- drivers/gpu/drm/i915/display/intel_psr.c
+++ drivers/gpu/drm/i915/display/intel_psr.c
@@ -1135,6 +1135,16 @@ static void intel_psr_enable_source(struct intel_dp *intel_dp)
 	}
 
 	/*
+	 * Wa_16014451276:adlp
+	 * All supported adlp panels have 1-based X granularity, this may
+	 * cause issues if non-supported panels are used.
+	 */
+	if (IS_ALDERLAKE_P(dev_priv) &&
+	    intel_dp->psr.psr2_enabled)
+		intel_de_rmw(dev_priv, CHICKEN_TRANS(cpu_transcoder), 0,
+			     ADLP_1_BASED_X_GRANULARITY);
+
+	/*
 	 * Per Spec: Avoid continuous PSR exit by masking MEMUP and HPD also
 	 * mask LPSP to avoid dependency on other drivers that might block
 	 * runtime_pm besides preventing  other hw tracking issues now we
@@ -1179,6 +1189,11 @@ static void intel_psr_enable_source(struct intel_dp *intel_dp)
 			     TRANS_SET_CONTEXT_LATENCY(intel_dp->psr.transcoder),
 			     TRANS_SET_CONTEXT_LATENCY_MASK,
 			     TRANS_SET_CONTEXT_LATENCY_VALUE(1));
+
+	/* Wa_16012604467:adlp */
+	if (IS_ALDERLAKE_P(dev_priv) && intel_dp->psr.psr2_enabled)
+		intel_de_rmw(dev_priv, CLKGATE_DIS_MISC, 0,
+			     CLKGATE_DIS_MISC_DMASC_GATING_DIS);
 }
 
 static bool psr_interrupt_error_check(struct intel_dp *intel_dp)
@@ -1365,6 +1380,11 @@ static void intel_psr_disable_locked(struct intel_dp *intel_dp)
 			     TRANS_SET_CONTEXT_LATENCY(intel_dp->psr.transcoder),
 			     TRANS_SET_CONTEXT_LATENCY_MASK, 0);
 
+	/* Wa_16012604467:adlp */
+	if (IS_ALDERLAKE_P(dev_priv) && intel_dp->psr.psr2_enabled)
+		intel_de_rmw(dev_priv, CLKGATE_DIS_MISC,
+			     CLKGATE_DIS_MISC_DMASC_GATING_DIS, 0);
+
 	/* Disable PSR on Sink */
 	drm_dp_dpcd_writeb(&intel_dp->aux, DP_PSR_EN_CFG, 0);
 
@@ -1542,8 +1562,13 @@ static void psr2_man_trk_ctl_calc(struct intel_crtc_state *crtc_state,
 	u32 val = PSR2_MAN_TRK_CTL_ENABLE;
 
 	if (full_update) {
+		/*
+		 * Wa_14014971508:adlp
+		 * SINGLE_FULL_FRAME bit is not hold in register so can not be
+		 * restored by DMC, so using CONTINUOS_FULL_FRAME to mimic that
+		 */
 		if (IS_ALDERLAKE_P(dev_priv))
-			val |= ADLP_PSR2_MAN_TRK_CTL_SF_SINGLE_FULL_FRAME;
+			val |= ADLP_PSR2_MAN_TRK_CTL_SF_CONTINUOS_FULL_FRAME;
 		else
 			val |= PSR2_MAN_TRK_CTL_SF_SINGLE_FULL_FRAME;
 
