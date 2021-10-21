1769bae4cb5b364b6a44cab60d76fa1a52105483
Jerome Marchand <jmarchan@redhat.com>
2021-10-21 16:06:08 +0200

    tracing: Replace deprecated CPU-hotplug functions.
    Bugzilla: http://bugzilla.redhat.com/1979382
    
    commit 99c37d1a63eafcd3673302a7953df760b46d0f6f
    Author: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Date:   Tue Aug 3 16:16:19 2021 +0200
    
        tracing: Replace deprecated CPU-hotplug functions.
    
        The functions get_online_cpus() and put_online_cpus() have been
        deprecated during the CPU hotplug rework. They map directly to
        cpus_read_lock() and cpus_read_unlock().
    
        Replace deprecated CPU-hotplug functions with the official version.
        The behavior remains unchanged.
    
        Link: https://lkml.kernel.org/r/20210803141621.780504-37-bigeasy@linutronix.de
    
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Ingo Molnar <mingo@redhat.com>
        Acked-by: Daniel Bristot de Oliveira <bristot@kernel.org>
        Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
        Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    
    Signed-off-by: Jerome Marchand <jmarchan@redhat.com>

--- kernel/trace/trace_hwlat.c
+++ kernel/trace/trace_hwlat.c
@@ -317,10 +317,10 @@ static void move_to_next_cpu(void)
 	if (!cpumask_equal(current_mask, current->cpus_ptr))
 		goto change_mode;
 
-	get_online_cpus();
+	cpus_read_lock();
 	cpumask_and(current_mask, cpu_online_mask, tr->tracing_cpumask);
 	next_cpu = cpumask_next(raw_smp_processor_id(), current_mask);
-	put_online_cpus();
+	cpus_read_unlock();
 
 	if (next_cpu >= nr_cpu_ids)
 		next_cpu = cpumask_first(current_mask);
@@ -390,7 +390,7 @@ static void stop_single_kthread(void)
 	struct hwlat_kthread_data *kdata = get_cpu_data();
 	struct task_struct *kthread;
 
-	get_online_cpus();
+	cpus_read_lock();
 	kthread = kdata->kthread;
 
 	if (!kthread)
@@ -400,7 +400,7 @@ static void stop_single_kthread(void)
 	kdata->kthread = NULL;
 
 out_put_cpus:
-	put_online_cpus();
+	cpus_read_unlock();
 }
 
 
@@ -417,14 +417,14 @@ static int start_single_kthread(struct trace_array *tr)
 	struct task_struct *kthread;
 	int next_cpu;
 
-	get_online_cpus();
+	cpus_read_lock();
 	if (WARN_ON(kdata->kthread))
 		goto out_put_cpus;
 
 	kthread = kthread_create(kthread_fn, NULL, "hwlatd");
 	if (IS_ERR(kthread)) {
 		pr_err(BANNER "could not start sampling thread\n");
-		put_online_cpus();
+		cpus_read_unlock();
 		return -ENOMEM;
 	}
 
@@ -444,7 +444,7 @@ static int start_single_kthread(struct trace_array *tr)
 	wake_up_process(kthread);
 
 out_put_cpus:
-	put_online_cpus();
+	cpus_read_unlock();
 	return 0;
 }
 
@@ -471,10 +471,10 @@ static void stop_per_cpu_kthreads(void)
 {
 	unsigned int cpu;
 
-	get_online_cpus();
+	cpus_read_lock();
 	for_each_online_cpu(cpu)
 		stop_cpu_kthread(cpu);
-	put_online_cpus();
+	cpus_read_unlock();
 }
 
 /*
@@ -507,7 +507,7 @@ static void hwlat_hotplug_workfn(struct work_struct *dummy)
 
 	mutex_lock(&trace_types_lock);
 	mutex_lock(&hwlat_data.lock);
-	get_online_cpus();
+	cpus_read_lock();
 
 	if (!hwlat_busy || hwlat_data.thread_mode != MODE_PER_CPU)
 		goto out_unlock;
@@ -518,7 +518,7 @@ static void hwlat_hotplug_workfn(struct work_struct *dummy)
 	start_cpu_kthread(cpu);
 
 out_unlock:
-	put_online_cpus();
+	cpus_read_unlock();
 	mutex_unlock(&hwlat_data.lock);
 	mutex_unlock(&trace_types_lock);
 }
@@ -574,7 +574,7 @@ static int start_per_cpu_kthreads(struct trace_array *tr)
 	unsigned int cpu;
 	int retval;
 
-	get_online_cpus();
+	cpus_read_lock();
 	/*
 	 * Run only on CPUs in which hwlat is allowed to run.
 	 */
@@ -588,12 +588,12 @@ static int start_per_cpu_kthreads(struct trace_array *tr)
 		if (retval)
 			goto out_error;
 	}
-	put_online_cpus();
+	cpus_read_unlock();
 
 	return 0;
 
 out_error:
-	put_online_cpus();
+	cpus_read_unlock();
 	stop_per_cpu_kthreads();
 	return retval;
 }
