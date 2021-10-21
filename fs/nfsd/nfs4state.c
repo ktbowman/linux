6f83fa50838747e0504b49829ee594fc29a22cb5
Steve Dickson <steved@redhat.com>
2021-10-21 16:26:29 -0400

    nfsd: move fsnotify on client creation outside spinlock
    Bugzilla: http://bugzilla.redhat.com/1719330
    
    commit 934bd07fae7e55232845f909f78873ab8678ca74
    Author: J. Bruce Fields <bfields@redhat.com>
    Date:   Tue May 25 14:53:44 2021 -0400
    
        nfsd: move fsnotify on client creation outside spinlock
    
        This was causing a "sleeping function called from invalid context"
        warning.
    
        I don't think we need the set_and_test_bit() here; clients move from
        unconfirmed to confirmed only once, under the client_lock.
    
        The (conf == unconf) is a way to check whether we're in that confirming
        case, hopefully that's not too obscure.
    
        Fixes: 472d155a0631 "nfsd: report client confirmation status in "info" file"
        Signed-off-by: J. Bruce Fields <bfields@redhat.com>
    
    Signed-off-by: Steve Dickson <steved@redhat.com>
    Conflicts: Removed non-existing trace_nfsd_clid_confirmed() tracepoint
    Conflicts: Replace fsnotify_dentry() with fsnotify_parent()

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -2742,10 +2742,7 @@ move_to_confirmed(struct nfs4_client *clp)
 	list_move(&clp->cl_idhash, &nn->conf_id_hashtbl[idhashval]);
 	rb_erase(&clp->cl_namenode, &nn->unconf_name_tree);
 	add_clp_to_name_tree(clp, &nn->conf_name_tree);
-	if (!test_and_set_bit(NFSD4_CLIENT_CONFIRMED, &clp->cl_flags) &&
-	    clp->cl_nfsd_dentry &&
-	    clp->cl_nfsd_info_dentry)
-		fsnotify_dentry(clp->cl_nfsd_info_dentry, FS_MODIFY);
+	set_bit(NFSD4_CLIENT_CONFIRMED, &clp->cl_flags);
 	renew_client_locked(clp);
 }
 
@@ -3392,6 +3389,8 @@ nfsd4_create_session(struct svc_rqst *rqstp,
 	/* cache solo and embedded create sessions under the client_lock */
 	nfsd4_cache_create_session(cr_ses, cs_slot, status);
 	spin_unlock(&nn->client_lock);
+	if (conf == unconf)
+		fsnotify_parent(NULL, conf->cl_nfsd_info_dentry, FS_MODIFY);
 	/* init connection and backchannel */
 	nfsd4_init_conn(rqstp, conn, new);
 	nfsd4_put_session(new);
@@ -3992,6 +3991,8 @@ nfsd4_setclientid_confirm(struct svc_rqst *rqstp,
 	}
 	get_client_locked(conf);
 	spin_unlock(&nn->client_lock);
+	if (conf == unconf)
+		fsnotify_parent(NULL, conf->cl_nfsd_info_dentry, FS_MODIFY);
 	nfsd4_probe_callback(conf);
 	spin_lock(&nn->client_lock);
 	put_client_renew_locked(conf);
