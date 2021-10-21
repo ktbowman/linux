b7166e69be6e4b453efc59e8e1a1c66f68ee87c3
Yauheni Kaliuta <ykaliuta@redhat.com>
2021-10-21 16:04:05 +0300

    bpf: Add kconfig knob for disabling unpriv bpf by default
    Bugzilla: http://bugzilla.redhat.com/1944799
    
    Conflicts: missing file Documentation/admin-guide/sysctl/kernel.rst,
     patch RH-only 2d9ac8d9dd67 ("[kernel] bpf: set unprivileged_bpf_disabled to 1 by default, add a boot parameter"
     missing f461d2dcd511 ("sysctl: avoid forward declarations")
    
     To make it working RHEL way set sysctl_unprivileged_bpf_disabled to
     1 instead of 2.
    
    commit 08389d888287c3823f80b0216766b71e17f0aba5
    Author: Daniel Borkmann <daniel@iogearbox.net>
    Date:   Tue May 11 22:35:17 2021 +0200
    
        bpf: Add kconfig knob for disabling unpriv bpf by default
    
        Add a kconfig knob which allows for unprivileged bpf to be disabled by default.
        If set, the knob sets /proc/sys/kernel/unprivileged_bpf_disabled to value of 2.
    
        This still allows a transition of 2 -> {0,1} through an admin. Similarly,
        this also still keeps 1 -> {1} behavior intact, so that once set to permanently
        disabled, it cannot be undone aside from a reboot.
    
        We've also added extra2 with max of 2 for the procfs handler, so that an admin
        still has a chance to toggle between 0 <-> 2.
    
        Either way, as an additional alternative, applications can make use of CAP_BPF
        that we added a while ago.
    
        Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
        Signed-off-by: Alexei Starovoitov <ast@kernel.org>
        Link: https://lore.kernel.org/bpf/74ec548079189e4e4dffaeb42b8987bb3c852eee.1620765074.git.daniel@iogearbox.net
    
    Signed-off-by: Yauheni Kaliuta <ykaliuta@redhat.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -61,7 +61,8 @@ static DEFINE_IDR(link_idr);
 static DEFINE_SPINLOCK(link_idr_lock);
 
 /* RHEL-only: default to 1 */
-int sysctl_unprivileged_bpf_disabled __read_mostly = 1;
+int sysctl_unprivileged_bpf_disabled __read_mostly =
+	IS_BUILTIN(CONFIG_BPF_UNPRIV_DEFAULT_OFF) ? 1 : 0;
 
 static int __init unprivileged_bpf_setup(char *str)
 {
