22dca8d857899175400f80fde5f01c9c8a5c529b
Michael Petlan <mpetlan@redhat.com>
2021-10-26 15:01:33 +0200

    perf: Add support for event removal on exec
    Bugzilla: https://bugzilla.redhat.com/2003981
    
    upstream
    ========
    commit 2e498d0a74e5b88a6689ae1b811f247f91ff188e
    Author: Marco Elver <elver@google.com>
    Date: Thu Apr 8 12:35:59 2021 +0200
    
    description
    ===========
    Adds bit perf_event_attr::remove_on_exec, to support removing an event
    from a task on exec.
    
    This option supports the case where an event is supposed to be
    process-wide only, and should not propagate beyond exec, to limit
    monitoring to the original process image only.
    
    KABI
    ====
    The perf related structures are incorporated into some key
    core structures (like 'struct perf_event *' pointer is used
    within 'struct thread_struct', 'struct perf_event_context*'
    pointer is in 'struct task_struct'). This breaks KABI any
    time we change perf related struct, while at the moment we
    don't have any perf related function on KABI list.
    
    Signed-off-by: Michael Petlan <mpetlan@redhat.com>

--- include/uapi/linux/perf_event.h
+++ include/uapi/linux/perf_event.h
@@ -410,7 +410,8 @@ struct perf_event_attr {
 				text_poke      :  1, /* include text poke events */
 				build_id       :  1, /* use build id in mmap2 events */
 				inherit_thread :  1, /* children only inherit if cloned with CLONE_THREAD */
-				__reserved_1   : 28;
+				remove_on_exec :  1, /* event is removed from task on exec */
+				__reserved_1   : 27;
 #else
 				__reserved_1   : 35;
 #endif /*  __GENKSYMS__ */
