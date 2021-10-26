4d6b3050f8a0ed2605522fc57fe569d63a9e42ed
Michael Petlan <mpetlan@redhat.com>
2021-10-26 15:01:32 +0200

    perf: Support only inheriting events if cloned with CLONE_THREAD
    Bugzilla: https://bugzilla.redhat.com/2003981
    
    upstream
    ========
    commit 2b26f0aa004995f49f7b6f4100dd0e4c39a9ed5f
    Author: Marco Elver <elver@google.com>
    Date: Thu Apr 8 12:35:58 2021 +0200
    
    description
    ===========
    Adds bit perf_event_attr::inherit_thread, to restricting inheriting
    events only if the child was cloned with CLONE_THREAD.
    
    This option supports the case where an event is supposed to be
    process-wide only (including subthreads), but should not propagate
    beyond the current process's shared environment.
    
    KABI
    ====
    The perf related structures are incorporated into some key
    core structures (like 'struct perf_event *' pointer is used
    within 'struct thread_struct', 'struct perf_event_context*'
    pointer is in 'struct task_struct'). This breaks KABI any
    time we change perf related struct, while at the moment we
    don't have any perf related function on KABI list.
    
    Signed-off-by: Michael Petlan <mpetlan@redhat.com>

--- include/uapi/linux/perf_event.h
+++ include/uapi/linux/perf_event.h
@@ -409,7 +409,8 @@ struct perf_event_attr {
 				cgroup         :  1, /* include cgroup events */
 				text_poke      :  1, /* include text poke events */
 				build_id       :  1, /* use build id in mmap2 events */
-				__reserved_1   : 29;
+				inherit_thread :  1, /* children only inherit if cloned with CLONE_THREAD */
+				__reserved_1   : 28;
 #else
 				__reserved_1   : 35;
 #endif /*  __GENKSYMS__ */
