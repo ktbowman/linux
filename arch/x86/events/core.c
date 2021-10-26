86bb360202ee39dd3be77e125fd4e76797a630e3
Michael Petlan <mpetlan@redhat.com>
2021-10-26 15:01:33 +0200

    perf/x86: Move cpuc->running into P4 specific code
    Bugzilla: https://bugzilla.redhat.com/2003981
    
    upstream
    ========
    commit 46ade4740bbf9bf4e804ddb2c85845cccd219f3c
    Author: Kan Liang <kan.liang@linux.intel.com>
    Date: Wed Apr 14 07:36:29 2021 -0700
    
    description
    ===========
    The 'running' variable is only used in the P4 PMU. Current perf sets the
    variable in the critical function x86_pmu_start(), which wastes cycles
    for everybody not running on P4.
    
    Move cpuc->running into the P4 specific p4_pmu_enable_event().
    
    Add a static per-CPU 'p4_running' variable to replace the 'running'
    variable in the struct cpu_hw_events. Saves space for the generic
    structure.
    
    The p4_pmu_enable_all() also invokes the p4_pmu_enable_event(), but it
    should not set cpuc->running. Factor out __p4_pmu_enable_event() for
    p4_pmu_enable_all().
    
    Conflicts:
    ==========
    Different context, due to not-yet-backported static_call-related
    perf changes.
    
    Signed-off-by: Michael Petlan <mpetlan@redhat.com>

--- arch/x86/events/core.c
+++ arch/x86/events/core.c
@@ -1484,7 +1484,6 @@ static void x86_pmu_start(struct perf_event *event, int flags)
 
 	cpuc->events[idx] = event;
 	__set_bit(idx, cpuc->active_mask);
-	__set_bit(idx, cpuc->running);
 	x86_pmu.enable(event);
 	perf_event_update_userpage(event);
 }
