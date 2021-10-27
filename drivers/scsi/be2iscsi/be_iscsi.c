21b18e245ef8fdee44c72879a2001d952609b481
Chris Leech <cleech@redhat.com>
2021-10-27 12:50:16 -0700

    scsi: iscsi: Drop suspend calls from ep_disconnect
    libiscsi will now suspend the send/tx queue for the drivers so we can drop
    it from the drivers ep_disconnect.
    
    Link: https://lore.kernel.org/r/20210525181821.7617-4-michael.christie@oracle.com
    Reviewed-by: Lee Duncan <lduncan@suse.com>
    Signed-off-by: Mike Christie <michael.christie@oracle.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 27e986289e739d08c1a4861cc3d3ec9b3a60845e)
    Signed-off-by: Chris Leech <cleech@redhat.com>
    Bugzilla: http://bugzilla.redhat.com/2011470

--- drivers/scsi/be2iscsi/be_iscsi.c
+++ drivers/scsi/be2iscsi/be_iscsi.c
@@ -1294,7 +1294,6 @@ static int beiscsi_conn_close(struct beiscsi_endpoint *beiscsi_ep)
 void beiscsi_ep_disconnect(struct iscsi_endpoint *ep)
 {
 	struct beiscsi_endpoint *beiscsi_ep;
-	struct beiscsi_conn *beiscsi_conn;
 	struct beiscsi_hba *phba;
 	uint16_t cri_index;
 
@@ -1311,11 +1310,6 @@ void beiscsi_ep_disconnect(struct iscsi_endpoint *ep)
 			      cri_index,
 			      beiscsi_ep->ep_cid);
 		return;
-	}
-
-	if (beiscsi_ep->conn) {
-		beiscsi_conn = beiscsi_ep->conn;
-		iscsi_suspend_queue(beiscsi_conn->conn);
 	}
 
 	if (!beiscsi_hba_is_online(phba)) {
