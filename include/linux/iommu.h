1608886d8dc5ae055d3ca6c92db3e62afc4fa041
Jerry Snitselaar <jsnitsel@redhat.com>
2021-10-27 09:31:46 -0700

    iommu: Pull IOVA cookie management into the core
    Bugzilla: https://bugzilla.redhat.com/2004349
    Upstream Status: kernel/git/torvalds/linux.git
    Conflict: Context diff due to include from rhel
              specific patch for gigabyte arm systems.
    	  RH_KABI for iommu_domain change.
    
    commit 46983fcd67ac5a830d41ebe3755314db67a6dd16
    Author: Robin Murphy <robin.murphy@arm.com>
    Date:   Wed Aug 11 13:21:15 2021 +0100
    
        iommu: Pull IOVA cookie management into the core
    
        Now that everyone has converged on iommu-dma for IOMMU_DOMAIN_DMA
        support, we can abandon the notion of drivers being responsible for the
        cookie type, and consolidate all the management into the core code.
    
        CC: Yong Wu <yong.wu@mediatek.com>
        CC: Chunyan Zhang <chunyan.zhang@unisoc.com>
        CC: Maxime Ripard <mripard@kernel.org>
        Tested-by: Heiko Stuebner <heiko@sntech.de>
        Tested-by: Marek Szyprowski <m.szyprowski@samsung.com>
        Tested-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
        Reviewed-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
        Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
        Signed-off-by: Robin Murphy <robin.murphy@arm.com>
        Link: https://lore.kernel.org/r/46a2c0e7419c7d1d931762dc7b6a69fa082d199a.1628682048.git.robin.murphy@arm.com
        Signed-off-by: Joerg Roedel <jroedel@suse.de>
    
    (cherry picked from commit 46983fcd67ac5a830d41ebe3755314db67a6dd16)
    Cc: Myron Stowe <myron.stowe@redhat.com>
    Cc: Alex Williamson <alex.williamson@redhat.com>
    Cc: Donald Dutile <ddutile@redhat.com>
    Signed-off-by: Jerry Snitselaar <jsnitsel@redhat.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -53,6 +53,7 @@ struct iommu_domain;
 struct notifier_block;
 struct iommu_sva;
 struct iommu_fault_event;
+struct iommu_dma_cookie;
 
 /* iommu fault flags */
 #define IOMMU_FAULT_READ	0x0
@@ -99,7 +100,7 @@ struct iommu_domain {
 	iommu_fault_handler_t handler;
 	void *handler_token;
 	struct iommu_domain_geometry geometry;
-	void *iova_cookie;
+	RH_KABI_BROKEN_REPLACE(void *iova_cookie, struct iommu_dma_cookie *iova_cookie)
 };
 
 enum iommu_cap {
