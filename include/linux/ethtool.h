d505a1e353dc5f09ac69d2b168699dccaeafcd02
Ivan Vecera <ivecera@redhat.com>
2021-11-18 14:33:07 +0100

    ethtool: add FEC statistics
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2019089
    
    Conflicts:
    - need to use RH_KABI_USE to add new ethtool_ops callback into reserved
      area to preserve KABI
    - minor context conflict due to missing backport of commit 9b300495350346
      ("ethtool: drop get_settings and set_settings callbacks")
    
    commit be85dbfeb37c8c4d4344da2ee594d78034b82489
    Author: Jakub Kicinski <kuba@kernel.org>
    Date:   Thu Apr 15 15:53:15 2021 -0700
    
        ethtool: add FEC statistics
    
        Similarly to pause statistics add stats for FEC.
    
        The IEEE standard mandates two sets of counters:
         - 30.5.1.1.17 aFECCorrectedBlocks
         - 30.5.1.1.18 aFECUncorrectableBlocks
        where block is a block of bits FEC operates on.
        Each of these counters is defined per lane (PCS instance).
    
        Multiple vendors provide number of corrected _bits_ rather
        than/as well as blocks.
    
        This set adds the 2 standard-based block counters and a extra
        one for corrected bits.
    
        Counters are exposed to user space via netlink in new attributes.
        Each attribute carries an array of u64s, first element is
        the total count, and the following ones are a per-lane break down.
    
        Much like with pause stats the operation will not fail when driver
        does not implement the get_fec_stats callback (nor can the driver
        fail the operation by returning an error). If stats can't be
        reported the relevant attributes will be empty.
    
        Signed-off-by: Jakub Kicinski <kuba@kernel.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- include/linux/ethtool.h
+++ include/linux/ethtool.h
@@ -281,6 +281,39 @@ struct ethtool_pause_stats {
 	u64 rx_pause_frames;
 };
 
+#define ETHTOOL_MAX_LANES	8
+
+/**
+ * struct ethtool_fec_stats - statistics for IEEE 802.3 FEC
+ * @corrected_blocks: number of received blocks corrected by FEC
+ *	Reported to user space as %ETHTOOL_A_FEC_STAT_CORRECTED.
+ *
+ *	Equivalent to `30.5.1.1.17 aFECCorrectedBlocks` from the standard.
+ *
+ * @uncorrectable_blocks: number of received blocks FEC was not able to correct
+ *	Reported to user space as %ETHTOOL_A_FEC_STAT_UNCORR.
+ *
+ *	Equivalent to `30.5.1.1.18 aFECUncorrectableBlocks` from the standard.
+ *
+ * @corrected_bits: number of bits corrected by FEC
+ *	Similar to @corrected_blocks but counts individual bit changes,
+ *	not entire FEC data blocks. This is a non-standard statistic.
+ *	Reported to user space as %ETHTOOL_A_FEC_STAT_CORR_BITS.
+ *
+ * @lane: per-lane/PCS-instance counts as defined by the standard
+ * @total: error counts for the entire port, for drivers incapable of reporting
+ *	per-lane stats
+ *
+ * Drivers should fill in either only total or per-lane statistics, core
+ * will take care of adding lane values up to produce the total.
+ */
+struct ethtool_fec_stats {
+	struct ethtool_fec_stat {
+		u64 total;
+		u64 lanes[ETHTOOL_MAX_LANES];
+	} corrected_blocks, uncorrectable_blocks, corrected_bits;
+};
+
 #define ETH_MODULE_EEPROM_PAGE_LEN	128
 #define ETH_MODULE_MAX_I2C_ADDRESS	0x7f
 
@@ -465,6 +498,11 @@ struct ethtool_ops_extended_rh {
  *	fields should be ignored (use %__ETHTOOL_LINK_MODE_MASK_NBITS
  *	instead of the latter), any change to them will be overwritten
  *	by kernel. Returns a negative error code or zero.
+ * @get_fec_stats: Report FEC statistics.
+ *	Core will sum up per-lane stats to get the total.
+ *	Drivers must not zero statistics which they don't report. The stats
+ *	structure is initialized to ETHTOOL_STAT_NOT_SET indicating driver does
+ *	not report statistics.
  * @get_fecparam: Get the network device Forward Error Correction parameters.
  * @set_fecparam: Set the network device Forward Error Correction parameters.
  * @get_ethtool_phy_stats: Return extended statistics about the PHY device.
@@ -596,7 +634,8 @@ struct ethtool_ops {
 	RH_KABI_USE(8, int	(*get_module_eeprom_by_page)(struct net_device *dev,
 				   const struct ethtool_module_eeprom *page,
 				   struct netlink_ext_ack *extack))
-	RH_KABI_RESERVE(9)
+	RH_KABI_USE(9, void	(*get_fec_stats)(struct net_device *dev,
+				   struct ethtool_fec_stats *fec_stats))
 	RH_KABI_RESERVE(10)
 	RH_KABI_RESERVE(11)
 	RH_KABI_RESERVE(12)
