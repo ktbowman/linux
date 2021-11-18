a124323965063c2f0bb269268c770814cb6dffb7
Gopal Tiwari <gtiwari@redhat.com>
2021-11-18 10:57:29 +0530

    Bluetooth: Add hci_dev_lock to get/set device flags
    Bugzilla: http://bugzilla.redhat.com/1990477
    
    commit 3ca33e3fb4f919b66a72145a87bfeada079e750d
    Author: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
    Date:   Fri Jun 19 17:10:24 2020 -0700
    
        Bluetooth: Add hci_dev_lock to get/set device flags
    
        Adding hci_dev_lock since hci_conn_params_(lookup|add) require this
        lock.
    
        Suggested-by: Miao-chen Chou <mcchou@chromium.org>
        Signed-off-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
        Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    
    Signed-off-by: Gopal Tiwari <gtiwari@redhat.com>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -3896,6 +3896,8 @@ static int get_device_flags(struct sock *sk, struct hci_dev *hdev, void *data,
 	bt_dev_dbg(hdev, "Get device flags %pMR (type 0x%x)\n",
 		   &cp->addr.bdaddr, cp->addr.type);
 
+	hci_dev_lock(hdev);
+
 	if (cp->addr.type == BDADDR_BREDR) {
 		br_params = hci_bdaddr_list_lookup_with_flags(&hdev->whitelist,
 							      &cp->addr.bdaddr,
@@ -3922,6 +3924,8 @@ static int get_device_flags(struct sock *sk, struct hci_dev *hdev, void *data,
 	status = MGMT_STATUS_SUCCESS;
 
 done:
+	hci_dev_unlock(hdev);
+
 	return mgmt_cmd_complete(sk, hdev->id, MGMT_OP_GET_DEVICE_FLAGS, status,
 				&rp, sizeof(rp));
 }
@@ -3960,6 +3964,8 @@ static int set_device_flags(struct sock *sk, struct hci_dev *hdev, void *data,
 		goto done;
 	}
 
+	hci_dev_lock(hdev);
+
 	if (cp->addr.type == BDADDR_BREDR) {
 		br_params = hci_bdaddr_list_lookup_with_flags(&hdev->whitelist,
 							      &cp->addr.bdaddr,
@@ -3986,6 +3992,8 @@ static int set_device_flags(struct sock *sk, struct hci_dev *hdev, void *data,
 	}
 
 done:
+	hci_dev_unlock(hdev);
+
 	if (status == MGMT_STATUS_SUCCESS)
 		device_flags_changed(sk, hdev, &cp->addr.bdaddr, cp->addr.type,
 				     supported_flags, current_flags);
