1b7d3303ddc782ffd2cc74e5a9cf45544f8a398a
Phil Sutter <psutter@redhat.com>
2021-11-23 11:38:35 +0100

    netfilter: nf_tables: allow to specify stateful expression in set definition
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1983635
    Upstream Status: commit 65038428b2c6c
    
    commit 65038428b2c6c5be79d3f78a6b79c0cdc3a58a41
    Author: Pablo Neira Ayuso <pablo@netfilter.org>
    Date:   Tue Mar 17 14:13:46 2020 +0100
    
        netfilter: nf_tables: allow to specify stateful expression in set definition
    
        This patch allows users to specify the stateful expression for the
        elements in this set via NFTA_SET_EXPR. This new feature allows you to
        turn on counters for all of the elements in this set.
    
        Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    Signed-off-by: Phil Sutter <psutter@redhat.com>

--- include/net/netfilter/nf_tables.h
+++ include/net/netfilter/nf_tables.h
@@ -417,6 +417,7 @@ void nft_unregister_set(struct nft_set_type *type);
  *	@policy: set parameterization (see enum nft_set_policies)
  *	@udlen: user data length
  *	@udata: user data
+ *	@expr: stateful expression
  * 	@ops: set ops
  * 	@flags: set flags
  *	@genmask: generation mask
@@ -445,6 +446,7 @@ struct nft_set {
 	u16				policy;
 	u16				udlen;
 	unsigned char			*udata;
+	struct nft_expr			*expr;
 	/* runtime data below here */
 	const struct nft_set_ops	*ops ____cacheline_aligned;
 	u16				flags:14,
