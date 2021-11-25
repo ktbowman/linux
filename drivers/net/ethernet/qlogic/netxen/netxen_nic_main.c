844a3a38bba605d99b1c21fa6aace57dd81935c4
Prarit Bhargava <prarit@redhat.com>
2021-11-25 10:14:14 -0500

    netxen: Mark nic driver unmaintained
    Bugzilla: https://bugzilla.redhat.com/2019378
    
    Upstream Status: RHEL only, https://gitlab.com/cki-project/kernel-ark
    commit d5c4f7aa9671eda599cc4599fde4df79de21b1a1
    Author: Prarit Bhargava <prarit@redhat.com>
    Date:   Tue Nov 23 20:22:08 2021 -0500
    
        netxen: Mark nic driver unmaintained
    
        Mark netxen nic driver unmaintained.
    
        Signed-off-by: Prarit Bhargava <prarit@redhat.com>
    
    Signed-off-by: Prarit Bhargava <prarit@redhat.com>

--- drivers/net/ethernet/qlogic/netxen/netxen_nic_main.c
+++ drivers/net/ethernet/qlogic/netxen/netxen_nic_main.c
@@ -1473,6 +1473,8 @@ netxen_nic_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	uint8_t revision_id;
 	u32 val;
 
+	pci_hw_unmaintained(ent, pdev);
+
 	if (pdev->revision >= NX_P3_A0 && pdev->revision <= NX_P3_B1) {
 		pr_warn("%s: chip revisions between 0x%x-0x%x will not be enabled\n",
 			module_name(THIS_MODULE), NX_P3_A0, NX_P3_B1);
@@ -3508,7 +3510,6 @@ static struct pci_driver netxen_driver = {
 static int __init netxen_init_module(void)
 {
 	printk(KERN_INFO "%s\n", netxen_nic_driver_string);
-	mark_hardware_unsupported(netxen_nic_driver_name);
 
 #ifdef CONFIG_INET
 	register_netdevice_notifier(&netxen_netdev_cb);
