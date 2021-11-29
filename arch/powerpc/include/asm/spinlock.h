ktbowman/linux

commit b6adcb3b7488ec2c6f784ace363ed0b20ebb1a48

tasklist_lock, ppc64: Provide infrastructure to support specific use of qrwlock
Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1978377
Upstream Status: RHEL only

On large ppcle64 SMP systems with many CPUs, the use of unfair rwlock
for tasklist_lock can sometimes lead to soft or hard lockups. To avoid
those problems, we have to switch to use qrwlock which is a fair rwlock.
Unfortunately, enabling qspinlock or qrwlock for the whole arch will
break kABI. As a result, we aim for the conversion of the internal
tasklist_lock to qrwlock only, the other rwlocks will still use the
unfair version.

This patch provides the infrastructure that enables the use of qrwlock
for tasklist_lock. For kernel files that use only use tasklist_lock
without other rwlocks, including <linux/rh_tasklist_lock.h> is enough
for the qrwlock conversion. For other kernel files that use tasklist_lock
and other rwlocks, the locking functions for tasklist_lock have to be
modified to use the qrwlock equivalent.

Note that the unfair ppcle rwlock is 4 bytes in size whereas
qrwlock needs 8 bytes. So we have to define qrwlock_t in
arch/powerpc/include/asm/spinlock.h.

The next patch will perform the actual conversion.

Signed-off-by: Waiman Long <longman@redhat.com>

arch/powerpc/include/asm/spinlock.h
@@ -14,5 +14,30 @@
 static inline void pv_spinlocks_init(void) { }
 #endif
 
+/*
+ * RHEL8 ppcle64 specific qrwlock APIs
+ */
+typedef struct {
+	atomic_long_t raw_lock;	/* 8 bytes */
+#ifdef CONFIG_DEBUG_SPINLOCK
+	unsigned int magic, owner_cpu;
+	void *owner;
+#endif
+#ifdef CONFIG_DEBUG_LOCK_ALLOC
+	struct lockdep_map dep_map;
+#endif
+} qrwlock_t;
+
+void qread_lock(qrwlock_t *lock);
+void qread_unlock(qrwlock_t *lock);
+void qwrite_lock_irq(qrwlock_t *lock);
+void qwrite_unlock_irq(qrwlock_t *lock);
+
+#define qrwlock_t		qrwlock_t
+#define qread_lock		qread_lock
+#define qread_unlock		qread_unlock
+#define qwrite_lock_irq		qwrite_lock_irq
+#define qwrite_unlock_irq	qwrite_unlock_irq
+
 #endif /* __KERNEL__ */
 #endif /* __ASM_SPINLOCK_H */

arch/powerpc/kernel/qlock.c
@@ -1,0 +1,34 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * This file is specific to RHEL8 ppcle64 architecture. Its purpose is to
+ * enable the use of qrwlock for tasklist_lock. Qspinlock will be used
+ * for fair queuing in qrwlock.
+ */
+#define CONFIG_PPC_QUEUED_SPINLOCKS
+#define CONFIG_QUEUED_SPINLOCKS
+#define CONFIG_QUEUED_RWLOCKS
+#define CONFIG_PARAVIRT_SPINLOCKS
+
+#include <linux/spinlock.h>
+#include "../../../kernel/locking/qspinlock.c"
+#include "../../../kernel/locking/qrwlock.c"
+
+void qread_lock(qrwlock_t *lock)
+{
+	__raw_read_lock((rwlock_t *)lock);
+}
+
+void qread_unlock(qrwlock_t *lock)
+{
+	read_unlock((rwlock_t *)lock);
+}
+
+void qwrite_lock_irq(qrwlock_t *lock)
+{
+	__raw_write_lock_irq((rwlock_t *)lock);
+}
+
+void qwrite_unlock_irq(qrwlock_t *lock)
+{
+	write_unlock_irq((rwlock_t *)lock);
+}

include/linux/rh_tasklist_lock.h
@@ -1,0 +1,41 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * RHEL8 ppcle64 arch uses qrwlock for tasklist_lock.
+ *
+ * Include this header file if tasklist_lock is the only lock accessed by
+ * the read_lock/unlock and write_lock_irq/unlock_irq functions. If not,
+ * manual replacement of those locking functions by their qread* and qwrite*
+ * equivalents is required.
+ */
+#ifndef __LINUX_RH_TASKLIST_LOCK_H
+#define __LINUX_RH_TASKLIST_LOCK_H
+#ifdef CONFIG_PPC64
+
+#undef read_lock
+#undef read_unlock
+#undef write_lock_irq
+#undef write_unlock_irq
+
+#define read_lock(l)		qread_lock(l)
+#define read_unlock(l)		qread_unlock(l)
+#define write_lock_irq(l)	qwrite_lock_irq(l)
+#define write_unlock_irq(l)	qwrite_unlock_irq(l)
+
+#ifdef CONFIG_DEBUG_SPINLOCK
+#define __QRW_LOCK_UNLOCKED(lockname)					\
+	(qrwlock_t)	{	.raw_lock = __ARCH_RW_LOCK_UNLOCKED,	\
+				.magic = RWLOCK_MAGIC,			\
+				.owner = SPINLOCK_OWNER_INIT,		\
+				.owner_cpu = -1,			\
+				RW_DEP_MAP_INIT(lockname) }
+#else
+#define __QRW_LOCK_UNLOCKED(lockname) \
+	(qrwlock_t)	{	.raw_lock = __ARCH_RW_LOCK_UNLOCKED,	\
+				RW_DEP_MAP_INIT(lockname) }
+#endif
+
+#define DEFINE_QRWLOCK(x)	qrwlock_t x = __QRW_LOCK_UNLOCKED(x)
+#else
+#define DEFINE_QRWLOCK(x)	DEFINE_RWLOCK(x)
+#endif
+#endif

include/linux/spinlock.h
@@ -493,4 +493,27 @@ int __alloc_bucket_spinlocks(spinlock_t **locks, unsigned int *lock_mask,
 
 void free_bucket_spinlocks(spinlock_t *locks);
 
+/*
+ * RHEL8 qrwlock macros
+ */
+#ifndef qrwlock_t
+#define qrwlock_t	rwlock_t
+#endif
+
+#ifndef qread_lock
+#define qread_lock(l)	read_lock(l)
+#endif
+
+#ifndef qread_unlock
+#define qread_unlock(l)	read_unlock(l)
+#endif
+
+#ifndef qwrite_lock_irq
+#define qwrite_lock_irq(l)	write_lock_irq(l)
+#endif
+
+#ifndef qwrite_unlock_irq
+#define qwrite_unlock_irq(l)	write_unlock_irq(l)
+#endif
+
 #endif /* __LINUX_SPINLOCK_H */

arch/powerpc/kernel/Makefile
@@ -35,7 +35,8 @@ obj-y				:= cputable.o syscalls.o \
 				   prom.o traps.o setup-common.o \
 				   udbg.o misc.o io.o misc_$(BITS).o \
 				   of_platform.o prom_parse.o firmware.o \
-				   dma-common.o hw_breakpoint_constraints.o
+				   dma-common.o hw_breakpoint_constraints.o \
+				   qlock.o
 obj-y				+= ptrace/
 obj-$(CONFIG_PPC64)		+= setup_64.o sys_ppc32.o signal_64.o \
 				   paca.o nvram_64.o note.o

commit e07c5d46bce0a44fe9d69222425d7a37d888f268

ppc64: Fix incorrect qrwlock use in debug kernel
Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2027200
Upstream Status: RHEL only

RHEL only commit b6adcb3b7488 ("tasklist_lock, ppc64: Provide
infrastructure to support specific use of qrwlock") adds a ppc64
specific API to use qrwlock for tasklist_lock. Specifically, a new
set of qrwlock APIs is provided in arch/powerpc/kernel/qlock.c.

However, these new APIs aren't using the qrwlock properly in the
debug kernel. It is because they are calling into entry points in
kernel/locking/spinlock_debug.c which currently use the unfair rwlock.

One possible solution is to duplicate the debug code in
kernel/locking/spinlock_debug.c for the ppc64le qrwlock case. Another
alternative is to skip lock debugging (CONFIG_DEBUG_SPINLOCK) for
qrwlock in ppc64 with lockdep code still enabled for the debug kernel.
The latter one is adopted by this patch as it is simpler and less
intrusive.

Fixes: b6adcb3b7488 ("tasklist_lock, ppc64: Provide infrastructure to support specific use of qrwlock")
Signed-off-by: Waiman Long <longman@redhat.com>

arch/powerpc/include/asm/spinlock.h
@@ -20,10 +20,6 @@ static inline void pv_spinlocks_init(void)
  */
 typedef struct {
 	atomic_long_t raw_lock;	/* 8 bytes */
-#ifdef CONFIG_DEBUG_SPINLOCK
-	unsigned int magic, owner_cpu;
-	void *owner;
-#endif
 #ifdef CONFIG_DEBUG_LOCK_ALLOC
 	struct lockdep_map dep_map;
 #endif

arch/powerpc/kernel/qlock.c
@@ -9,6 +9,10 @@
 #define CONFIG_QUEUED_RWLOCKS
 #define CONFIG_PARAVIRT_SPINLOCKS
 
+#ifdef CONFIG_DEBUG_SPINLOCK
+#undef CONFIG_DEBUG_SPINLOCK
+#endif
+
 #include <linux/spinlock.h>
 #include "../../../kernel/locking/qspinlock.c"
 #include "../../../kernel/locking/qrwlock.c"
@@ -20,7 +24,7 @@ void qread_lock(qrwlock_t *lock)
 
 void qread_unlock(qrwlock_t *lock)
 {
-	read_unlock((rwlock_t *)lock);
+	__raw_read_unlock((rwlock_t *)lock);
 }
 
 void qwrite_lock_irq(qrwlock_t *lock)
@@ -30,5 +34,5 @@ void qwrite_lock_irq(qrwlock_t *lock)
 
 void qwrite_unlock_irq(qrwlock_t *lock)
 {
-	write_unlock_irq((rwlock_t *)lock);
+	__raw_write_unlock_irq((rwlock_t *)lock);
 }

include/linux/rh_tasklist_lock.h
@@ -21,18 +21,9 @@
 #define write_lock_irq(l)	qwrite_lock_irq(l)
 #define write_unlock_irq(l)	qwrite_unlock_irq(l)
 
-#ifdef CONFIG_DEBUG_SPINLOCK
-#define __QRW_LOCK_UNLOCKED(lockname)					\
-	(qrwlock_t)	{	.raw_lock = __ARCH_RW_LOCK_UNLOCKED,	\
-				.magic = RWLOCK_MAGIC,			\
-				.owner = SPINLOCK_OWNER_INIT,		\
-				.owner_cpu = -1,			\
-				RW_DEP_MAP_INIT(lockname) }
-#else
 #define __QRW_LOCK_UNLOCKED(lockname) \
 	(qrwlock_t)	{	.raw_lock = __ARCH_RW_LOCK_UNLOCKED,	\
 				RW_DEP_MAP_INIT(lockname) }
-#endif
 
 #define DEFINE_QRWLOCK(x)	qrwlock_t x = __QRW_LOCK_UNLOCKED(x)
 #else