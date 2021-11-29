e845d00923335bc4559b605d21ce1818577e16ce
Miklos Szeredi <mszeredi@redhat.com>
2021-11-29 14:42:19 +0100

    fanotify: rename struct fanotify_{,perm_}event_info
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1897416
    Upstream status: Linus
    Testing: ltp fanotify*
    Conflicts: missing backports
    
    commit 33913997d5c06781c162952c6e5017131fc5aa19
    Author: Amir Goldstein <amir73il@gmail.com>
    Date:   Thu Jan 10 19:04:32 2019 +0200
    
        fanotify: rename struct fanotify_{,perm_}event_info
        
        struct fanotify_event_info "inherits" from struct fsnotify_event and
        therefore a more appropriate (and short) name for it is fanotify_event.
        Same for struct fanotify_perm_event_info, which now "inherits" from
        struct fanotify_event.
        
        We plan to reuse the name struct fanotify_event_info for user visible
        event info record format.
        
        Signed-off-by: Amir Goldstein <amir73il@gmail.com>
        Signed-off-by: Jan Kara <jack@suse.cz>
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/notify/fanotify/fanotify.c
+++ fs/notify/fanotify/fanotify.c
@@ -19,7 +19,7 @@
 static bool should_merge(struct fsnotify_event *old_fsn,
 			 struct fsnotify_event *new_fsn)
 {
-	struct fanotify_event_info *old, *new;
+	struct fanotify_event *old, *new;
 
 	pr_debug("%s: old=%p new=%p\n", __func__, old_fsn, new_fsn);
 	old = FANOTIFY_E(old_fsn);
@@ -58,7 +58,7 @@ static int fanotify_merge(struct list_head *list, struct fsnotify_event *event)
 }
 
 static int fanotify_get_response(struct fsnotify_group *group,
-				 struct fanotify_perm_event_info *event,
+				 struct fanotify_perm_event *event,
 				 struct fsnotify_iter_info *iter_info)
 {
 	int ret;
@@ -144,11 +144,11 @@ static u32 fanotify_group_event_mask(struct fsnotify_iter_info *iter_info,
 		~marks_ignored_mask;
 }
 
-struct fanotify_event_info *fanotify_alloc_event(struct fsnotify_group *group,
+struct fanotify_event *fanotify_alloc_event(struct fsnotify_group *group,
 						 struct inode *inode, u32 mask,
 						 const struct path *path)
 {
-	struct fanotify_event_info *event = NULL;
+	struct fanotify_event *event = NULL;
 	gfp_t gfp = GFP_KERNEL_ACCOUNT;
 	struct mem_cgroup *old_memcg;
 
@@ -167,7 +167,7 @@ struct fanotify_event_info *fanotify_alloc_event(struct fsnotify_group *group,
 	old_memcg = set_active_memcg(group->memcg);
 
 	if (fanotify_is_perm_event(mask)) {
-		struct fanotify_perm_event_info *pevent;
+		struct fanotify_perm_event *pevent;
 
 		pevent = kmem_cache_alloc(fanotify_perm_event_cachep, gfp);
 		if (!pevent)
@@ -204,7 +204,7 @@ static int fanotify_handle_event(struct fsnotify_group *group,
 				 struct fsnotify_iter_info *iter_info)
 {
 	int ret = 0;
-	struct fanotify_event_info *event;
+	struct fanotify_event *event;
 	struct fsnotify_event *fsn_event;
 
 	BUILD_BUG_ON(FAN_ACCESS != FS_ACCESS);
@@ -282,7 +282,7 @@ static void fanotify_free_group_priv(struct fsnotify_group *group)
 
 static void fanotify_free_event(struct fsnotify_event *fsn_event)
 {
-	struct fanotify_event_info *event;
+	struct fanotify_event *event;
 
 	event = FANOTIFY_E(fsn_event);
 	path_put(&event->path);
