a5312147da3563589857cf483b2db4de3f23a01e
Nico Pache <npache@redhat.com>
2021-11-29 14:29:27 -0500

    arm64: Do not pass tagged addresses to __is_lm_address()
    commit 91cb2c8b072e00632adf463b78b44f123d46a0fa
    Author: Catalin Marinas <catalin.marinas@arm.com>
    Date:   Mon Feb 1 19:06:33 2021 +0000
    
        arm64: Do not pass tagged addresses to __is_lm_address()
    
        Commit 519ea6f1c82f ("arm64: Fix kernel address detection of
        __is_lm_address()") fixed the incorrect validation of addresses below
        PAGE_OFFSET. However, it no longer allowed tagged addresses to be passed
        to virt_addr_valid().
    
        Fix this by explicitly resetting the pointer tag prior to invoking
        __is_lm_address(). This is consistent with the __lm_to_phys() macro.
    
        Fixes: 519ea6f1c82f ("arm64: Fix kernel address detection of __is_lm_address()")
        Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
        Acked-by: Ard Biesheuvel <ardb@kernel.org>
        Cc: <stable@vger.kernel.org> # 5.4.x
        Cc: Will Deacon <will@kernel.org>
        Cc: Vincenzo Frascino <vincenzo.frascino@arm.com>
        Cc: Mark Rutland <mark.rutland@arm.com>
        Link: https://lore.kernel.org/r/20210201190634.22942-2-catalin.marinas@arm.com
    
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1954335
    Signed-off-by: Nico Pache <npache@redhat.com>

--- arch/arm64/include/asm/memory.h
+++ arch/arm64/include/asm/memory.h
@@ -361,7 +361,7 @@ static inline void *phys_to_virt(phys_addr_t x)
 #endif
 
 #define virt_addr_valid(addr)	({					\
-	__typeof__(addr) __addr = addr;					\
+	__typeof__(addr) __addr = __tag_reset(addr);			\
 	__is_lm_address(__addr) && pfn_valid(virt_to_pfn(__addr));	\
 })
 
