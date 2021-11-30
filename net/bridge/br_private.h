ba24b3c68cf257bc074d6262c3cc7788fa4a56e4
Ivan Vecera <ivecera@redhat.com>
2021-11-30 09:25:55 +0100

    net: bridge: mcast: toggle also host vlan state in br_multicast_toggle_vlan
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2019055
    
    commit affce9a774ca2514aaa5638fde92c57a476dfd79
    Author: Nikolay Aleksandrov <nikolay@nvidia.com>
    Date:   Mon Aug 16 17:57:07 2021 +0300
    
        net: bridge: mcast: toggle also host vlan state in br_multicast_toggle_vlan
    
        When changing vlan mcast state by br_multicast_toggle_vlan it iterates
        over all ports and enables/disables the port mcast ctx based on the new
        state, but I forgot to update the host vlan (bridge master vlan entry)
        with the new state so it will be left out. Also that function is not
        used outside of br_multicast.c, so make it static.
    
        Fixes: f4b7002a7076 ("net: bridge: add vlan mcast snooping knob")
        Signed-off-by: Nikolay Aleksandrov <nikolay@nvidia.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- net/bridge/br_private.h
+++ net/bridge/br_private.h
@@ -934,7 +934,6 @@ void br_multicast_port_ctx_init(struct net_bridge_port *port,
 				struct net_bridge_mcast_port *pmctx);
 void br_multicast_port_ctx_deinit(struct net_bridge_mcast_port *pmctx);
 void br_multicast_toggle_one_vlan(struct net_bridge_vlan *vlan, bool on);
-void br_multicast_toggle_vlan(struct net_bridge_vlan *vlan, bool on);
 int br_multicast_toggle_vlan_snooping(struct net_bridge *br, bool on,
 				      struct netlink_ext_ack *extack);
 bool br_multicast_toggle_global_vlan(struct net_bridge_vlan *vlan, bool on);
@@ -1363,11 +1362,6 @@ static inline void br_multicast_port_ctx_deinit(struct net_bridge_mcast_port *pm
 
 static inline void br_multicast_toggle_one_vlan(struct net_bridge_vlan *vlan,
 						bool on)
-{
-}
-
-static inline void br_multicast_toggle_vlan(struct net_bridge_vlan *vlan,
-					    bool on)
 {
 }
 
