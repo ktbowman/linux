9e90167eff917ae55b61860222022a0226e8f26a
Ivan Vecera <ivecera@redhat.com>
2021-11-30 09:23:25 +0100

    net: switchdev: add a context void pointer to struct switchdev_notifier_info
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2019055
    
    Conflicts:
    - hunks for missing files and N/A one for DSA were removed as DSA is
      not enabled in RHEL
    
    commit 69bfac968a06aab5927160f8736485f85c3e8ee8
    Author: Vladimir Oltean <vladimir.oltean@nxp.com>
    Date:   Sun Jun 27 14:54:24 2021 +0300
    
        net: switchdev: add a context void pointer to struct switchdev_notifier_info
    
        In the case where the driver asks for a replay of a certain type of
        event (port object or attribute) for a bridge port that is a LAG, it may
        do so because this port has just joined the LAG.
    
        But there might already be other switchdev ports in that LAG, and it is
        preferable that those preexisting switchdev ports do not act upon the
        replayed event.
    
        The solution is to add a context to switchdev events, which is NULL most
        of the time (when the bridge layer initiates the call) but which can be
        set to a value controlled by the switchdev driver when a replay is
        requested. The driver can then check the context to figure out if all
        ports within the LAG should act upon the switchdev event, or just the
        ones that match the context.
    
        We have to modify all switchdev_handle_* helper functions as well as the
        prototypes in the drivers that use these helpers too, because these
        helpers hide the underlying struct switchdev_notifier_info from us and
        there is no way to retrieve the context otherwise.
    
        The context structure will be populated and used in later patches.
    
        Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>
        Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- include/net/switchdev.h
+++ include/net/switchdev.h
@@ -265,6 +265,7 @@ enum switchdev_notifier_type {
 struct switchdev_notifier_info {
 	struct net_device *dev;
 	struct netlink_ext_ack *extack;
+	const void *ctx;
 };
 
 struct switchdev_notifier_fdb_info {
@@ -331,20 +332,20 @@ void switchdev_port_fwd_mark_set(struct net_device *dev,
 int switchdev_handle_port_obj_add(struct net_device *dev,
 			struct switchdev_notifier_port_obj_info *port_obj_info,
 			bool (*check_cb)(const struct net_device *dev),
-			int (*add_cb)(struct net_device *dev,
+			int (*add_cb)(struct net_device *dev, const void *ctx,
 				      const struct switchdev_obj *obj,
 				      struct netlink_ext_ack *extack));
 int switchdev_handle_port_obj_del(struct net_device *dev,
 			struct switchdev_notifier_port_obj_info *port_obj_info,
 			bool (*check_cb)(const struct net_device *dev),
-			int (*del_cb)(struct net_device *dev,
+			int (*del_cb)(struct net_device *dev, const void *ctx,
 				      const struct switchdev_obj *obj));
 
 RH_KABI_FORCE_CHANGE(1)
 int switchdev_handle_port_attr_set(struct net_device *dev,
 			struct switchdev_notifier_port_attr_info *port_attr_info,
 			bool (*check_cb)(const struct net_device *dev),
-			int (*set_cb)(struct net_device *dev,
+			int (*set_cb)(struct net_device *dev, const void *ctx,
 				      const struct switchdev_attr *attr,
 				      struct netlink_ext_ack *extack));
 #else
@@ -416,7 +417,7 @@ static inline int
 switchdev_handle_port_obj_add(struct net_device *dev,
 			struct switchdev_notifier_port_obj_info *port_obj_info,
 			bool (*check_cb)(const struct net_device *dev),
-			int (*add_cb)(struct net_device *dev,
+			int (*add_cb)(struct net_device *dev, const void *ctx,
 				      const struct switchdev_obj *obj,
 				      struct netlink_ext_ack *extack))
 {
@@ -427,7 +428,7 @@ static inline int
 switchdev_handle_port_obj_del(struct net_device *dev,
 			struct switchdev_notifier_port_obj_info *port_obj_info,
 			bool (*check_cb)(const struct net_device *dev),
-			int (*del_cb)(struct net_device *dev,
+			int (*del_cb)(struct net_device *dev, const void *ctx,
 				      const struct switchdev_obj *obj))
 {
 	return 0;
@@ -437,7 +438,7 @@ static inline int
 switchdev_handle_port_attr_set(struct net_device *dev,
 			struct switchdev_notifier_port_attr_info *port_attr_info,
 			bool (*check_cb)(const struct net_device *dev),
-			int (*set_cb)(struct net_device *dev,
+			int (*set_cb)(struct net_device *dev, const void *ctx,
 				      const struct switchdev_attr *attr,
 				      struct netlink_ext_ack *extack))
 {
