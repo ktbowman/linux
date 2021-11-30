e1cdeaa6c5d231499c615c201e466363d5993465
Ivan Vecera <ivecera@redhat.com>
2021-11-30 09:14:34 +0100

    net: bridge: add a flag to avoid refreshing fdb when changing/adding
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2019055
    
    commit b5f1d9ec283bd28a452cf61d7e5c2f2b1a9cccda
    Author: Nikolay Aleksandrov <nikolay@cumulusnetworks.com>
    Date:   Tue Jun 23 23:47:18 2020 +0300
    
        net: bridge: add a flag to avoid refreshing fdb when changing/adding
    
        When we modify or create a new fdb entry sometimes we want to avoid
        refreshing its activity in order to track it properly. One example is
        when a mac is received from EVPN multi-homing peer by FRR, which doesn't
        want to change local activity accounting. It makes it static and sets a
        flag to track its activity.
    
        Signed-off-by: Nikolay Aleksandrov <nikolay@cumulusnetworks.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- net/bridge/br_fdb.c
+++ net/bridge/br_fdb.c
@@ -863,6 +863,7 @@ static int fdb_add_entry(struct net_bridge *br, struct net_bridge_port *source,
 			 struct nlattr *nfea_tb[])
 {
 	bool is_sticky = !!(ndm->ndm_flags & NTF_STICKY);
+	bool refresh = !nfea_tb[NFEA_DONT_REFRESH];
 	struct net_bridge_fdb_entry *fdb;
 	u16 state = ndm->ndm_state;
 	bool modified = false;
@@ -940,7 +941,8 @@ static int fdb_add_entry(struct net_bridge *br, struct net_bridge_port *source,
 
 	fdb->used = jiffies;
 	if (modified) {
-		fdb->updated = jiffies;
+		if (refresh)
+			fdb->updated = jiffies;
 		fdb_notify(br, fdb, RTM_NEWNEIGH, true);
 	}
 
@@ -980,6 +982,7 @@ static int __br_fdb_add(struct ndmsg *ndm, struct net_bridge *br,
 
 static const struct nla_policy br_nda_fdb_pol[NFEA_MAX + 1] = {
 	[NFEA_ACTIVITY_NOTIFY]	= { .type = NLA_U8 },
+	[NFEA_DONT_REFRESH]	= { .type = NLA_FLAG },
 };
 
 /* Add new permanent fdb entry with RTM_NEWNEIGH */
