125e1ca06f4ec4be63b972af5149154facaa295f
Ivan Vecera <ivecera@redhat.com>
2021-11-30 09:20:33 +0100

    switchdev: mrp: Remove SWITCHDEV_ATTR_ID_MRP_PORT_STAT
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2019055
    
    Conflicts:
    - adjusted due to usage of RH KABI macros
    
    commit 059d2a1004981dce19f0127dabc1b4ec927d202a
    Author: Horatiu Vultur <horatiu.vultur@microchip.com>
    Date:   Sat Feb 6 22:47:34 2021 +0100
    
        switchdev: mrp: Remove SWITCHDEV_ATTR_ID_MRP_PORT_STAT
    
        Now that MRP started to use also SWITCHDEV_ATTR_ID_PORT_STP_STATE to
        notify HW, then SWITCHDEV_ATTR_ID_MRP_PORT_STAT is not used anywhere
        else, therefore we can remove it.
    
        Fixes: c284b545900830 ("switchdev: mrp: Extend switchdev API to offload MRP")
        Signed-off-by: Horatiu Vultur <horatiu.vultur@microchip.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- include/net/switchdev.h
+++ include/net/switchdev.h
@@ -32,7 +32,6 @@ enum switchdev_attr_id {
 	SWITCHDEV_ATTR_ID_BRIDGE_MC_DISABLED,
 	SWITCHDEV_ATTR_ID_BRIDGE_MROUTER,
 #if IS_ENABLED(CONFIG_BRIDGE_MRP)
-	RH_KABI_BROKEN_INSERT_ENUM(SWITCHDEV_ATTR_ID_MRP_PORT_STATE)
 	RH_KABI_BROKEN_INSERT_ENUM(SWITCHDEV_ATTR_ID_MRP_PORT_ROLE)
 #endif
 };
@@ -69,7 +68,6 @@ struct switchdev_attr {
 		u16 vlan_protocol;			/* BRIDGE_VLAN_PROTOCOL */
 		bool mc_disabled;			/* MC_DISABLED */
 #if IS_ENABLED(CONFIG_BRIDGE_MRP)
-		u8 mrp_port_state;			/* MRP_PORT_STATE */
 		u8 mrp_port_role;			/* MRP_PORT_ROLE */
 #endif
 		) /* RH_KABI_BROKEN_INSERT_BLOCK */
