8e7c253cbac146dd9c381b5fc5e280435c806fdd
Jiri Benc <jbenc@redhat.com>
2021-12-14 13:02:42 +0100

    bpf: Clean up sockmap related Kconfigs
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1947007
    
    Conflicts:
    - net/Kconfig: conflict due to missing 43da14110cb4 ("net: Fix Kconfig
      indentation, continued"); adjust the result to match the current
      indentation upstream
    - init/Kconfig: renamed to kernel/bpf/Kconfig by out of order backport of
      b24abcff918a ("bpf, kconfig: Add consolidated menu entry for bpf with core
      options")
    - [minor] include/net/udp.h: context difference due to out of order backport
      of 000ac44da7d0 ("udp: fixup csum for GSO receive slow path")
    - [minor] init/Kconfig: context difference due to out of order backport of
      48cac3f4a96d ("bpf: Implement formatted output helpers with bstr_printf")
    - [minor] net/Kconfig: context difference due to missing a7f7f6248d97
      ("treewide: replace '---help---' in Kconfig files with 'help'")
    
    commit 887596095ec2a9ea39ffcf98f27bf2e77c5eb512
    Author: Cong Wang <cong.wang@bytedance.com>
    Date:   Tue Feb 23 10:49:26 2021 -0800
    
        bpf: Clean up sockmap related Kconfigs
    
        As suggested by John, clean up sockmap related Kconfigs:
    
        Reduce the scope of CONFIG_BPF_STREAM_PARSER down to TCP stream
        parser, to reflect its name.
    
        Make the rest sockmap code simply depend on CONFIG_BPF_SYSCALL
        and CONFIG_INET, the latter is still needed at this point because
        of TCP/UDP proto update. And leave CONFIG_NET_SOCK_MSG untouched,
        as it is used by non-sockmap cases.
    
        Signed-off-by: Cong Wang <cong.wang@bytedance.com>
        Signed-off-by: Alexei Starovoitov <ast@kernel.org>
        Reviewed-by: Lorenz Bauer <lmb@cloudflare.com>
        Acked-by: John Fastabend <john.fastabend@gmail.com>
        Acked-by: Jakub Sitnicki <jakub@cloudflare.com>
        Link: https://lore.kernel.org/bpf/20210223184934.6054-2-xiyou.wangcong@gmail.com
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>

--- include/net/udp.h
+++ include/net/udp.h
@@ -532,9 +532,9 @@ static inline void udp_post_segment_fix_csum(struct sk_buff *skb)
 		skb->csum_valid = 1;
 }
 
-#ifdef CONFIG_BPF_STREAM_PARSER
+#ifdef CONFIG_BPF_SYSCALL
 struct sk_psock;
 struct proto *udp_bpf_get_proto(struct sock *sk, struct sk_psock *psock);
-#endif /* BPF_STREAM_PARSER */
+#endif
 
 #endif	/* _UDP_H */
