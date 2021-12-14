28b746272cf439d33ccc9cdde46f1427b5227a19
Jiri Benc <jbenc@redhat.com>
2021-12-14 13:02:38 +0100

    net: Use skb accessors in network core
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1947007
    
    Conflicts:
    - tcp_zerocopy_receive: convert only the existing frag->size; we don't have
      789762ceec8f ("tcp: adjust rcv zerocopy hints based on frag sizes"), skip
      that block
    - [minor] skb_can_coalesce: different context due to out of order backport
      of 7240b60c98d6 ("linux: Add skb_frag_t page_offset accessors")
    
    Omitted-fix: 3afb09618840 (fixes the block that we skipped in this backport)
    
    commit d8e18a516f8f67404c0d21af8c93d0474fba0876
    Author: Matthew Wilcox (Oracle) <willy@infradead.org>
    Date:   Mon Jul 22 20:08:26 2019 -0700
    
        net: Use skb accessors in network core
    
        In preparation for unifying the skb_frag and bio_vec, use the fine
        accessors which already exist and use skb_frag_t instead of
        struct skb_frag_struct.
    
        Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -1832,13 +1832,13 @@ static int tcp_zerocopy_receive(struct sock *sk,
 				break;
 			frags = skb_shinfo(skb)->frags;
 			while (offset) {
-				if (frags->size > offset)
+				if (skb_frag_size(frags) > offset)
 					goto out;
-				offset -= frags->size;
+				offset -= skb_frag_size(frags);
 				frags++;
 			}
 		}
-		if (frags->size != PAGE_SIZE || frags->page_offset)
+		if (skb_frag_size(frags) != PAGE_SIZE || frags->page_offset)
 			break;
 		ret = vm_insert_page(vma, address + length,
 				     skb_frag_page(frags));
@@ -3861,7 +3861,7 @@ int tcp_md5_hash_skb_data(struct tcp_md5sig_pool *hp,
 		return 1;
 
 	for (i = 0; i < shi->nr_frags; ++i) {
-		const struct skb_frag_struct *f = &shi->frags[i];
+		const skb_frag_t *f = &shi->frags[i];
 		unsigned int offset = f->page_offset;
 		struct page *page = skb_frag_page(f) + (offset >> PAGE_SHIFT);
 
