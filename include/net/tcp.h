babb8dae091ceaa6f0bc257ced40bd0ffab52921
Jiri Benc <jbenc@redhat.com>
2021-12-14 13:02:39 +0100

    tcp: remove indirect calls for icsk->icsk_af_ops->send_check
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1947007
    
    Conflicts:
    - [minor] include/net/tcp.h: context conflict with parallely applied
      34e1ec319e99 ("net: ipv4: remove unused arg exact_dif in compute_score")
      and with out of order backport of b03d2142bea8 ("tcp: move ipv6_specific
      declaration to remove a warning")
    - [minor] __tcp_transmit_skb: context difference due to out of order
      backport of d0b0da46d425 ("bpf: tcp: Add bpf_skops_hdr_opt_len() and
      bpf_skops_write_hdr_opt()")
    
    commit dd2e0b86fc4ee146ac8f3275833d0187efeb950a
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Fri Jun 19 12:12:35 2020 -0700
    
        tcp: remove indirect calls for icsk->icsk_af_ops->send_check
    
        Mitigate RETPOLINE costs in __tcp_transmit_skb()
        by using INDIRECT_CALL_INET() wrapper.
    
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Jiri Benc <jbenc@redhat.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -952,6 +952,8 @@ static inline int tcp_v6_sdif(const struct sk_buff *skb)
 
 extern const struct inet_connection_sock_af_ops ipv6_specific;
 
+INDIRECT_CALLABLE_DECLARE(void tcp_v6_send_check(struct sock *sk, struct sk_buff *skb));
+
 #endif
 
 /* TCP_SKB_CB reference means this can not be used from early demux */
