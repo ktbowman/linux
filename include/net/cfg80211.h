1f4b1a9b4479261ce8c7cecfaec3ad163ea73815
Íñigo Huguet <ihuguet@redhat.com>
2021-12-22 16:02:02 +0100

    nl80211/cfg80211: add BSS color to NDP ranging parameters
    Bugzilla: http://bugzilla.redhat.com/2004141
    
    Conflicts: deleted change to copyright header due to not cleanly applying
    
    commit dd3e4fc75b4ab8186a133cfe9d49666a2f8186e0
    Author: Avraham Stern <avraham.stern@intel.com>
    Date:   Fri Jun 18 13:41:36 2021 +0300
    
        nl80211/cfg80211: add BSS color to NDP ranging parameters
        
        In NDP ranging, the initiator need to set the BSS color in the NDP
        to the BSS color of the responder. Add the BSS color as a parameter
        for NDP ranging.
        
        Signed-off-by: Avraham Stern <avraham.stern@intel.com>
        Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
        Link: https://lore.kernel.org/r/iwlwifi.20210618133832.f097a6144b59.I27dec8b994df52e691925ea61be4dd4fa6d396c0@changeid
        Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    
    Signed-off-by: Íñigo Huguet <ihuguet@redhat.com>

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -3520,7 +3520,10 @@ struct cfg80211_pmsr_result {
  *		 If neither @trigger_based nor @non_trigger_based is set,
  *		 EDCA based ranging will be used.
  * @lmr_feedback: negotiate for I2R LMR feedback. Only valid if either
- *	@trigger_based or @non_trigger_based is set.
+ *		 @trigger_based or @non_trigger_based is set.
+ * @bss_color: the bss color of the responder. Optional. Set to zero to
+ *	indicate the driver should set the BSS color. Only valid if
+ *	@non_trigger_based or @trigger_based is set.
  *
  * See also nl80211 for the respective attribute documentation.
  */
@@ -3538,6 +3541,7 @@ struct cfg80211_pmsr_ftm_request_peer {
 	u8 burst_duration;
 	u8 ftms_per_burst;
 	u8 ftmr_retries;
+	u8 bss_color;
 };
 
 /**
