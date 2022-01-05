270635cc5dbe2568ec8d66891e4413e646fae372
Waiman Long <longman@redhat.com>
2022-01-05 14:54:21 -0500

    hugetlb: allow to free gigantic pages regardless of the configuration
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2032811
    Conflicts:
     1) All the hunks for files in unsupported archs are dropped.
     2) Merge conflict in mm/hugetlb.c due to the presence of a later
        upstream commit f60858f9d327 ("hugetlbfs: don't retry when pool
        page allocations start to fail").
     3) Merge conflict in arch/powerpc/platforms/Kconfig.cputype due to
        missing upstream commit 1bb2bae2e6c7 ("powerpc/mm/radix: Use
        KUEP API for Radix MMU") and commit 890274c2dc4c0 ("powerpc/64s:
        Implement KUAP for Radix MMU").
     4) Fuzzes in arch/arm64/Kconfig and arch/s390/Kconfig.
    
    commit 4eb0716e868eed963967adb0b1b11d9bd8ca1d01
    Author: Alexandre Ghiti <alex@ghiti.fr>
    Date:   Mon, 13 May 2019 17:19:04 -0700
    
        hugetlb: allow to free gigantic pages regardless of the configuration
    
        On systems without CONTIG_ALLOC activated but that support gigantic pages,
        boottime reserved gigantic pages can not be freed at all.  This patch
        simply enables the possibility to hand back those pages to memory
        allocator.
    
        Link: http://lkml.kernel.org/r/20190327063626.18421-5-alex@ghiti.fr
        Signed-off-by: Alexandre Ghiti <alex@ghiti.fr>
        Acked-by: David S. Miller <davem@davemloft.net> [sparc]
        Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
        Cc: Andy Lutomirsky <luto@kernel.org>
        Cc: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
        Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
        Cc: Borislav Petkov <bp@alien8.de>
        Cc: Catalin Marinas <catalin.marinas@arm.com>
        Cc: Dave Hansen <dave.hansen@linux.intel.com>
        Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
        Cc: "H . Peter Anvin" <hpa@zytor.com>
        Cc: Ingo Molnar <mingo@redhat.com>
        Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
        Cc: Michael Ellerman <mpe@ellerman.id.au>
        Cc: Paul Mackerras <paulus@samba.org>
        Cc: Peter Zijlstra <peterz@infradead.org>
        Cc: Rich Felker <dalias@libc.org>
        Cc: Thomas Gleixner <tglx@linutronix.de>
        Cc: Vlastimil Babka <vbabka@suse.cz>
        Cc: Will Deacon <will.deacon@arm.com>
        Cc: Yoshinori Sato <ysato@users.sourceforge.jp>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -8617,8 +8617,9 @@ done:
 				pfn_max_align_up(end), migratetype);
 	return ret;
 }
+#endif /* CONFIG_CONTIG_ALLOC */
 
-void free_contig_range(unsigned long pfn, unsigned nr_pages)
+void free_contig_range(unsigned long pfn, unsigned int nr_pages)
 {
 	unsigned int count = 0;
 
@@ -8630,7 +8631,6 @@ void free_contig_range(unsigned long pfn, unsigned nr_pages)
 	}
 	WARN(count != 0, "%d pages are still in use!\n", count);
 }
-#endif
 
 #ifdef CONFIG_MEMORY_HOTPLUG
 /*
