8257f01a6b5ccc6bb497cdae346a53079c282145
Jaroslav Kysela <jkysela@redhat.com>
2022-01-05 13:22:12 +0100

    ALSA: vx222: Fix assignment in if condition
    PCI VX222 driver code contains lots of assignments in if condition,
    which is a bad coding style that may confuse readers and occasionally
    lead to bugs.
    
    This patch is merely for coding-style fixes, no functional changes.
    
        Link: https://lore.kernel.org/r/20210608140540.17885-52-tiwai@suse.de
        Signed-off-by: Takashi Iwai <tiwai@suse.de>
    
    Author: Takashi Iwai <tiwai@suse.de>
    Date: Tue Jun 8 16:05:25 2021 +0200
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>
    (cherry picked from commit 029fd1eae7e1e1fd5307b4f32b756584752186d5)
    Bugzilla: https://bugzilla.redhat.com/2003613

--- sound/pci/vx222/vx222.c
+++ sound/pci/vx222/vx222.c
@@ -132,7 +132,8 @@ static int snd_vx222_create(struct snd_card *card, struct pci_dev *pci,
 	const struct snd_vx_ops *vx_ops;
 
 	/* enable PCI device */
-	if ((err = pci_enable_device(pci)) < 0)
+	err = pci_enable_device(pci);
+	if (err < 0)
 		return err;
 	pci_set_master(pci);
 
@@ -146,7 +147,8 @@ static int snd_vx222_create(struct snd_card *card, struct pci_dev *pci,
 	vx = to_vx222(chip);
 	vx->pci = pci;
 
-	if ((err = pci_request_regions(pci, CARD_NAME)) < 0) {
+	err = pci_request_regions(pci, CARD_NAME);
+	if (err < 0) {
 		snd_vx222_free(chip);
 		return err;
 	}
@@ -163,7 +165,8 @@ static int snd_vx222_create(struct snd_card *card, struct pci_dev *pci,
 	chip->irq = pci->irq;
 	card->sync_irq = chip->irq;
 
-	if ((err = snd_device_new(card, SNDRV_DEV_LOWLEVEL, chip, &ops)) < 0) {
+	err = snd_device_new(card, SNDRV_DEV_LOWLEVEL, chip, &ops);
+	if (err < 0) {
 		snd_vx222_free(chip);
 		return err;
 	}
@@ -206,7 +209,8 @@ static int snd_vx222_probe(struct pci_dev *pci,
 			hw = &vx222_v2_hw;
 		break;
 	}
-	if ((err = snd_vx222_create(card, pci, hw, &vx)) < 0) {
+	err = snd_vx222_create(card, pci, hw, &vx);
+	if (err < 0) {
 		snd_card_free(card);
 		return err;
 	}
@@ -222,12 +226,14 @@ static int snd_vx222_probe(struct pci_dev *pci,
 	vx->core.dev = &pci->dev;
 #endif
 
-	if ((err = snd_vx_setup_firmware(&vx->core)) < 0) {
+	err = snd_vx_setup_firmware(&vx->core);
+	if (err < 0) {
 		snd_card_free(card);
 		return err;
 	}
 
-	if ((err = snd_card_register(card)) < 0) {
+	err = snd_card_register(card);
+	if (err < 0) {
 		snd_card_free(card);
 		return err;
 	}
