1e2d71366b5d5c0ea2ecaf3c861f30efb798d9c2
Michel Dänzer <mdaenzer@redhat.com>
2022-01-05 19:03:11 +0100

    drm/i915/display: Match PSR2 selective fetch sequences with specification
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1988103
    Upstream Status: v5.16-rc1
    
    Conflicts:
            drivers/gpu/drm/i915/display/intel_display.c
            drivers/gpu/drm/i915/display/intel_psr.c
    
    commit 73262db68c27ed25452ffd3b57e051e1791de713
    Author:     José Roberto de Souza <jose.souza@intel.com>
    AuthorDate: Wed Sep 22 14:52:41 2021 -0700
    Commit:     José Roberto de Souza <jose.souza@intel.com>
    CommitDate: Thu Sep 23 10:06:16 2021 -0700
    
        We were not completely following the selective fetch programming
        sequence, here some things we were doing wrong:
        - not programming plane selective fetch a PSR2_MAN_TRK_CTL registers
        when doing a modeset
        - programming PSR2_MAN_TRK_CTL out of vblank
    
        With this changes the last remainig underrun found in Alderlake-P is
        fixed.
    
        Bspec: 55229
        Tested-by: Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>
        Reviewed-by: Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>
        Cc: Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>
        Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
        Link: https://patchwork.freedesktop.org/patch/msgid/20210922215242.66683-2-jose.souza@intel.com
    
    Signed-off-by: Michel Dänzer <mdaenzer@redhat.com>

--- drivers/gpu/drm/i915/display/intel_psr.c
+++ drivers/gpu/drm/i915/display/intel_psr.c
@@ -594,6 +594,8 @@ static void hsw_activate_psr2(struct intel_dp *intel_dp)
 		val |= EDP_PSR2_SU_SDP_SCANLINE;
 
 	if (intel_dp->psr.psr2_sel_fetch_enabled) {
+		u32 tmp;
+
 		/* WA 1408330847 */
 		if (IS_TGL_DISPLAY_STEP(dev_priv, STEP_A0, STEP_B0) ||
 		    IS_RKL_REVID(dev_priv, RKL_REVID_A0, RKL_REVID_A0))
@@ -601,9 +603,8 @@ static void hsw_activate_psr2(struct intel_dp *intel_dp)
 				     DIS_RAM_BYPASS_PSR2_MAN_TRACK,
 				     DIS_RAM_BYPASS_PSR2_MAN_TRACK);
 
-		intel_de_write(dev_priv,
-			       PSR2_MAN_TRK_CTL(intel_dp->psr.transcoder),
-			       PSR2_MAN_TRK_CTL_ENABLE);
+		tmp = intel_de_read(dev_priv, PSR2_MAN_TRK_CTL(intel_dp->psr.transcoder));
+		drm_WARN_ON(&dev_priv->drm, !(tmp & PSR2_MAN_TRK_CTL_ENABLE));
 	} else if (HAS_PSR2_SEL_FETCH(dev_priv)) {
 		intel_de_write(dev_priv,
 			       PSR2_MAN_TRK_CTL(intel_dp->psr.transcoder), 0);
@@ -1499,6 +1500,18 @@ static void psr_force_hw_tracking_exit(struct intel_dp *intel_dp)
 		intel_psr_exit(intel_dp);
 }
 
+void intel_psr2_disable_plane_sel_fetch(struct intel_plane *plane,
+					const struct intel_crtc_state *crtc_state)
+{
+	struct drm_i915_private *dev_priv = to_i915(plane->base.dev);
+	enum pipe pipe = plane->pipe;
+
+	if (!crtc_state->enable_psr2_sel_fetch)
+		return;
+
+	intel_de_write_fw(dev_priv, PLANE_SEL_FETCH_CTL(pipe, plane->id), 0);
+}
+
 void intel_psr2_program_plane_sel_fetch(struct intel_plane *plane,
 					const struct intel_crtc_state *crtc_state,
 					const struct intel_plane_state *plane_state,
@@ -1513,11 +1526,11 @@ void intel_psr2_program_plane_sel_fetch(struct intel_plane *plane,
 	if (!crtc_state->enable_psr2_sel_fetch)
 		return;
 
-	val = plane_state ? plane_state->ctl : 0;
-	val &= plane->id == PLANE_CURSOR ? val : PLANE_SEL_FETCH_CTL_ENABLE;
-	intel_de_write_fw(dev_priv, PLANE_SEL_FETCH_CTL(pipe, plane->id), val);
-	if (!val || plane->id == PLANE_CURSOR)
+	if (plane->id == PLANE_CURSOR) {
+		intel_de_write_fw(dev_priv, PLANE_SEL_FETCH_CTL(pipe, plane->id),
+				  plane_state->ctl);
 		return;
+	}
 
 	clip = &plane_state->psr2_sel_fetch_area;
 
@@ -1540,14 +1553,16 @@ void intel_psr2_program_plane_sel_fetch(struct intel_plane *plane,
 	val = (drm_rect_height(clip) - 1) << 16;
 	val |= (drm_rect_width(&plane_state->uapi.src) >> 16) - 1;
 	intel_de_write_fw(dev_priv, PLANE_SEL_FETCH_SIZE(pipe, plane->id), val);
+
+	intel_de_write_fw(dev_priv, PLANE_SEL_FETCH_CTL(pipe, plane->id),
+			  PLANE_SEL_FETCH_CTL_ENABLE);
 }
 
 void intel_psr2_program_trans_man_trk_ctl(const struct intel_crtc_state *crtc_state)
 {
 	struct drm_i915_private *dev_priv = to_i915(crtc_state->uapi.crtc->dev);
 
-	if (!HAS_PSR2_SEL_FETCH(dev_priv) ||
-	    !crtc_state->enable_psr2_sel_fetch)
+	if (!crtc_state->enable_psr2_sel_fetch)
 		return;
 
 	intel_de_write(dev_priv, PSR2_MAN_TRK_CTL(crtc_state->cpu_transcoder),
