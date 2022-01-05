056bdd0a420b78ee9e5ae381ec74db2956c5280e
Waiman Long <longman@redhat.com>
2022-01-05 14:54:29 -0500

    mm/hugetlb: narrow the hugetlb_lock protection area during preparing huge page
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2032811
    Conflicts: Merge conflict due to the presence of a later upstream commit
    	   ff5461176213 ("hugetlb: fix uninitialized subpool pointer").
    
    commit 2f37511cb6c2a59b67f8f13ad206a0298992eaf5
    Author: Wei Yang <richard.weiyang@linux.alibaba.com>
    Date:   Tue, 13 Oct 2020 16:56:36 -0700
    
        mm/hugetlb: narrow the hugetlb_lock protection area during preparing huge page
    
        set_hugetlb_cgroup_[rsvd] just manipulate page local data, which is not
        necessary to be protected by hugetlb_lock.
    
        Let's take this out.
    
        Signed-off-by: Wei Yang <richard.weiyang@linux.alibaba.com>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Reviewed-by: Baoquan He <bhe@redhat.com>
        Reviewed-by: Mike Kravetz <mike.kravetz@oracle.com>
        Cc: Vlastimil Babka <vbabka@suse.cz>
        Link: https://lkml.kernel.org/r/20200831022351.20916-7-richard.weiyang@linux.alibaba.com
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -1494,9 +1494,9 @@ static void prep_new_huge_page(struct hstate *h, struct page *page, int nid)
 	INIT_LIST_HEAD(&page->lru);
 	set_compound_page_dtor(page, HUGETLB_PAGE_DTOR);
 	hugetlb_set_page_subpool(page, NULL);
-	spin_lock(&hugetlb_lock);
 	set_hugetlb_cgroup(page, NULL);
 	set_hugetlb_cgroup_rsvd(page, NULL);
+	spin_lock(&hugetlb_lock);
 	h->nr_huge_pages++;
 	h->nr_huge_pages_node[nid]++;
 	spin_unlock(&hugetlb_lock);
