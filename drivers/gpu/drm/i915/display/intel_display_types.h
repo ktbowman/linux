741d02504c606c2b907cf85eb53bed1be368a2f3
Michel Dänzer <mdaenzer@redhat.com>
2022-01-06 12:15:51 +0100

    drm/i915: Move is_ccs_modifier() to intel_fb.c
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1988103
    Upstream Status: https://anongit.freedesktop.org/git/drm-intel.git
    
    Conflicts:
            drivers/gpu/drm/i915/display/intel_display_types.h
    
    commit e359c47bfa6155b56d378864b7cc4a0d4e680c1b
    Author:     Imre Deak <imre.deak@intel.com>
    AuthorDate: Wed Oct 20 22:51:37 2021 +0300
    Commit:     Imre Deak <imre.deak@intel.com>
    CommitDate: Thu Oct 21 21:44:36 2021 +0300
    
        Move the function to intel_fb.c and rename it adding the intel_fb_
        prefix following the naming of exported functions.
    
        Cc: Juha-Pekka Heikkila <juhapekka.heikkila@gmail.com>
        Signed-off-by: Imre Deak <imre.deak@intel.com>
        Reviewed-by: Juha-Pekka Heikkila <juhapekka.heikkila@gmail.com>
        Link: https://patchwork.freedesktop.org/patch/msgid/20211020195138.1841242-11-imre.deak@intel.com
    
    Signed-off-by: Michel Dänzer <mdaenzer@redhat.com>

--- drivers/gpu/drm/i915/display/intel_display_types.h
+++ drivers/gpu/drm/i915/display/intel_display_types.h
@@ -2037,13 +2037,4 @@ static inline u32 intel_fdi_link_freq(struct drm_i915_private *dev_priv,
 		return dev_priv->fdi_pll_freq;
 }
 
-static inline bool is_ccs_modifier(u64 modifier)
-{
-	return modifier == I915_FORMAT_MOD_Y_TILED_GEN12_RC_CCS ||
-	       modifier == I915_FORMAT_MOD_Y_TILED_GEN12_RC_CCS_CC ||
-	       modifier == I915_FORMAT_MOD_Y_TILED_GEN12_MC_CCS ||
-	       modifier == I915_FORMAT_MOD_Y_TILED_CCS ||
-	       modifier == I915_FORMAT_MOD_Yf_TILED_CCS;
-}
-
 #endif /*  __INTEL_DISPLAY_TYPES_H__ */
