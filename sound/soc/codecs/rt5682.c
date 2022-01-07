23b29d0671609156da24a51b77b0dde8dc23e297
Jaroslav Kysela <jkysela@redhat.com>
2022-01-07 20:26:33 +0100

    ASoC: codecs: rt5682(s): use the older clock API calls, but keep some modern
    With the recent changes, the clock API evolved, but the RHEL8 kernel
    has not backported all changes.
    
    With commit 3eee6c7d119c ("clkdev: add managed clkdev lookup registration")
    the situation is easier, but there are little pieces which must use
    the older API. The changes are straight and simple.
    
    Upstream status: RHEL-only
    
    Signed-off-by: Jaroslav Kysela <jkysela@redhat.com>
    Bugzilla: https://bugzilla.redhat.com/2003613

--- sound/soc/codecs/rt5682.c
+++ sound/soc/codecs/rt5682.c
@@ -2862,8 +2862,7 @@ int rt5682_register_dai_clks(struct rt5682_priv *rt5682)
 
 	for (i = 0; i < RT5682_DAI_NUM_CLKS; ++i) {
 		struct clk_init_data init = { };
-		struct clk_parent_data parent_data;
-		const struct clk_hw *parent;
+		const char *parent_name;
 
 		dai_clk_hw = &rt5682->dai_clks_hw[i];
 
@@ -2871,17 +2870,15 @@ int rt5682_register_dai_clks(struct rt5682_priv *rt5682)
 		case RT5682_DAI_WCLK_IDX:
 			/* Make MCLK the parent of WCLK */
 			if (rt5682->mclk) {
-				parent_data = (struct clk_parent_data){
-					.fw_name = "mclk",
-				};
-				init.parent_data = &parent_data;
+				parent_name = __clk_get_name(rt5682->mclk);
+				init.parent_names = &parent_name;
 				init.num_parents = 1;
 			}
 			break;
 		case RT5682_DAI_BCLK_IDX:
 			/* Make WCLK the parent of BCLK */
-			parent = &rt5682->dai_clks_hw[RT5682_DAI_WCLK_IDX];
-			init.parent_hws = &parent;
+			parent_name = clk_hw_get_name(&rt5682->dai_clks_hw[RT5682_DAI_WCLK_IDX]);
+			init.parent_names = &parent_name;
 			init.num_parents = 1;
 			break;
 		default:
