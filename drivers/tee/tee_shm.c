d153d40de1b11c3d8e6a5f47f9ce545a1a70cf51
Chris von Recklinghausen <crecklin@redhat.com>
2022-01-25 15:40:52 -0500

    tee: remove redundant teedev in struct tee_shm
    Bugzilla: http://bugzilla.redhat.com/2030755
    CVE: CVE-2021-44733
    
    commit 5271b2011e448f1be7433554e4684e91951476fa
    Author: Jens Wiklander <jens.wiklander@linaro.org>
    Date:   Thu Nov 7 11:42:59 2019 +0100
    
        tee: remove redundant teedev in struct tee_shm
    
        The ctx element in struct tee_shm is always valid. So remove the now
        redundant teedev element.
    
        Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    
    Signed-off-by: Chris von Recklinghausen <crecklin@redhat.com>

--- drivers/tee/tee_shm.c
+++ drivers/tee/tee_shm.c
@@ -13,7 +13,7 @@
 
 static void tee_shm_release(struct tee_shm *shm)
 {
-	struct tee_device *teedev = shm->teedev;
+	struct tee_device *teedev = shm->ctx->teedev;
 
 	mutex_lock(&teedev->mutex);
 	idr_remove(&teedev->idr, shm->id);
@@ -42,8 +42,7 @@ static void tee_shm_release(struct tee_shm *shm)
 		kfree(shm->pages);
 	}
 
-	if (shm->ctx)
-		teedev_ctx_put(shm->ctx);
+	teedev_ctx_put(shm->ctx);
 
 	kfree(shm);
 
@@ -130,7 +129,6 @@ static struct tee_shm *__tee_shm_alloc(struct tee_context *ctx,
 	}
 
 	shm->flags = flags | TEE_SHM_POOL;
-	shm->teedev = teedev;
 	shm->ctx = ctx;
 	if (flags & TEE_SHM_DMA_BUF)
 		poolm = teedev->pool->dma_buf_mgr;
@@ -239,7 +237,6 @@ struct tee_shm *tee_shm_register(struct tee_context *ctx, unsigned long addr,
 	}
 
 	shm->flags = flags | TEE_SHM_REGISTER;
-	shm->teedev = teedev;
 	shm->ctx = ctx;
 	shm->id = -1;
 	addr = untagged_addr(addr);
