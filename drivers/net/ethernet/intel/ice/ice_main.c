a68b8ea98633901d16ce17dbb35bc6c530d136fa
Ivan Vecera <ivecera@redhat.com>
2022-03-11 12:24:48 +0100

    ice: fix conversion to new udp_tunnel infrastructure
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2062343
    Upstream status: RHEL only
    
    Recent backport of b20e6c17c4685d ("ice: convert to new udp_tunnel
    infrastructure") uses 'shared' field from RHEL specific extension
    of udp_tunnel_nic structure but does not initialize this extension
    so value of 'shared' field is not take into account by network
    core.
    This patch adds missing RH_KABI_AUX_SET_SIZE for udp_tunnel_nic.
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- drivers/net/ethernet/intel/ice/ice_main.c
+++ drivers/net/ethernet/intel/ice/ice_main.c
@@ -4314,6 +4314,7 @@ ice_probe(struct pci_dev *pdev, const struct pci_device_id __always_unused *ent)
 	pf->hw.udp_tunnel_nic.unset_port = ice_udp_tunnel_unset_port;
 	pf->hw.udp_tunnel_nic.flags = UDP_TUNNEL_NIC_INFO_MAY_SLEEP |
 		__RH_UDP_TUNNEL_NIC_INFO_EXTENDED;
+	RH_KABI_AUX_SET_SIZE(&pf->hw.udp_tunnel_nic, udp_tunnel_nic_info);
 	pf->hw.udp_tunnel_nic._rh.shared = &pf->hw.udp_tunnel_shared;
 	i = 0;
 	if (pf->hw.tnl.valid_count[TNL_VXLAN]) {
