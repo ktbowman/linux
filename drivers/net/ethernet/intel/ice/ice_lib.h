27ac87f1a0e5e7e31e847d548182ed4f46d0fdb9
Ivan Vecera <ivecera@redhat.com>
2022-03-11 12:25:37 +0100

    ice: enable ndo_setup_tc support for mqprio_qdisc
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=2062343
    
    Conflicts:
    - due to backports applied in the past functions ice_set_min_bw_limit(),
      ice_set_max_bw_limit() and ice_get_link_speed_mbps() were misplaced
      and this causes context conflict. Apply the correction to this backport.
    
    commit fbc7b27af0f9fb181811424e29caf6825594a841
    Author: Kiran Patil <kiran.patil@intel.com>
    Date:   Fri Oct 15 16:35:16 2021 -0700
    
        ice: enable ndo_setup_tc support for mqprio_qdisc
    
        Add support in driver for TC_QDISC_SETUP_MQPRIO. This support
        enables instantiation of channels in HW using existing MQPRIO
        infrastructure which is extended to be offloadable. This
        provides a mechanism to configure dedicated set of queues for
        each TC.
    
        Configuring channels using "tc mqprio":
        --------------------------------------
        tc qdisc add dev <ethX> root mqprio num_tc 3 map 0 1 2 \
                queues 4@0 4@4 4@8  hw 1 mode channel
    
        Above command configures 3 TCs having 4 queues each. "hw 1 mode channel"
        implies offload of channel configuration to HW. When driver processes
        configuration received via "ndo_setup_tc: QDISC_SETUP_MQPRIO", each
        TC maps to HW VSI with specified queues.
    
        User can optionally specify bandwidth min and max rate limit per TC
        (see example below). If shaper params like min and/or max bandwidth
        rate limit are specified, driver configures VSI specific rate limiter
        in HW.
    
        Configuring channels and bandwidth shaper parameters using "tc mqprio":
        ----------------------------------------------------------------
        tc qdisc add dev <ethX> root mqprio \
                num_tc 4 map 0 1 2 3 queues 4@0 4@4 4@8 4@12 hw 1 mode channel \
                shaper bw_rlimit min_rate 1Gbit 2Gbit 3Gbit 4Gbit \
                max_rate 4Gbit 5Gbit 6Gbit 7Gbit
    
        Command to view configured TCs:
        -----------------------------
        tc qdisc show dev <ethX>
    
        Deleting TCs:
        ------------
        tc qdisc del dev <ethX> root mqprio
    
        Signed-off-by: Kiran Patil <kiran.patil@intel.com>
        Signed-off-by: Amritha Nambiar <amritha.nambiar@intel.com>
        Signed-off-by: Sudheer Mogilappagari <sudheer.mogilappagari@intel.com>
        Tested-by: Bharathi Sreenivas <bharathi.sreenivas@intel.com>
        Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- drivers/net/ethernet/intel/ice/ice_lib.h
+++ drivers/net/ethernet/intel/ice/ice_lib.h
@@ -122,7 +122,10 @@ bool ice_is_vsi_dflt_vsi(struct ice_sw *sw, struct ice_vsi *vsi);
 int ice_set_dflt_vsi(struct ice_sw *sw, struct ice_vsi *vsi);
 
 int ice_clear_dflt_vsi(struct ice_sw *sw);
-
+int ice_set_min_bw_limit(struct ice_vsi *vsi, u64 min_tx_rate);
+int ice_set_max_bw_limit(struct ice_vsi *vsi, u64 max_tx_rate);
+int ice_get_link_speed_kbps(struct ice_vsi *vsi);
+int ice_get_link_speed_mbps(struct ice_vsi *vsi);
 int
 ice_vsi_update_security(struct ice_vsi *vsi, void (*fill)(struct ice_vsi_ctx *));
 
@@ -137,7 +140,4 @@ void ice_vsi_ctx_clear_allow_override(struct ice_vsi_ctx *ctx);
 bool ice_is_feature_supported(struct ice_pf *pf, enum ice_feature f);
 void ice_clear_feature_support(struct ice_pf *pf, enum ice_feature f);
 void ice_init_feature_support(struct ice_pf *pf);
-int ice_set_min_bw_limit(struct ice_vsi *vsi, u64 min_tx_rate);
-int ice_set_max_bw_limit(struct ice_vsi *vsi, u64 max_tx_rate);
-int ice_get_link_speed_mbps(struct ice_vsi *vsi);
 #endif /* !_ICE_LIB_H_ */
