4ed9d6b8e808f9432cc2359edc4768b290e10608
Robert Richter <rrichter@amd.com>
2022-07-09 16:00:03 +0200

    check-for-restricted-cxl-device
    Signed-off-by: Robert Richter <rrichter@amd.com>

--- drivers/cxl/acpi.c
+++ drivers/cxl/acpi.c
@@ -274,6 +274,8 @@ static int add_root_nvdimm_bridge(struct device *match, void *data)
 struct pci_host_bridge *cxl_find_next_rch(struct pci_host_bridge *host)
 {
 	struct pci_bus *bus = host ? host->bus : NULL;
+	struct pci_dev *pdev;
+	bool is_restricted_host;
 
 	while ((bus = pci_find_next_bus(bus)) != NULL) {
 		host = bus ? to_pci_host_bridge(bus->bridge) : NULL;
@@ -287,6 +289,20 @@ struct pci_host_bridge *cxl_find_next_rch(struct pci_host_bridge *host)
 			continue;
 
 		dev_dbg(&host->dev, "PCI bridge is root bridge\n");
+
+		/* Check CXL DVSEC of dev 0 func 0 */
+		pdev = pci_get_slot(bus, PCI_DEVFN(0, 0));
+		is_restricted_host = pdev
+			&& (pci_pcie_type(pdev) == PCI_EXP_TYPE_RC_END)
+			&& pci_find_dvsec_capability(pdev,
+						PCI_DVSEC_VENDOR_ID_CXL,
+						CXL_DVSEC_PCIE_DEVICE);
+		pci_dev_put(pdev);
+
+		if (!is_restricted_host)
+			continue;
+
+		dev_dbg(&host->dev, "CXL restricted host found\n");
 
 		return host;
 	}
@@ -312,6 +328,8 @@ static int __init cxl_restricted_host_probe(struct platform_device *pdev)
 
 		dev_dbg(&host->dev, "PCI ACPI host found: %s\n",
 			acpi_dev_name(adev));
+
+		dev_info(&host->dev, "host supports CXL\n");
 	}
 
 	return 0;
