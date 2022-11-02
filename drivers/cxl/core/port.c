b2bc25f184e5e8fe229fe82fe8064363ae4ef49d
Robert Richter <rrichter@amd.com>
2022-11-02 20:06:02 +0100

    cxl/mem: Skip intermediate port enumeration of restricted endpoints (RCDs)
    When an endpoint is found, all ports in beetween the endpoint and the
    CXL host bridge need to be created. In the RCH case there are no ports
    in between a host bridge and the endpoint. Skip the enumeration of
    intermediate ports.
    
    The port enumeration does not only create all ports, it also
    initializes the endpoint chain by adding the endpoint to every
    downstream port up to the root bridge. This must be done also in RCD
    mode, but is much more simple as the endpoint only needs to be added
    to the host bridge's dport.
    
    Note: For endpoint removal the cxl_detach_ep() is not needed as it is
    released in cxl_port_release().
    
    Signed-off-by: Robert Richter <rrichter@amd.com>

--- drivers/cxl/core/port.c
+++ drivers/cxl/core/port.c
@@ -1366,7 +1366,23 @@ int devm_cxl_enumerate_ports(struct cxl_memdev *cxlmd)
 {
 	struct device *dev = &cxlmd->dev;
 	struct device *iter;
+	struct cxl_dport *dport;
+	struct cxl_port *port;
 	int rc;
+
+	/*
+	 * Skip intermediate port enumeration in the RCH case, there
+	 * are no ports in between a host bridge and an endpoint. Only
+	 * initialize the EP chain.
+	 */
+	if (is_cxl_restricted(cxlmd)) {
+		port = cxl_mem_find_port(cxlmd, &dport);
+		if (!port)
+			return -ENXIO;
+		rc = cxl_add_ep(dport, &cxlmd->dev);
+		put_device(&port->dev);
+		return rc;
+	}
 
 	rc = devm_add_action_or_reset(&cxlmd->dev, cxl_detach_ep, cxlmd);
 	if (rc)
@@ -1381,8 +1397,6 @@ retry:
 	for (iter = dev; iter; iter = grandparent(iter)) {
 		struct device *dport_dev = grandparent(iter);
 		struct device *uport_dev;
-		struct cxl_dport *dport;
-		struct cxl_port *port;
 
 		if (!dport_dev)
 			return 0;
