a5525806b0e0e315672cbd59cb61178b016833f7
Robert Richter <rrichter@amd.com>
2023-02-07 09:51:11 +0100

    cxl/port: Disable decoder setup for endpoints in RCD mode
    In RCD mode the HDM decoder capability is optional for endpoints and
    may not exist. The HDM range registers are used instead. Since the
    driver relies on the existence of an HDM decoder capability, its
    absence will cause the initialization of a memory card to fail.
    
    Moreover, the driver also tries to enable or reuse enabled memory
    ranges. In the worst case this may lead to a system hang due to
    disabling system memory that was previously provided and setup by
    system firmware.
    
    To solve the issues described, disable decoder setup for RCD endpoints
    and instead rely exclusively on system firmware to enable those memory
    ranges. Decoders are used by the kernel to setup and configure CXL
    memory regions, esp. to enable and disable them. Since Hot-plug is not
    supported for devices in RCD mode, the ability to disable that memory
    by the kernel using a decoder is not a necessarily requirement,
    decoders are not needed then.
    
    Fixes: 34e37b4c432c ("cxl/port: Enable HDM Capability after validating DVSEC Ranges")
    Signed-off-by: Robert Richter <rrichter@amd.com>

--- drivers/cxl/port.c
+++ drivers/cxl/port.c
@@ -85,14 +85,6 @@ static int cxl_endpoint_port_probe(struct cxl_port *port)
 	struct cxl_port *root;
 	int rc;
 
-	rc = cxl_dvsec_rr_decode(cxlds->dev, cxlds->cxl_dvsec, &info);
-	if (rc < 0)
-		return rc;
-
-	cxlhdm = devm_cxl_setup_hdm(port, &info);
-	if (IS_ERR(cxlhdm))
-		return PTR_ERR(cxlhdm);
-
 	/* Cache the data early to ensure is_visible() works */
 	read_cdat_data(port);
 
@@ -100,6 +92,23 @@ static int cxl_endpoint_port_probe(struct cxl_port *port)
 	rc = devm_add_action_or_reset(&port->dev, schedule_detach, cxlmd);
 	if (rc)
 		return rc;
+
+	/*
+	 * The HDM decoder capability may not exist. Do not
+	 * use decoders in RCD mode, instead rely on firmware
+	 * to setup the range or decoder registers and to
+	 * enable memory.
+	 */
+	if (cxlds->rcd)
+		return cxl_await_media_ready(cxlds);
+
+	rc = cxl_dvsec_rr_decode(cxlds->dev, cxlds->cxl_dvsec, &info);
+	if (rc < 0)
+		return rc;
+
+	cxlhdm = devm_cxl_setup_hdm(port, &info);
+	if (IS_ERR(cxlhdm))
+		return PTR_ERR(cxlhdm);
 
 	rc = cxl_hdm_decode_init(cxlds, cxlhdm, &info);
 	if (rc)
