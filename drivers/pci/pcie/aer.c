5fde590a413c207e952b8b9ae7cf4f8ea8156a0f
Robert Richter <rrichter@amd.com>
2023-02-17 14:45:47 +0100

    PCI/AER: Forward RCH downstream port-detected errors to the CXL.mem dev handler
    In Restricted CXL Device (RCD) mode a CXL device is exposed as an
    RCiEP, but CXL downstream and upstream ports are not enumerated and
    not visible in the PCIe hierarchy. Protocol and link errors are sent
    to an RCEC.
    
    Restricted CXL host (RCH) downstream port-detected errors are signaled
    as internal AER errors, either Uncorrectable Internal Error (UIE) or
    Corrected Internal Errors (CIE). The error source is the id of the
    RCEC. A CXL handler must then inspect the error status in various CXL
    registers residing in the dport's component register space (CXL RAS
    cap) or the dport's RCRB (AER ext cap). [1]
    
    Errors showing up in the RCEC's error handler must be handled and
    connected to the CXL subsystem. Implement this by forwarding the error
    to all CXL devices below the RCEC. Since the entire CXL device is
    controlled only using PCIe Configuration Space of device 0, Function
    0, only pass it there [2]. These devices have the Memory Device class
    code set (PCI_CLASS_MEMORY_CXL, 502h) and the existing cxl_pci driver
    can implement the handler. In addition to errors directed to the CXL
    endpoint device, the handler must also inspect the CXL downstream
    port's CXL RAS and PCIe AER external capabilities that is connected to
    the device.
    
    Since CXL downstream port errors are signaled using internal errors,
    the handler requires those errors to be unmasked. This is subject of a
    follow-on patch.
    
    The reason for choosing this implementation is that a CXL RCEC device
    is bound to the AER port driver, but the driver does not allow it to
    register a custom specific handler to support CXL. Connecting the RCEC
    hard-wired with a CXL handler does not work, as the CXL subsystem
    might not be present all the time. The alternative to add an
    implementation to the portdrv to allow the registration of a custom
    RCEC error handler isn't worth doing it as CXL would be its only user.
    Instead, just check for an CXL RCEC and pass it down to the connected
    CXL device's error handler. With this approach the code can entirely
    be implemented in the PCIe AER driver and is independent of the CXL
    subsystem. The CXL driver only provides the handler.
    
    [1] CXL 3.0 spec, 12.2.1.1 RCH Downstream Port-detected Errors
    [2] CXL 3.0 spec, 8.1.3 PCIe DVSEC for CXL Devices
    
    Co-developed-by: Terry Bowman <terry.bowman@amd.com>
    Signed-off-by: Robert Richter <rrichter@amd.com>
    Signed-off-by: Terry Bowman <terry.bowman@amd.com>
    Cc: "Oliver O'Halloran" <oohall@gmail.com>
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Cc: linuxppc-dev@lists.ozlabs.org
    Cc: linux-pci@vger.kernel.org

--- drivers/pci/pcie/aer.c
+++ drivers/pci/pcie/aer.c
@@ -943,6 +943,65 @@ static bool find_source_device(struct pci_dev *parent,
 	return true;
 }
 
+#ifdef CONFIG_PCIEAER_CXL
+
+static bool is_cxl_mem_dev(struct pci_dev *dev)
+{
+	/*
+	 * A CXL device is controlled only using PCIe Configuration
+	 * Space of device 0, Function 0.
+	 */
+	if (dev->devfn != PCI_DEVFN(0, 0))
+		return false;
+
+	/* Right now there is only a CXL.mem driver */
+	if ((dev->class >> 8) != PCI_CLASS_MEMORY_CXL)
+		return false;
+
+	return true;
+}
+
+static bool is_internal_error(struct aer_err_info *info)
+{
+	if (info->severity == AER_CORRECTABLE)
+		return info->status & PCI_ERR_COR_INTERNAL;
+
+	return info->status & PCI_ERR_UNC_INTN;
+}
+
+static void handle_error_source(struct pci_dev *dev, struct aer_err_info *info);
+
+static int cxl_handle_error_iter(struct pci_dev *dev, void *data)
+{
+	struct aer_err_info *e_info = (struct aer_err_info *)data;
+
+	if (!is_cxl_mem_dev(dev))
+		return 0;
+
+	/* pci_dev_put() in handle_error_source() */
+	dev = pci_dev_get(dev);
+	if (dev)
+		handle_error_source(dev, e_info);
+
+	return 0;
+}
+
+static void cxl_handle_error(struct pci_dev *dev, struct aer_err_info *info)
+{
+	/*
+	 * CXL downstream port errors are signaled as RCEC internal
+	 * errors. Forward them to all CXL devices below the RCEC.
+	 */
+	if (pci_pcie_type(dev) == PCI_EXP_TYPE_RC_EC &&
+	    is_internal_error(info))
+		pcie_walk_rcec(dev, cxl_handle_error_iter, info);
+}
+
+#else
+static inline void cxl_handle_error(struct pci_dev *dev,
+				    struct aer_err_info *info) { }
+#endif
+
 /**
  * handle_error_source - handle logging error into an event log
  * @dev: pointer to pci_dev data structure of error source device
@@ -953,6 +1012,8 @@ static bool find_source_device(struct pci_dev *parent,
 static void handle_error_source(struct pci_dev *dev, struct aer_err_info *info)
 {
 	int aer = dev->aer_cap;
+
+	cxl_handle_error(dev, info);
 
 	if (info->severity == AER_CORRECTABLE) {
 		/*
